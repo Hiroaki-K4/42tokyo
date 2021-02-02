cb18a27afb555288b6b261c6ae679070d78a3400
Hiroaki-K4 <49no19@gmail.com>
2021-02-02 22:44:04 +0900

    edit

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -10,41 +10,43 @@
 int get_next_line(int fd, char **line)
 {
     char *buf;
-    static char *store;
-    int i;
+    static char store[20000000];
+    // int i;
 
     if (store == NULL)
-        store = "d";
+        strcpy(store, "");
     if (strchr(store, '\n') == NULL)
     {
         if (!(buf = (char *)malloc(sizeof(char) * BUFFER_SIZE)))
             return (-1);
-        i = 0;
+        // i = 0;
         while (read(fd, buf, BUFFER_SIZE) > 0)
         {
-            printf("buf: %s\n", buf);
             if (strchr(buf, '\n') != NULL)
             {
-                while (buf[i] != '\n')
-                    i++;
-                if (i == 0 && strcmp(buf, "") != 0)
-                {
-                    if (!(*line = strdup(store)))
-                        return (-1);
-                    printf("line: %s\n", *line);
-                    // store = 
-                    return (1);
-                }
-                printf("count: %d\n", i);
+                // while (buf[i] != '\n')
+                //     i++;
+                // if (i == 0 && strcmp(buf, "") != 0)
+                // {
+                //     // if (!(*line = strdup(store)))
+                //     //     return (-1);
+                //     // printf("line: %s\n", *line);
+                //     // store = 
+                //     return (1);
+                // }
+                // if (!(*line = strdup(buf)))
+                //         return (-1);
+                // printf("ok");
+                // printf("count: %d\n", i);
             }
             else
             {
-                // TO DO 改行が見つからないときに既存のstoreにつなげる処理を書く
-                strcalt(store, buf);
+                strcat(store, buf);
+                // printf("buf: %s\n", buf);
+                printf("store: %s\n", store);
             }
-
-            // if (!(*line = strdup(buf)))
-            //     return (-1);
+            if (!(*line = strdup(buf)))
+                return (-1);
         }
     }
     // i = read(fd, buf, BUFFER_SIZE);
@@ -53,6 +55,8 @@ int get_next_line(int fd, char **line)
     // printf("line: %s\n", *line);
     // printf("%d\n", i);
     // printf("%s\n", buf);
+
+    // TO DO storeの中に改行があったときの処理を書く
     return 0;
 }
 
