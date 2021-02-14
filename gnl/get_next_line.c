8f57504c8ea7cbec79a783def7a9e7e8c14422d7
Hiroaki-K4 <49no19@gmail.com>
2021-02-14 15:35:02 +0900

    edit

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/02/14 13:12:24 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/02/14 15:03:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		get_new_line(int fd, char **store, char **line)
 {
-	int i;
+	int		i;
 
 	i = 0;
 	while (store[fd][i] != '\n')
@@ -26,12 +26,58 @@ int		get_new_line(int fd, char **store, char **line)
 	return (1);
 }
 
+int		save_new_line(int fd, char **store, char **line, char *buf)
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	while (buf[i] != '\n')
+		i++;
+	if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(store[fd]) + i + 1))))
+		return (-1);
+	ft_strlcpy(tmp, store[fd], ft_strlen(store[fd]) + 1);
+	ft_strlcat(tmp, buf, ft_strlen(store[fd]) + i + 1);
+	free(store[fd]);
+	store[fd] = tmp;
+	if (!(*line = ft_strdup(store[fd])))
+		return (-1);
+	ft_strlcpy(store[fd], &buf[i + 1], ft_strlen(&buf[i + 1]) + 1);
+	return (1);
+}
+
+int		get_make_line(int fd, char **store, char **line)
+{
+	char	*buf;
+	int		i;
+
+	if (!(buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	i = 1;
+	while (i > 0)
+	{
+		i = read(fd, buf, BUFFER_SIZE);
+		buf[i] = '\0';
+		if (i == -1)
+			return (-1);
+		if (ft_strchr(buf, '\n') != NULL)
+			return (save_new_line(fd, store, line, buf));
+		else
+		{
+			if (!(store[fd] = ft_strjoin(store[fd], buf)))
+				return (-1);
+		}
+	}
+	if (!(*line = ft_strdup(store[fd])))
+		return (-1);
+	free(buf);
+	return (0);
+}
+
 int		get_next_line(int fd, char **line)
 {
-	char *buf;
+	int			j;
 	static char *store[255];
-	char *tmp;
-	int i;
 
 	if (BUFFER_SIZE < 1)
 		return (-1);
@@ -40,40 +86,11 @@ int		get_next_line(int fd, char **line)
 			return (-1);
 	if (ft_strchr(store[fd], '\n') == NULL)
 	{
-		if (!(buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		j = get_make_line(fd, store, line);
+		if (j == 1)
+			return (1);
+		if (j == -1)
 			return (-1);
-		i = 1;
-		while (i > 0)
-		{
-			i = read(fd, buf, BUFFER_SIZE);
-			buf[i] = '\0';
-			if (i == -1)
-				return (-1);
-			if (ft_strchr(buf, '\n') != NULL)
-			{
-				i = 0;
-				while (buf[i] != '\n')
-					i++;
-				if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(store[fd]) + i + 1))))
-					return (-1);
-				ft_strlcpy(tmp, store[fd], ft_strlen(store[fd]) + 1);
-				ft_strlcat(tmp, buf, ft_strlen(store[fd]) + i + 1);
-				free(store[fd]);
-				store[fd] = tmp;
-				if (!(*line = ft_strdup(store[fd])))
-					return (-1);
-				ft_strlcpy(store[fd], &buf[i + 1], ft_strlen(&buf[i + 1]) + 1);
-				return (1);
-			}
-			else
-			{
-				if (!(store[fd] = ft_strjoin(store[fd], buf)))
-					return (-1);
-			}
-		}
-		if (!(*line = ft_strdup(store[fd])))
-			return (-1);
-		free(buf);
 	}
 	else
 		return (get_new_line(fd, store, line));
