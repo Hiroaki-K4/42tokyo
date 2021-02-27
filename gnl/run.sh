0a30673a2e762e046e57ffc4595b0fb696a6d9cc
Hiroaki-K4 <49no19@gmail.com>
2021-02-27 10:34:13 +0900

    edit

--- gnl/run.sh
+++ gnl/run.sh
@@ -9,26 +9,26 @@
 # gcc -Wall -Wextra -Werror -D BUFFER_SIZE=10000 get_next_line.c get_next_line_utils.c test.c
 # ./a.out sample.txt
 
-# echo "~~~short_line~~~"
-# for i in `seq 15`
-# do
-# gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$i get_next_line.c get_next_line_utils.c test.c
-# ./a.out short.txt
-# done
+echo "~~~short_line~~~"
+for i in `seq 15`
+do
+gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$i get_next_line.c get_next_line_utils.c test.c
+./a.out short.txt
+done
 
-# echo "~~~long_line~~~"
-# for i in `seq 15`
-# do
-# gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$i get_next_line.c get_next_line_utils.c test.c
-# ./a.out long.txt
-# done
+echo "~~~long_line~~~"
+for i in `seq 15`
+do
+gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$i get_next_line.c get_next_line_utils.c test.c
+./a.out long.txt
+done
 
-# echo "~~~empty_test~~~"
-# for i in `seq 15`
-# do
-# gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$i get_next_line.c get_next_line_utils.c test.c
-# ./a.out empty.txt
-# done
+echo "~~~empty_test~~~"
+for i in `seq 15`
+do
+gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$i get_next_line.c get_next_line_utils.c test.c
+./a.out empty.txt
+done
 
 # gcc -Wall -Wextra -Werror -D BUFFER_SIZE=10 get_next_line.c get_next_line_utils.c test.c
 # ./a.out main1.c
@@ -82,18 +82,18 @@
 # ./a.out test_006.txt
 # echo "~~none.txt~~"
 # ./a.out none.txt
-gcc -Wall -Wextra -Werror -g -D BUFFER_SIZE=2000 main_file.c get_next_line.c get_next_line_utils.c
-echo "~~test_001.txt~~"
-./a.out test_001.txt
-echo "~~test_002.txt~~"
-./a.out test_002.txt
-echo "~~test_003.txt~~"
-./a.out test_003.txt
-echo "~~test_004.txt~~"
-./a.out test_004.txt
-echo "~~test_005.txt~~"
-./a.out test_005.txt
-echo "~~test_006.txt~~"
-./a.out test_006.txt
-echo "~~none.txt~~"
-./a.out none.txt
+# gcc -Wall -Wextra -Werror -g -D BUFFER_SIZE=2000 main_file.c get_next_line.c get_next_line_utils.c
+# echo "~~test_001.txt~~"
+# ./a.out test_001.txt
+# echo "~~test_002.txt~~"
+# ./a.out test_002.txt
+# echo "~~test_003.txt~~"
+# ./a.out test_003.txt
+# echo "~~test_004.txt~~"
+# ./a.out test_004.txt
+# echo "~~test_005.txt~~"
+# ./a.out test_005.txt
+# echo "~~test_006.txt~~"
+# ./a.out test_006.txt
+# echo "~~none.txt~~"
+# ./a.out none.txt
