91bad59a88bbb3a4d60125c704bb82da1574cf0e
Hiroaki-K4 <49no19@gmail.com>
2021-02-27 23:00:54 +0900

    edit

--- printf/test.c
+++ printf/test.c
@@ -6,18 +6,35 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/02/27 22:29:33 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/02/27 23:00:42 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 int ft_printf(const char *arg, ...)
 {
     va_list ap;
+    int i;
     
     va_start(ap, arg);
+    printf("arg: %s", arg);
+    i = 0;
+    while (arg[i])
+    {
+        if (arg[i] == '%')
+        {
+            i++;
+            write(1, &arg[i], 1);
+        }
+        else
+        {
+            write(1, &arg[i], 1);
+        }
+        i++;
+    }
     printf("%s\n", va_arg(ap, char*));
     printf("%s\n", va_arg(ap, char*));
     va_end(ap);
