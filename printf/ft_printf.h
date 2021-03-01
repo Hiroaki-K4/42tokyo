6808ec91b66672e7374f39b0501d2448bc9bfe39
Hiroaki-K4 <49no19@gmail.com>
2021-03-01 22:01:42 +0900

    edit

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 14:52:50 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/01 21:59:09 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/01 22:01:34 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,4 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
-// size_t		ft_strlen(const char *str);
-// void	ft_putstr_fd(char *s, int fd);
 int ft_printf(const char *arg, ...);
-// int		ft_isdigit(int c);
