3d00bef017548f795bcf257dac4cbc859c462371
Hiroaki-K4 <49no19@gmail.com>
2021-03-02 22:56:49 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/02 22:38:39 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/02 22:41:11 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int ft_printf(const char *arg, ...)
     }
     va_end(ap);
     
-    return (0);
+    return (i);
 }
 
 // int ft_printf(const char *arg, ...)
