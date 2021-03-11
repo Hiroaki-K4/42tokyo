8be30c4ee4c67ee9ad9eaee5af411e40874c6785
Hiroaki-K4 <49no19@gmail.com>
2021-03-11 21:08:52 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/11 21:08:44 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/11 21:08:50 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ int str_to_precision(const char *arg, int *i, va_list *ap)
         precision = va_arg(*ap, int);
     // if ()
     printf("pre: %d\n", precision);
-    printf("i: %d\n", i);
+    printf("i: %d\n", *i);
     return (0);
 }
 
