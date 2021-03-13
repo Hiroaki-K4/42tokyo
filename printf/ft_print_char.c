7071c19315c045ad6a8f90684be2b34ec8a8eaa2
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 11:26:17 +0900

    edit

--- printf/ft_print_char.c
+++ printf/ft_print_char.c
@@ -6,15 +6,15 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 11:21:37 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 11:25:35 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 11:26:14 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void print_char(va_list *ap, a_list flag_list)
-{
-    ap = NULL;
-    flag_list.format = 0;
-    printf("ok");
-}
+// void print_char(va_list *ap, a_list flag_list)
+// {
+//     ap = NULL;
+//     flag_list.format = 0;
+//     printf("ok");
+// }
