60d60c316dc3a522fe9c9205a5623406e9ee59d7
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 17:30:32 +0900

    edit

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:11 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 17:29:36 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 17:30:26 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,10 @@ int main(void)
     i = ft_printf("ft_printf: [%-10c %-c %c]\n", 'a', samp1, 'c');
     printf("len: %d\n", i);
     printf("~~string~~");
-    i = printf("printf:    [%-10c %-c %c]\n", 'a', 'b', 'c');
+    i = printf("printf:    [%s]\n", "ccc");
     printf("len: %d\n", i);
+    i = ft_rintf("ft_printf:    [%s]\n", "ccc");
+    ft_printf("len: %d\n", i);
     // ft_printf("ft_printf: [aa]\n");
     // ft_printf("ft_printf: [%0-2425.3d]\n", 10);
     // ft_printf("ft_printf: [%0-*.*ad]\n", 2425, 3, 10);
