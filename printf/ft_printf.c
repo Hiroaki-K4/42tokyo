1f6111ea485478f11d9c0392531a28ebc71887c4
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 11:25:56 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/12 09:55:54 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 11:25:51 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,7 @@ int output_per(va_list *ap, a_list flag_list)
 int ft_printf_per(const char *arg, int i, va_list *ap)
 {
     int j;
-    int k;
+    // int k;
     a_list flag_list;
 
     flag_list = init_list(flag_list);
@@ -115,8 +115,8 @@ int ft_printf_per(const char *arg, int i, va_list *ap)
         flag_list.precision = str_to_num(&arg[i], &i, ap);
     }
     flag_list.format = ft_strchr_place("cspdiuxX%", arg[i], &i);
-    k = output_per(ap, flag_list);
-    printf("k: %d\n", k);
+    // k = output_per(ap, flag_list);
+    // printf("k: %d\n", k);
     // Output
     return (-1);
 }
