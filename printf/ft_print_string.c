9be1bcc8260f5bd12332600a0eed0d30e097eb6e
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 17:49:38 +0900

    edit

--- printf/ft_print_string.c
+++ printf/ft_print_string.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 17:44:52 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 17:46:01 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 17:49:36 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int print_string(va_list *ap, a_list flag_list)
 {
+    int len;
     char *pub;
 
+    len = 0;
     pub = va_arg(*ap, char*);
     // if (flag_list.field != -1)
     // {
@@ -23,18 +25,18 @@ int print_string(va_list *ap, a_list flag_list)
     //     {
     //         if (flag_list.precision != -1)
     //         {
-
+    //             len = field_flag_pre();
     //         }
     //         else
     //         {
-
+    //             len = field_flag();
     //         }
     //     }
     //     else
     //     {
     //         if (flag_list.precision != -1)
     //         {
-
+                
     //         }
     //         else
     //         {
@@ -67,6 +69,7 @@ int print_string(va_list *ap, a_list flag_list)
     //         }
     //     }
     // }
+    printf("len: %d\n", len);
     printf("pub: %s\n", pub);
     printf("flag: %d\n", flag_list.precision);
     return (0);
