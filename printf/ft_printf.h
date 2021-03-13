de22d57f851968fa12ae1763607ce33f143e0f6e
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 11:22:16 +0900

    edit

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,12 +6,13 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 14:52:50 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/12 09:54:22 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 11:22:15 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 #include "libft/libft.h"
 
 typedef	struct
