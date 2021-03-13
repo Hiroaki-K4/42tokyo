bcb83cb9df2964a71f2dbd7b4d4cb585242d1e1d
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 16:23:52 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 16:23:24 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 16:23:49 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -119,6 +119,7 @@ int ft_printf_per(const char *arg, int i, va_list *ap)
         flag_list.precision = str_to_num(&arg[i], &i, ap);
     }
     flag_list.format = ft_strchr_place("cspdiuxX%", arg[i], &i);
+    printf("k: %d\n", k);
     k += output_per(ap, flag_list);
     printf("k: %d\n", k);
     // Output
