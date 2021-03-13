59b2f5330019acaf354f59ef1fc533b58643c976
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 20:38:46 +0900

    edit

--- printf/ft_print_string.c
+++ printf/ft_print_string.c
@@ -6,69 +6,57 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 17:44:52 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 20:34:36 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 20:38:40 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int field_flag_pre(va_list *ap, a_list flag_list)
+int field_flag_pre(char *str, a_list flag_list)
 {
-    char *arg;
 
-    arg = va_arg(*ap, char *);
-    printf("arg: %s\n", arg);
+    printf("arg: %s\n", str);
     printf("flag: %d\n", flag_list.precision);
     return (0);
 }
 
 
-int field_flag(va_list *ap, a_list flag_list)
+int field_flag(char *str, a_list flag_list)
 {
-    char *arg;
 
-    arg = va_arg(*ap, char *);
-    printf("arg: %s\n", arg);
+    printf("arg: %s\n", str);
     printf("flag: %d\n", flag_list.precision);
     return (0);
 }
 
-int field_pre(va_list *ap, a_list flag_list)
+int field_pre(char *str, a_list flag_list)
 {
-    char *arg;
 
-    arg = va_arg(*ap, char *);
-    printf("arg: %s\n", arg);
+    printf("arg: %s\n", str);
     printf("flag: %d\n", flag_list.precision);
     return (0);
 }
 
-int field_only(va_list *ap, a_list flag_list)
+int field_only(char *str, a_list flag_list)
 {
-    char *arg;
 
-    arg = va_arg(*ap, char *);
-    printf("arg: %s\n", arg);
+    printf("arg: %s\n", str);
     printf("flag: %d\n", flag_list.precision);
     return (0);
 }
 
-int pre_only(va_list *ap, a_list flag_list)
+int pre_only(char *str, a_list flag_list)
 {
-    char *arg;
 
-    arg = va_arg(*ap, char *);
-    printf("arg: %s\n", arg);
+    printf("arg: %s\n", str);
     printf("flag: %d\n", flag_list.precision);
     return (0);
 }
 
-int no_option(va_list *ap, a_list flag_list)
+int no_option(char *str, a_list flag_list)
 {
-    char *arg;
 
-    arg = va_arg(*ap, char *);
-    printf("arg: %s\n", arg);
+    printf("arg: %s\n", str);
     printf("flag: %d\n", flag_list.precision);
     return (0);
 }
@@ -76,36 +64,36 @@ int no_option(va_list *ap, a_list flag_list)
 int print_string(va_list *ap, a_list flag_list)
 {
     int len;
-    char *pub;
+    char *str;
 
     len = 0;
-    pub = va_arg(*ap, char*);
+    str = va_arg(*ap, char*);
     if (flag_list.field != -1)
     {
         if (flag_list.flag[0] == 1)
         {
             if (flag_list.precision != -1)
-                len = field_flag_pre(ap, flag_list);
+                len = field_flag_pre(str, flag_list);
             else
-                len = field_flag(ap, flag_list);
+                len = field_flag(str, flag_list);
         }
         else
         {
             if (flag_list.precision != -1)
-                len = field_pre(ap, flag_list);
+                len = field_pre(str, flag_list);
             else
-                len = field_only(ap, flag_list);
+                len = field_only(str, flag_list);
         }
     }
     else
     {
         if (flag_list.precision != -1)
-            len = pre_only(ap, flag_list);
+            len = pre_only(str, flag_list);
         else
-            len = no_option(ap, flag_list);
+            len = no_option(str, flag_list);
     }
     printf("len: %d\n", len);
-    printf("pub: %s\n", pub);
+    printf("str: %s\n", str);
     printf("flag: %d\n", flag_list.precision);
     return (len);
 }
