cdafd6ef957d0848a301eb3baf3d25843005c2f4
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 16:55:33 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 16:55:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 16:55:30 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -157,7 +157,6 @@ int ft_printf(const char *arg, ...)
         else
         {
             print_len = ft_printf_per(arg, &i, &ap);
-            printf("iiii: %d\n", i);
             printf("arg: %c\n", arg[i]);
             // printf("i: %d\n", i);
             // printf("yes");
