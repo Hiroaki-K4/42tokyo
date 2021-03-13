cea288cdc37f0820f67d0549c388807b2d84dfc3
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 21:04:05 +0900

    edit

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:11 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 21:00:41 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 21:04:01 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int main(void)
     printf("~~string~~\n");
     i = printf("printf:    [%s]\n", "ccc");
     printf("len: %d\n", i);
-    i = ft_printf("ft_printf:    [%s]\n", "ccc");
+    i = ft_printf("ft_printf: [%s]\n", "ccc");
     ft_printf("len: %d\n", i);
     // ft_printf("ft_printf: [aa]\n");
     // ft_printf("ft_printf: [%0-2425.3d]\n", 10);
