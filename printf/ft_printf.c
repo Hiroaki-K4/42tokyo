e799093803275a19fd1b60cfbcb9d89ef9294b04
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 16:20:16 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 16:19:09 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 16:20:12 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -147,11 +147,13 @@ int ft_printf(const char *arg, ...)
         {
             i = ft_printf_str(arg, i);
             // printf("i: %d\n", i);
+            printf("no");
         }
         else
         {
             i = ft_printf_per(arg, i+1, &ap);
             // printf("i: %d\n", i);
+            printf("yes");
         }
     }
     va_end(ap);
