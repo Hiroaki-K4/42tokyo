439f75d6f0bd35add89dd4c175b039b1a813efc1
Hiroaki-K4 <49no19@gmail.com>
2021-03-13 16:13:33 +0900

    edit

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:11 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 16:11:07 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/13 16:13:19 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int main(void)
     // ft_printf("ft_printf: [aa]\n");
     // ft_printf("ft_printf: [%0-2425.3d]\n", 10);
     // ft_printf("ft_printf: [%0-*.*ad]\n", 2425, 3, 10);
-    printf("printf:    [%010d]\n", 10);
+    // printf("printf:    [%010d]\n", 10);
     // ft_printf("ft_printf: [%c]\n", 'a');
     // printf("printf:    [%c]\n", samp1);
     // ft_printf("ft_printf: [%c]\n", samp1);
