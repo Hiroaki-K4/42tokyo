a5e52412208009cc3c6c1d4e48c0d27f050123a4
Hiroaki-K4 <49no19@gmail.com>
2021-03-14 11:28:31 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/14 11:25:55 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/14 11:28:29 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,8 @@ int print_int(va_list *ap, a_list flag_list)
         //many func
         printf("ok\n");
     }
-    else
-        len = no_field(num, str_num, flag_list);
+    // else
+    //     len = no_field(num, str_num, flag_list);
     printf("num: %d\n", num);
     printf("flag: %d\n", flag_list.precision);
     return(len);
