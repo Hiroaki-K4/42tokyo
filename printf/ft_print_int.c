7738390f4094e031728d6d05665ef219feb754a3
Hiroaki-K4 <49no19@gmail.com>
2021-03-14 22:32:55 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/14 22:31:27 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/14 22:32:53 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,6 +128,7 @@ int field_precision(int num, char *str, a_list flag_list)
             j++;
         }
         tmp[i] = '\0';
+        printf("tmp: %s\n", tmp);
     }
     
     printf("num: %d\n", num);
