457824187137b477cdc47c0dc3a1580ad8bc438b
Hiroaki-K4 <49no19@gmail.com>
2021-03-14 21:22:13 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/14 21:19:49 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/14 21:22:12 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ int field_no_precision(int num, char *str, a_list flag_list)
         len = 0;
     }
     printf("num: %d\n", num);
-    printf("str: %s\n", str);
+    // printf("str: %s\n", str);
     printf("flag_list: %d\n", flag_list.precision);
     return (len);
 }
