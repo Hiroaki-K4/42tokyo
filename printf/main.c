114d370053922807db2ce36fa2d9595511171fc4
Hiroaki-K4 <49no19@gmail.com>
2021-03-14 14:51:57 +0900

    edit

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:11 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/14 14:51:44 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/14 14:51:55 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int main(void)
     i = ft_printf("ft_printf: [%-10s]\n", "ccc");
     printf("len: %d\n", i);
     printf("~~int~~\n");
-    i = printf("printf:    [%-010d]\n", 300);
+    i = printf("printf:    [%010d]\n", 300);
     printf("len: %d\n", i);
     // i = ft_printf("ft_printf: [%d]\n", 300);
     // printf("len: %d\n", i);
