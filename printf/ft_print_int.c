ce1425930f36f7d53c7c5101653ebeed57613185
Hiroaki-K4 <49no19@gmail.com>
2021-03-14 22:44:10 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/14 22:43:51 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/14 22:44:07 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,7 +136,7 @@ int field_precision(int num, char *str, a_list flag_list)
             j++;
         }
         tmp[i] = '\0';
-        printf("tmp: %s\n", tmp);
+        // printf("tmp: %s\n", tmp);
     }
     if (flag_list.flag[0] == 1)
     {
