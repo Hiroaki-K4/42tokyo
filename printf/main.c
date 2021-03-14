8e8ee76cf8144a53aacfa6e2c0d144f183550cc6
Hiroaki-K4 <49no19@gmail.com>
2021-03-14 10:47:22 +0900

    edit

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:11 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/13 22:23:47 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/14 10:47:11 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,26 +67,5 @@ int main(void)
     printf("len: %d\n", i);
     i = printf("printf:    [%-10.2d]\n", 300);
     printf("len: %d\n", i);
-    // ft_printf("ft_printf: [aa]\n");
-    // ft_printf("ft_printf: [%0-2425.3d]\n", 10);
-    // ft_printf("ft_printf: [%0-*.*ad]\n", 2425, 3, 10);
-    // printf("printf:    [%010d]\n", 10);
-    // ft_printf("ft_printf: [%c]\n", 'a');
-    // printf("printf:    [%c]\n", samp1);
-    // ft_printf("ft_printf: [%c]\n", samp1);
-    // printf("printf:    [%s]\n", "ccc");
-    // ft_printf("ft_printf: [%s]\n", "ccc");
-    // printf("printf:    [%s]\n", samp2);
-    // ft_printf("ft_printf: [%s]\n", samp2);
-    // printf("printf:    [%c %s]\n", samp1, samp2);
-    // ft_printf("ft_printf: [%c %s]\n", samp1, samp2);
-    // printf("printf:    [%s %c]\n", samp2, samp1);
-    // ft_printf("ft_printf: [%s %c]\n", samp2, samp1);
-    // printf("printf:    [%s %c %%%]\n", samp2, samp1);
-    // ft_printf("ft_printf: [%s %c %%%]\n", samp2, samp1);
-    // printf("printf:    [%10c]\n", 'a');
-    // ft_printf("ft_printf: [%10c]\n", 'a');
-    // printf("printf:    [%10s]\n", "bbb");
-    // ft_printf("ft_printf: [%10s]\n", "bbb");
     return (0);
 }
