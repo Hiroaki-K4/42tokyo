5efa87058cbb0eb3c6f5422de044c8414b90a594
Hiroaki-K4 <49no19@gmail.com>
2021-03-14 21:56:48 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/14 21:42:29 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/14 21:56:37 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,6 @@ int field_no_precision(int num, char *str, a_list flag_list)
             write(1, " ", 1);
             i++;
         }
-        len = flag_list.field;
     }
     else
     {
@@ -77,13 +76,44 @@ int field_no_precision(int num, char *str, a_list flag_list)
                 i++;
             }
             write(1, str, ft_strlen(str));
-            len = flag_list.field;
         }
-        else
+        if (num < 0 && flag_list.flag[0] != 1 && flag_list.flag == 1)
         {
-            
+            // if (flag_list.flag[0] == 1)
+            // {
+            //     write(1, str, ft_strlen(str));
+            //     i = 0;
+            //     while (flag_list.field - ft_strlen(str) - i)
+            //     {
+            //         write(1, " ", 1);
+            //         i++;
+            //     }
+                
+            // }
+            // if (flag_list.flag[1] == 1)
+            // {
+            //     write(1, "-", 1);
+            //     i = 1;
+            //     while (flag_list.field - ft_strlen(str) - i)
+            //     {
+            //         write(1, "0", 1);
+            //         i++;
+            //     }
+            //     num *= -1;
+            //     write(1, ft_itoa(num), ft_strlen(ft_itoa(num)));
+            // }
+            write(1, "-", 1);
+            i = 1;
+            while (flag_list.field - ft_strlen(str) - i)
+            {
+                write(1, "0", 1);
+                i++;
+            }
+            num *= -1;
+            write(1, ft_itoa(num), ft_strlen(ft_itoa(num)));
         }
     }
+    len = flag_list.field;
     return (len);
 }
 
