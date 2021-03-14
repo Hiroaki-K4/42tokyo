738d1f2a766710cf55213e9455d40766c3b5a0ae
Hiroaki-K4 <49no19@gmail.com>
2021-03-14 22:44:00 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/14 22:39:53 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/14 22:43:51 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,6 @@ int field_precision(int num, char *str, a_list flag_list)
             j++;
         }
         tmp[i] = '\0';
-        printf("tmp_len: %ld\n", ft_strlen(tmp));
     }
     else
     {
@@ -138,6 +137,36 @@ int field_precision(int num, char *str, a_list flag_list)
         }
         tmp[i] = '\0';
         printf("tmp: %s\n", tmp);
+    }
+    if (flag_list.flag[0] == 1)
+    {
+        i = 0;
+        while (ft_strlen(tmp) - i)
+        {
+            write(1, tmp, ft_strlen(tmp));
+            i++;
+        }
+        i = 0;
+        while (flag_list.field - ft_strlen(tmp) - i)
+        {
+            write(1, " ", 1);
+            i++;
+        }
+    }
+    else
+    {
+        i = 0;
+        while (flag_list.field - ft_strlen(tmp) - i)
+        {
+            write(1, " ", 1);
+            i++;
+        }
+        i = 0;
+        while (ft_strlen(tmp) - i)
+        {
+            write(1, tmp, ft_strlen(tmp));
+            i++;
+        }
     }
     return (flag_list.field);
 }
