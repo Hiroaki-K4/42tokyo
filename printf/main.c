2cfc1efa0fd6557617003a4f826acf036e943b15
Hiroaki-K4 <49no19@gmail.com>
2021-03-15 22:54:21 +0900

    edit

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:11 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/15 22:53:29 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/15 22:54:19 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,17 +59,17 @@ int main(void)
     printf("len: %d\n", i);
     printf("len: %d\n", j);
     i = printf("printf:    [%10d]\n", 300);
+    j = ft_printf("ft_printf: [%10d]\n", 300);
     printf("len: %d\n", i);
-    i = ft_printf("ft_printf: [%10d]\n", 300);
-    printf("len: %d\n", i);
+    printf("len: %d\n", j);
     i = printf("printf:    [%-010d]\n", 300);
+    j = ft_printf("ft_printf: [%-010d]\n", 300);
     printf("len: %d\n", i);
-    i = ft_printf("ft_printf: [%-010d]\n", 300);
-    printf("len: %d\n", i);
+    printf("len: %d\n", j);
     i = printf("printf:    [%-10.5d]\n", -300);
+    j = ft_printf("ft_printf: [%-10.5d]\n", -300);
     printf("len: %d\n", i);
-    i = ft_printf("ft_printf: [%-10.5d]\n", -300);
-    printf("len: %d\n", i);
+    printf("len: %d\n", j);
     // i = printf("printf:    [%-10.5d]\n", 300);
     // printf("len: %d\n", i);
     // i = printf("printf:    [%10.d]\n", 300);
