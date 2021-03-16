3411db2c8a85add6b56881ab32ce4ddf034e0192
Hiroaki-K4 <49no19@gmail.com>
2021-03-16 09:14:26 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/16 09:13:25 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/16 09:14:24 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,10 @@ int str_to_num(const char *arg, int *i, va_list *ap)
         }
     }
     else
+    {
         num = -1;
+        (*i)++;
+    }
     return (num);
 }
 
