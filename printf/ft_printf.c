5343a66abc605f073d07d10f6fedf68f83803035
Hiroaki-K4 <49no19@gmail.com>
2021-03-16 09:21:37 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/16 09:19:42 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/16 09:21:35 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ int str_to_num(const char *arg, int *i, va_list *ap)
         return (num);
     }
     j = ft_atoi(arg);
-    printf("j: %d\n", j);
+    // printf("j: %d\n", j);
     // if (j == 0)
     // {
     //     num = 0;
@@ -102,6 +102,7 @@ int str_to_num(const char *arg, int *i, va_list *ap)
         num = -1;
         // (*i)++;
     }
+    printf("num: %d\n", num);
     return (num);
 }
 
