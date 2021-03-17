454ebf5f37dd25d8f993aa07043970cd5076a2d7
Hiroaki-K4 <49no19@gmail.com>
2021-03-17 21:50:12 +0900

    edit

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:11 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/17 21:34:44 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/17 21:50:03 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,12 @@ int main(void)
     
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("~~string~~\n");
-    i = printf("printf:    [%09s]\n", "000hi low");
-    j = ft_printf("ft_printf: [%09s]\n", "000hi low");
+    i = printf("printf:    [%3.6s]\n", "hi low");
+    // j = ft_printf("ft_printf: [%3.6s]\n", "hi low");
+    printf("len: %d\n", i);
+    printf("len: %d\n", j);
+    i = printf("printf:    [%09s]\n", "hi low");
+    j = ft_printf("ft_printf: [%09s]\n", "hi low");
     printf("len: %d\n", i);
     printf("len: %d\n", j);
     i = printf("printf:    [%.00s]\n", "");
