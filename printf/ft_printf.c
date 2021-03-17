f4b8dc86aeb7153381ece0a7f1e50df19a6fb2c6
Hiroaki-K4 <49no19@gmail.com>
2021-03-17 21:21:30 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/17 21:19:43 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/17 21:21:28 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,6 +141,7 @@ int ft_printf_per(const char *arg, int *i, va_list *ap)
     // Check the precision
     if (arg[*i] == '.')
     {
+        printf("argc: %c\n", arg[*i]);
         (*i)++;
         flag_list.precision = str_to_num(&arg[*i], i, ap, 1);
         // printf("pre: %d\n", flag_list.precision);
