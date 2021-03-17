b24b2ed3c5d49dbca062e81fc2bea348f757a9a8
Hiroaki-K4 <49no19@gmail.com>
2021-03-17 23:12:55 +0900

    edit

--- printf/ft_print_unsigned_int.c
+++ printf/ft_print_unsigned_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 23:07:41 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/17 23:08:38 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/17 23:12:51 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int print_unsigned_int(va_list *ap, a_list flag_list)
 {
+    unsigned int num;
     
+    num = va_arg(*ap, unsigned int);
+    printf("num: %d\n", num);
+    printf("flag: %d\n", flag_list.precision);
+    return (0);
 }
