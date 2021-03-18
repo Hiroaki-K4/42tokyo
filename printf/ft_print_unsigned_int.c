9bb912216cb70df8d005b62dd5eaacedff3e6132
Hiroaki-K4 <49no19@gmail.com>
2021-03-18 09:25:47 +0900

    edit

--- printf/ft_print_unsigned_int.c
+++ printf/ft_print_unsigned_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 23:07:41 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/17 23:13:41 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/18 09:25:45 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,66 @@
 int print_unsigned_int(va_list *ap, a_list flag_list)
 {
     unsigned int num;
+    int len;
+    int keta;
+    char *str_num;
     
-    num = va_arg(*ap, unsigned int);
-    printf("num: %u\n", num);
-    printf("flag: %d\n", flag_list.precision);
-    return (0);
+    len = 0;
+    num = va_arg(*ap, int);
+    str_num = ft_itoa(num);
+    keta = ft_strlen(str_num);
+    if (flag_list.flag[1] == 1 && flag_list.precision != -1)
+        flag_list.flag[1] = 0;
+    if (flag_list.precision == 0 && num == 0)
+        return (pre_arg_zero(flag_list));
+    if (flag_list.field > (int)ft_strlen(str_num))
+    {
+        if (flag_list.precision > keta)
+            len = field_precision(num, str_num, flag_list);
+        else
+            len = field_no_precision(num, str_num, flag_list);
+    }
+    else
+        len = no_field_int(num, str_num, flag_list);
+    // printf("len: %d\n", len);
+    return(len);
+    
+
+
+
+
+    // num = va_arg(*ap, unsigned int);
+    // printf("num: %u\n", num);
+    // printf("flag: %d\n", flag_list.precision);
+    // return (0);
 }
+
+// int print_int(va_list *ap, a_list flag_list)
+// {
+//     int num;
+//     int len;
+//     int keta;
+//     char *str_num;
+
+//     len = 0;
+//     num = va_arg(*ap, int);
+//     str_num = ft_itoa(num);
+//     keta = ft_strlen(str_num);
+//     if (flag_list.flag[1] == 1 && flag_list.precision != -1)
+//         flag_list.flag[1] = 0;
+//     if (num < 0)
+//         keta--;
+//     if (flag_list.precision == 0 && num == 0)
+//         return (pre_arg_zero(flag_list));
+//     if (flag_list.field > (int)ft_strlen(str_num))
+//     {
+//         if (flag_list.precision > keta)
+//             len = field_precision(num, str_num, flag_list);
+//         else
+//             len = field_no_precision(num, str_num, flag_list);
+//     }
+//     else
+//         len = no_field_int(num, str_num, flag_list);
+//     // printf("len: %d\n", len);
+//     return(len);
+// }
