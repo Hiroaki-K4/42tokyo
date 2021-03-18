44dce07bea211c517464e0dce5fa459ef5596dfd
Hiroaki-K4 <49no19@gmail.com>
2021-03-18 10:29:51 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/18 09:29:29 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/18 10:29:49 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int no_field_int(int num, char *str, a_list flag_list)
     int len;
     int digit;
 
+    printf("ok1\n");
     if (num >= 0)
         digit = ft_strlen(str);
     else
