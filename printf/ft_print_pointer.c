d358701a26a9ac34ac648df82f4fd798a7f29df0
Hiroaki-K4 <49no19@gmail.com>
2021-03-18 21:11:09 +0900

    edit

--- printf/ft_print_pointer.c
+++ printf/ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:41:01 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/18 21:10:43 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/18 21:11:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int print_pointer(va_list *ap, a_list flag_list)
     void *num;
     
     num = va_arg(*ap, void *);
-    len = ft_strlen(num);
+    len = ft_strlen((char*)num);
     printf("len: %d\n", len);
     // if (flag_list)
     printf("num: %p\n", num);
