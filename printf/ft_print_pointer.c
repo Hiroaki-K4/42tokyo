2790170eff951a00298257374e4d6c36c453fc76
Hiroaki-K4 <49no19@gmail.com>
2021-03-18 21:05:43 +0900

    edit

--- printf/ft_print_pointer.c
+++ printf/ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:41:01 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/18 20:52:44 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/18 21:03:01 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int print_pointer(va_list *ap, a_list flag_list)
     void *num;
     
     num = va_arg(*ap, void *);
+    // if (flag_list)
     printf("num: %p\n", num);
     // printf("num: %p\n", &num);
     printf("flag: %d\n", flag_list.precision);
