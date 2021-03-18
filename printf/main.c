40c9386f1de34f99a6d73e05c73657ac1695d85a
Hiroaki-K4 <49no19@gmail.com>
2021-03-18 18:47:42 +0900

    edit

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:11 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/18 18:39:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/18 18:47:39 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -244,9 +244,9 @@ int main(void)
     printf("~~pointer p~~\n");
     int a;
     i = printf("printf:    [%p]\n", &a);
-    // j = ft_printf("ft_printf: [%p]\n", 10);
+    j = ft_printf("ft_printf: [%p]\n", &a);
     printf("len: %d\n", i);
-    // printf("len: %d\n", j);
+    printf("len: %d\n", j);
     
 
     // printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
