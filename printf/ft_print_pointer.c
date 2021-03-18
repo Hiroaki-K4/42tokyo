e46ac8fe41419990fecf83b30ec6c0f95b5c5590
Hiroaki-K4 <49no19@gmail.com>
2021-03-18 22:30:44 +0900

    edit

--- printf/ft_print_pointer.c
+++ printf/ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:41:01 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/18 22:15:24 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/18 22:30:40 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,37 @@ int print_pointer(va_list *ap, a_list flag_list)
     char *str_num;
     int keta;
     int len;
+    char *ans_str;
+    int i;
     
     num = va_arg(*ap, unsigned long);
     str_num = ft_itoa_hex_long(num, "0123456789abcdef");
     // tmp = (unsigned int)&num;
     keta = ft_strlen(str_num);
+    if (!(ans_str = (char *)malloc(sizeof(char) * (keta + 3))))
+		return (NULL);
+    ans_str[keta] = '\0';
+    ans_str[0] = '0';
+    ans_str[1] = 'x';
+    i = 0;
+    while (str_num[i])
+    {
+        ans_str[i + 2] = str_num[i];
+        i++;
+    }
     if (flag_list.flag[1] == 1 && flag_list.precision != -1)
         flag_list.flag[1] = 0;
     if (flag_list.precision == 0 && num == 0)
         return (pre_arg_zero(flag_list));
-    if (flag_list.field > (int)ft_strlen(str_num))
+    if (flag_list.field > (int)ft_strlen(ans_str))
     {
         if (flag_list.precision > keta)
-            len = field_precision(num, str_num, flag_list);
+            len = field_precision(num, ans_str, flag_list);
         else
-            len = field_no_precision(num, str_num, flag_list);
+            len = field_no_precision(num, ans_str, flag_list);
     }
     else
-        len = no_field_int(num, str_num, flag_list);
+        len = no_field_int(num, ans_str, flag_list);
     // printf("len: %d\n", len);
     // if (flag_list)
     // printf("num: %lu\n", num);
