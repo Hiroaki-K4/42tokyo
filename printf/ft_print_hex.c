be2883181dc848f308f8c161ebf4212e24670c4a
Hiroaki-K4 <49no19@gmail.com>
2021-03-18 18:16:07 +0900

    edit

--- printf/ft_print_hex.c
+++ printf/ft_print_hex.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 11:15:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/18 18:15:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/18 18:16:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 int print_hex(va_list *ap, a_list flag_list)
 {
     unsigned int num;
-    char *str;
+    char *str_num;
     int keta;
     int len;
 
     num = va_arg(*ap, unsigned int);
     if (flag_list.format == 6)
-        str = ft_itoa_hex(num, "0123456789abcdef");
+        str_num = ft_itoa_hex(num, "0123456789abcdef");
     else
-        str = ft_itoa_hex(num, "0123456789ABCDEF");
-    keta = ft_strlen(str);
+        str_num = ft_itoa_hex(num, "0123456789ABCDEF");
+    keta = ft_strlen(str_num);
     if (flag_list.flag[1] == 1 && flag_list.precision != -1)
         flag_list.flag[1] = 0;
     if (flag_list.precision == 0 && num == 0)
         return (pre_arg_zero(flag_list));
-    if (flag_list.field > (int)ft_strlen(str_num))
+    if (flag_list.field > (int)ft_strlen(_SC_TRACE_USER_EVENT_MAX))
     {
         if (flag_list.precision > keta)
             len = field_precision(num, str_num, flag_list);
