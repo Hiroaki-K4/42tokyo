7f324af948f98753d052d56ed26a70b5de65fba3
Hiroaki-K4 <49no19@gmail.com>
2021-03-18 10:31:25 +0900

    edit

--- printf/ft_print_unsigned_int.c
+++ printf/ft_print_unsigned_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 23:07:41 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/18 10:31:10 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/18 10:31:22 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int print_unsigned_int(va_list *ap, a_list flag_list)
     
     len = 0;
     num = va_arg(*ap, unsigned int);
-    printf("num: %d\n", num);
+    // printf("num: %d\n", num);
     str_num = ft_itoa_unsigned(num);
-    // printf("str_num: %s\n", str_num);
+    printf("str_num: %s\n", str_num);
     keta = ft_strlen(str_num);
     if (flag_list.flag[1] == 1 && flag_list.precision != -1)
         flag_list.flag[1] = 0;
