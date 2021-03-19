38faaf63a4cfb4ab6529bc3d3f69c44882f43c31
Hiroaki-K4 <49no19@gmail.com>
2021-03-19 22:52:49 +0900

    edit

--- printf/ft_print_pointer.c
+++ printf/ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:41:01 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/19 22:51:31 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/19 22:52:48 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,32 @@ int print_pointer(va_list *ap, a_list flag_list)
     str_num = ft_itoa_hex_long(num, "0123456789abcdef");
     // tmp = (unsigned int)&num;
     keta = ft_strlen(str_num);
+    // if (!(ans_str = (char *)malloc(sizeof(char) * (keta + 3))))
+	// 	return (-1);
+    // ans_str[keta] = '\0';
+    // ans_str[0] = '0';
+    // ans_str[1] = 'x';
+    // i = 0;
+    // while (str_num[i])
+    // {
+    //     ans_str[i + 2] = str_num[i];
+    //     i++;
+    // }
+    size = 0;
+    if (flag_list.flag[0] == 1 && flag_list.flag[1])
+        flag_list.flag[1] = 0;
+    if (flag_list.precision != -1)
+    {
+        flag_list.flag[1] = 0;
+        if (flag_list.precision > keta)
+            size = flag_list.precision + 2;
+        else
+            size = keta + 2;
+    }
+    else if (flag_list.flag[1] == 1 && flag_list.field > (keta + 2))
+        size = flag_list.field;
+    else
+        size = keta + 2;
     if (!(ans_str = (char *)malloc(sizeof(char) * (keta + 3))))
 		return (-1);
     ans_str[keta] = '\0';
@@ -38,27 +64,7 @@ int print_pointer(va_list *ap, a_list flag_list)
         ans_str[i + 2] = str_num[i];
         i++;
     }
-    size = 0;
-    if (flag_list.flag[0] == 1 && flag_list.flag[1])
-        flag_list.flag[1] = 0;
-    if (flag_list.precision != -1)
-    {
-        // printf("ok\n");
-        flag_list.flag[1] = 0;
-        if (flag_list.precision > keta)
-        {
-            size = flag_list.precision + 2;
-        }
-        else
-            size = keta + 2;
-    }
-    else if (flag_list.flag[1] == 1 && flag_list.field > (keta + 2))
-        size = flag_list.field;
-    else
-        size = keta + 2;
-    printf("size: %d\n", size);
-    // if (flag_list.precision == 0 && num == 0)
-    //     return (pre_arg_zero(flag_list));
+    printf("ans: %s\n", ans_str);
     if (flag_list.field > (int)ft_strlen(ans_str))
     {
         if (flag_list.precision > keta)
