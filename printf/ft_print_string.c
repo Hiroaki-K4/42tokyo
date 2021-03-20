70c02594aecad2b3fe7d9f86b19aa4d2fd1d1c68
Hiroaki-K4 <49no19@gmail.com>
2021-03-20 12:22:25 +0900

    edit

--- printf/ft_print_string.c
+++ printf/ft_print_string.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 17:44:52 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/20 12:21:58 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/20 12:22:23 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int field_flag_str(char *str, a_list flag_list)
     int i;
     int len;
 
+    printf("ok1\n");
     if (flag_list.precision != -1 && flag_list.precision < (int)ft_strlen(str))
     {
         write(1, str, flag_list.precision);
