ac78115cc1089cfe86bbf25ed36c508d4a651f39
Hiroaki-K4 <49no19@gmail.com>
2021-03-20 12:13:31 +0900

    edit

--- printf/ft_print_pointer.c
+++ printf/ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:41:01 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/20 12:10:00 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/20 12:13:23 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,8 @@ int print_pointer(va_list *ap, a_list flag_list)
     num = va_arg(*ap, unsigned long);
     // printf("num: %ld\n", num);
     str_num = ft_itoa_hex_long(num, "0123456789abcdef");
-    if (num == 0 && flag_list.precision == 0)
-        str_num = "";
+    // if (num == 0 && flag_list.precision == 0)
+    //     str_num = "";
     keta = ft_strlen(str_num);
     // printf("keta: %d\n", keta);
     size = 0;
@@ -88,7 +88,7 @@ int print_pointer(va_list *ap, a_list flag_list)
         size = keta + 2;
     if (!(ans_str = (char *)malloc(sizeof(char) * (size + 1))))
 		return (-1);
-    ans_str[keta] = '\0';
+    ans_str[size] = '\0';
     ans_str[0] = '0';
     ans_str[1] = 'x';
     if (size > (keta + 2))
