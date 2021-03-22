02fcdda42ec393ada0ffc6f84460e04037398afe
Hiroaki-K4 <49no19@gmail.com>
2021-03-22 21:34:09 +0900

    edit

--- printf/ft_print_pointer.c
+++ printf/ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:41:01 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/22 21:32:09 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/22 21:34:05 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,8 +84,8 @@ int		make_return_len(t_plist flag_list, int size, char *ans_str, int i)
 
 char	*make_pointer_str(char *str_num, int j, int len, int size)
 {
-	int i;
-	char *ans_str;
+	int		i;
+	char	*ans_str;
 
 	i = 0;
 	if (!(ans_str = (char *)malloc(sizeof(char) * (size + 1))))
@@ -121,13 +121,13 @@ int		print_pointer(va_list *ap, t_plist flag_list, int i, int j)
 
 	num = (unsigned long)va_arg(*ap, void *);
 	str_num = ft_itoa_hex_long(num, "0123456789abcdef");
-	len = ft_strlen(str_num);
+	// len = ft_strlen(str_num);
 	if (flag_list.precision == 0 && num == 0)
 		return (pre_arg_zero_pointer(flag_list, 0, 0));
 	if (flag_list.flag[0] == 1 && flag_list.flag[1])
 		flag_list.flag[1] = 0;
-	size = make_str_size(flag_list, len);
-	if (!(ans_str = make_pointer_str(str_num, j, len, size)))
+	size = make_str_size(flag_list, ft_strlen(str_num));
+	if (!(ans_str = make_pointer_str(str_num, j, ft_strlen(str_num), size)))
 		return (-1);
 	if (flag_list.field > size)
 		len = make_return_len(flag_list, size, ans_str, 0);
