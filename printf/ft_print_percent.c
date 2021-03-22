d28ca5a53f31e4c67ad28edad97bd0130e7d6023
Hiroaki-K4 <49no19@gmail.com>
2021-03-22 14:01:50 +0900

    edit

--- printf/ft_print_percent.c
+++ printf/ft_print_percent.c
@@ -6,13 +6,28 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 23:28:58 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/22 13:59:15 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/22 14:01:44 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	print_percent(t_plist flag_list, int i, int len)
+void	print_percent_sub(t_plist flag_list, int i)
+{
+	if (flag_list.flag[1] == 1)
+	{
+		while ((flag_list.field - 1 - (i++)) > 0)
+			write(1, "0", 1);
+	}
+	else
+	{
+		while ((flag_list.field - 1 - (i++)) > 0)
+			write(1, " ", 1);
+	}
+	write(1, "%", 1);
+}
+
+int		print_percent(t_plist flag_list, int i, int len)
 {
 	if (flag_list.field > 1)
 	{
@@ -24,17 +39,18 @@ int	print_percent(t_plist flag_list, int i, int len)
 		}
 		else
 		{
-			if (flag_list.flag[1] == 1)
-			{
-				while ((flag_list.field - 1 - (i++)) > 0)
-					write(1, "0", 1);
-			}
-			else
-			{
-				while ((flag_list.field - 1 - (i++)) > 0)
-					write(1, " ", 1);
-			}
-			write(1, "%", 1);
+			// if (flag_list.flag[1] == 1)
+			// {
+			// 	while ((flag_list.field - 1 - (i++)) > 0)
+			// 		write(1, "0", 1);
+			// }
+			// else
+			// {
+			// 	while ((flag_list.field - 1 - (i++)) > 0)
+			// 		write(1, " ", 1);
+			// }
+			// write(1, "%", 1);
+			print_percent_sub(flag_list, i);
 		}
 		len = flag_list.field;
 	}
