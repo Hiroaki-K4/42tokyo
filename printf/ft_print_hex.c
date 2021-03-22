1bb8424a2ae6e01ba4fb643ed4d14c17d0bc4006
Hiroaki-K4 <49no19@gmail.com>
2021-03-22 10:51:21 +0900

    edit

--- printf/ft_print_hex.c
+++ printf/ft_print_hex.c
@@ -6,39 +6,39 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 11:15:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/22 09:55:28 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/22 10:50:52 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int print_hex(va_list *ap, t_plist flag_list)
+int	print_hex(va_list *ap, t_plist flag_list, int keta, int len)
 {
-    unsigned int num;
-    char *str_num;
-    int keta;
-    int len;
+	unsigned	int	num;
+	char			*str_num;
+	// int				keta;
+	// int				len;
 
-    num = va_arg(*ap, unsigned int);
-    if (flag_list.format == 6)
-        str_num = ft_itoa_hex(num, "0123456789abcdef");
-    else
-        str_num = ft_itoa_hex(num, "0123456789ABCDEF");
-    keta = ft_strlen(str_num);
-    if ((int)num < 0)
-        num *= -1;
-    if (flag_list.flag[1] == 1 && flag_list.precision != -1)
-        flag_list.flag[1] = 0;
-    if (flag_list.precision == 0 && num == 0)
-        return (pre_arg_zero(flag_list));
-    if (flag_list.field > (int)ft_strlen(str_num))
-    {
-        if (flag_list.precision > keta)
-            len = field_precision(num, str_num, flag_list, 0);
-        else
-            len = field_no_precision(num, str_num, flag_list, 0);
-    }
-    else
-        len = no_field_int(num, str_num, flag_list, 0);
-    return (len);
+	num = va_arg(*ap, unsigned int);
+	if (flag_list.format == 6)
+		str_num = ft_itoa_hex(num, "0123456789abcdef");
+	else
+		str_num = ft_itoa_hex(num, "0123456789ABCDEF");
+	keta = ft_strlen(str_num);
+	if ((int)num < 0)
+		num *= -1;
+	if (flag_list.flag[1] == 1 && flag_list.precision != -1)
+		flag_list.flag[1] = 0;
+	if (flag_list.precision == 0 && num == 0)
+		return (pre_arg_zero(flag_list));
+	if (flag_list.field > (int)ft_strlen(str_num))
+	{
+		if (flag_list.precision > keta)
+			len = field_precision(num, str_num, flag_list, 0);
+		else
+			len = field_no_precision(num, str_num, flag_list, 0);
+	}
+	else
+		len = no_field_int(num, str_num, flag_list, 0);
+	return (len);
 }
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 14:52:50 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/22 10:43:27 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/22 10:51:00 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int		field_precision(int num, char *str, t_plist flag_list, int i);
 int		field_no_precision(int num, char *str, t_plist flag_list, int i);
 int		no_field_int(int num, char *str, t_plist flag_list, int len);
 char	*ft_itoa_unsigned(unsigned int n);
-int		print_hex(va_list *ap, t_plist flag_list);
+int		print_hex(va_list *ap, t_plist flag_list, int keta, int len);
 char	*ft_itoa_hex(unsigned int n, char *arg);
 char	*ft_itoa_hex_long(unsigned long n, char *arg);
 int		print_pointer(va_list *ap, t_plist flag_list);
