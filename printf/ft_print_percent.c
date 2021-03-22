1d12e64390a4d2884db9f4fef5a273914641459a
Hiroaki-K4 <49no19@gmail.com>
2021-03-22 11:42:08 +0900

    edit

--- printf/ft_print_percent.c
+++ printf/ft_print_percent.c
@@ -6,59 +6,59 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 23:28:58 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/21 17:50:41 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/22 11:41:54 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int print_percent(t_plist flag_list)
+int	print_percent(t_plist flag_list)
 {
-    int i;
-    int len;
+	int i;
+	int len;
 
-    len = 0;
-    if (flag_list.field > 1)
-    {
-        if (flag_list.flag[0] == 1)
-        {
-            write(1, "%", 1);
-            i = 0;
-            while (flag_list.field - 1 - i)
-            {
-                write(1, " ", 1);
-                i++;
-            }
-        }
-        else
-        {
-            if (flag_list.flag[1] == 1)
-            {
-                i = 0;
-                while (flag_list.field - 1 - i)
-                {
-                    write(1, "0", 1);
-                    i++;
-                }
-                write(1, "%", 1);
-            }
-            else
-            {
-                i = 0;
-                while (flag_list.field - 1 - i)
-                {
-                    write(1, " ", 1);
-                    i++;
-                }
-                write(1, "%", 1);
-            }
-        }
-        len = flag_list.field;
-    }
-    else
-    {
-        write(1, "%", 1);
-        len = 1;
-    }
-    return (len);
+	len = 0;
+	if (flag_list.field > 1)
+	{
+		if (flag_list.flag[0] == 1)
+		{
+			write(1, "%", 1);
+			i = 0;
+			while (flag_list.field - 1 - i)
+			{
+				write(1, " ", 1);
+				i++;
+			}
+		}
+		else
+		{
+			if (flag_list.flag[1] == 1)
+			{
+				i = 0;
+				while (flag_list.field - 1 - i)
+				{
+					write(1, "0", 1);
+					i++;
+				}
+				write(1, "%", 1);
+			}
+			else
+			{
+				i = 0;
+				while (flag_list.field - 1 - i)
+				{
+					write(1, " ", 1);
+					i++;
+				}
+				write(1, "%", 1);
+			}
+		}
+		len = flag_list.field;
+	}
+	else
+	{
+		write(1, "%", 1);
+		len = 1;
+	}
+	return (len);
 }
