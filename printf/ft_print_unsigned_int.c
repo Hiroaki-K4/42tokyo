1740b1a09edbfdcf186479f5f3c145d8c5ac57f4
Hiroaki-K4 <49no19@gmail.com>
2021-03-22 10:58:16 +0900

    edit

--- printf/ft_print_unsigned_int.c
+++ printf/ft_print_unsigned_int.c
@@ -6,138 +6,138 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 23:07:41 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/21 17:50:53 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/22 10:58:12 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int no_field_unsigned(char *str, t_plist flag_list)
+int	no_field_unsigned(char *str, t_plist flag_list)
 {
-    int len;
-    int digit;
+	int len;
+	int digit;
 
-    digit = ft_strlen(str);
-    if (flag_list.precision > digit)
-    {
-        len = flag_list.precision;
-        while (flag_list.precision - digit > 0)
-        {
-            write(1, "0", 1);
-            digit++;
-        }
-        write(1, str, ft_strlen(str));
-    }
-    else
-    {
-        write(1, str, ft_strlen(str));
-        len = ft_strlen(str);
-    }
-    return (len);
+	digit = ft_strlen(str);
+	if (flag_list.precision > digit)
+	{
+		len = flag_list.precision;
+		while (flag_list.precision - digit > 0)
+		{
+			write(1, "0", 1);
+			digit++;
+		}
+		write(1, str, ft_strlen(str));
+	}
+	else
+	{
+		write(1, str, ft_strlen(str));
+		len = ft_strlen(str);
+	}
+	return (len);
 }
 
-int field_precision_unsigned(char *str, t_plist flag_list)
+int	field_precision_unsigned(char *str, t_plist flag_list)
 {
-    int i;
-    int j;
-    char *tmp;
-    
-    if (!(tmp = (char *)malloc(sizeof(char) * (flag_list.precision + 1))))
-        return (-1);
-    i = 0;
-    while (flag_list.precision - ft_strlen(str) - i)
-    {
-        tmp[i] = '0';
-        i++;
-    }
-    j = 0;
-    while (ft_strlen(str) - j)
-    {
-        tmp[i] = str[j];
-        i++;
-        j++;
-    }
-    tmp[i] = '\0';
-    if (flag_list.flag[0] == 1)
-    {
-        write(1, tmp, ft_strlen(tmp));
-        i = 0;
-        while (flag_list.field - ft_strlen(tmp) - i)
-        {
-            write(1, " ", 1);
-            i++;
-        }
-    }
-    else
-    {
-        i = 0;
-        while ((flag_list.field - (int)ft_strlen(tmp) - i) > 0)
-        {
-            write(1, " ", 1);
-            i++;
-        }
-        write(1, tmp, ft_strlen(tmp));
-        if (flag_list.precision > flag_list.field)
-            return (flag_list.precision);
-    }
-    free(tmp);
-    return (flag_list.field);
+	int		i;
+	int		j;
+	char	*tmp;
+
+	if (!(tmp = (char *)malloc(sizeof(char) * (flag_list.precision + 1))))
+		return (-1);
+	i = 0;
+	while (flag_list.precision - ft_strlen(str) - i)
+	{
+		tmp[i] = '0';
+		i++;
+	}
+	j = 0;
+	while (ft_strlen(str) - j)
+	{
+		tmp[i] = str[j];
+		i++;
+		j++;
+	}
+	tmp[i] = '\0';
+	if (flag_list.flag[0] == 1)
+	{
+		write(1, tmp, ft_strlen(tmp));
+		i = 0;
+		while (flag_list.field - ft_strlen(tmp) - i)
+		{
+			write(1, " ", 1);
+			i++;
+		}
+	}
+	else
+	{
+		i = 0;
+		while ((flag_list.field - (int)ft_strlen(tmp) - i) > 0)
+		{
+			write(1, " ", 1);
+			i++;
+		}
+		write(1, tmp, ft_strlen(tmp));
+		if (flag_list.precision > flag_list.field)
+			return (flag_list.precision);
+	}
+	free(tmp);
+	return (flag_list.field);
 }
 
-int field_no_precision_unsigned(char *str, t_plist flag_list)
+int	field_no_precision_unsigned(char *str, t_plist flag_list)
 {
-    int i;    
+	int i;    
 
-    if (flag_list.flag[0] == 1)
-    {
-        write(1, str, ft_strlen(str));
-        i = 0;
-        while (flag_list.field - ft_strlen(str) - i)
-        {
-            write(1, " ", 1);
-            i++;
-        }
-    }
-    else
-    {
-        i = 0;
-        while (flag_list.field - ft_strlen(str) - i)
-        {
-            if (flag_list.flag[1] == 1)
-                write(1, "0", 1);
-            else
-                write(1, " ", 1);
-            i++;
-        }
-        write(1, str, ft_strlen(str));
-    }
-    return (flag_list.field);
+	if (flag_list.flag[0] == 1)
+	{
+		write(1, str, ft_strlen(str));
+		i = 0;
+		while (flag_list.field - ft_strlen(str) - i)
+		{
+			write(1, " ", 1);
+			i++;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (flag_list.field - ft_strlen(str) - i)
+		{
+			if (flag_list.flag[1] == 1)
+				write(1, "0", 1);
+			else
+				write(1, " ", 1);
+			i++;
+		}
+		write(1, str, ft_strlen(str));
+	}
+	return (flag_list.field);
 }
 
-int print_unsigned_int(va_list *ap, t_plist flag_list)
+int	print_unsigned_int(va_list *ap, t_plist flag_list)
 {
-    unsigned int num;
-    int len;
-    int keta;
-    char *str_num;
-    
-    len = 0;
-    num = va_arg(*ap, unsigned int);
-    str_num = ft_itoa_unsigned(num);
-    // printf("str_num: %s\n", str_num);
-    keta = ft_strlen(str_num);
-    if (flag_list.flag[1] == 1 && flag_list.precision != -1)
-        flag_list.flag[1] = 0;
-    if (flag_list.precision == 0 && num == 0)
-        return (pre_arg_zero(flag_list));
-    if (flag_list.field > (int)ft_strlen(str_num))
-    {
-        if (flag_list.precision > keta)
-            len = field_precision_unsigned(str_num, flag_list);
-        else
-            len = field_no_precision_unsigned(str_num, flag_list);
-    }
-    else
-        len = no_field_unsigned(str_num, flag_list);
-    return(len);
+	unsigned	int	num;
+	int				len;
+	int				keta;
+	char			*str_num;
+
+	len = 0;
+	num = va_arg(*ap, unsigned int);
+	str_num = ft_itoa_unsigned(num);
+	// printf("str_num: %s\n", str_num);
+	keta = ft_strlen(str_num);
+	if (flag_list.flag[1] == 1 && flag_list.precision != -1)
+		flag_list.flag[1] = 0;
+	if (flag_list.precision == 0 && num == 0)
+		return (pre_arg_zero(flag_list));
+	if (flag_list.field > (int)ft_strlen(str_num))
+	{
+		if (flag_list.precision > keta)
+			len = field_precision_unsigned(str_num, flag_list);
+		else
+			len = field_no_precision_unsigned(str_num, flag_list);
+	}
+	else
+		len = no_field_unsigned(str_num, flag_list);
+	return(len);
 }
