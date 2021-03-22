aa1bc342057d18ec3289dceb25198b2a669430a4
Hiroaki-K4 <49no19@gmail.com>
2021-03-22 22:19:25 +0900

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/22 22:17:22 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/22 22:19:22 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,10 +105,12 @@ int		ft_printf(const char *arg, ...)
 			print_len += ft_printf_str(arg, &i);
 		else
 		{
-			if ((j = ft_printf_per(arg, &i, &ap)) == -1)
+			j = ft_printf_per(arg, &i, &ap);
+			if (j == -1)
 				return (-1);
 			// print_len += ft_printf_per(arg, &i, &ap);
 			print_len += j;
+		}
 		
 	}
 	va_end(ap);
