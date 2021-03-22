1cb4902d1b3872a29bd1cccbcd6d33a596580931
Hiroaki-K4 <49no19@gmail.com>
2021-03-22 11:50:02 +0900

    edit

--- printf/ft_print_percent.c
+++ printf/ft_print_percent.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 23:28:58 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/22 11:47:20 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/22 11:49:58 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,12 @@
 
 int	print_percent(t_plist flag_list, int i, int len)
 {
-	// int i;
-	// int len;
-
-	// len = 0;
 	if (flag_list.field > 1)
 	{
 		if (flag_list.flag[0] == 1)
 		{
 			write(1, "%", 1);
-			i = 0;
+			// i = 0;
 			while ((flag_list.field - 1 - (i++)) > 0)
 				write(1, " ", 1);
 		}
@@ -31,14 +27,14 @@ int	print_percent(t_plist flag_list, int i, int len)
 		{
 			if (flag_list.flag[1] == 1)
 			{
-				i = 0;
+				// i = 0;
 				while ((flag_list.field - 1 - (i++)) > 0)
 					write(1, "0", 1);
 				write(1, "%", 1);
 			}
 			else
 			{
-				i = 0;
+				// i = 0;
 				while ((flag_list.field - 1 - (i++)) > 0)
 					write(1, " ", 1);
 				write(1, "%", 1);
