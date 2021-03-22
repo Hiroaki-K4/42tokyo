08af10e7546d6b53962dbb455844017ec0f70bef
Hiroaki-K4 <49no19@gmail.com>
2021-03-22 09:25:47 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/21 22:16:58 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/22 09:25:33 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ int	no_field_int(int num, char *str, t_plist flag_list, int len)
 {
 	int digit;
 
-	if (num >= 0)
-		digit = ft_strlen(str);
-	else
+	// if (num >= 0)
+	digit = ft_strlen(str);
+	// else
+	if (num < 0)
 		digit = ft_strlen(str) - 1;
 	if (flag_list.precision > digit)
 	{
