e27700e4af4d04d5d2d061390516e9beec3d1598
Hiroaki-K4 <49no19@gmail.com>
2021-03-23 09:40:27 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/23 09:37:09 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/23 09:40:19 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int	no_field_int(int num, char *str, t_plist flag_list, int len)
 		{
 			if (!(tmp = ft_itoa(num * (-1))))
 				return (-1);
+			free(str);
 			str = tmp;
-			free(tmp);
 			write(1, "-", 1);
 			len = flag_list.precision + 1;
 		}
