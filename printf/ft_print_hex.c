3cb4a09e36aff61d172816d65bf9a48902ecfbcf
Hiroaki-K4 <49no19@gmail.com>
2021-03-23 11:33:58 +0900

    edit

--- printf/ft_print_hex.c
+++ printf/ft_print_hex.c
@@ -6,13 +6,13 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 11:15:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/23 11:33:32 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/23 11:33:56 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	get_hex_len(t_plist flag_list, unsigned int num, char str_num)
+int	get_hex_len(t_plist flag_list, unsigned int num, char *str_num)
 {
 	int len;
 
