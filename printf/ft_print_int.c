aa03f5f43e08d5f31672444193748e5dc920331a
Hiroaki-K4 <49no19@gmail.com>
2021-03-23 11:42:48 +0900

    edit

--- printf/ft_print_int.c
+++ printf/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 22:01:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/23 11:41:50 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/23 11:42:42 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	no_field_int(int num, char *str, t_plist flag_list, int len)
 {
 	int		digit;
-	// char	*tmp;
 
 	digit = ft_strlen(str);
 	if (num < 0)
@@ -24,17 +23,7 @@ int	no_field_int(int num, char *str, t_plist flag_list, int len)
 	{
 		len = flag_list.precision;
 		if (num < 0)
-		{
-			// if (!(tmp = ft_itoa(num * (-1))))
-			// 	return (-1);
-			// write(1, "-", 1);
-			// len = flag_list.precision + 1;
-			// while (flag_list.precision - (digit++) > 0)
-			// 	write(1, "0", 1);
-			// write(1, tmp, ft_strlen(tmp));
-			// free(tmp);
 			len = no_field_int_sub(flag_list, num, digit);
-		}
 		else
 		{
 			while (flag_list.precision - (digit++) > 0)
