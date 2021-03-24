d9285b1b6e721501c79b5c30c28c58da4910f459
Hiroaki-K4 <49no19@gmail.com>
2021-03-24 09:53:53 +0900

    edit

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:11 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/24 09:51:44 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/24 09:53:50 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,8 +94,8 @@ int main(void)
     
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("~~int~~\n");
-    i = printf("printf:    [%-020d]\n", -2147483648);
-    j = ft_printf("ft_printf: [%-020d]\n", -2147483648);
+    i = printf("printf:    [%020d]\n", -2147483648);
+    j = ft_printf("ft_printf: [%020d]\n", -2147483648);
     printf("len: %d\n", i);
     printf("len: %d\n", j);
     i = printf("printf:    [%% *.5i 42 == |% *.5i|]\n", 4, 42);
