13820347067ad144de960efad21868664027cb59
Hiroaki-K4 <49no19@gmail.com>
2021-03-27 20:55:41 +0900

    edit

--- exam_02/inter/inter.c
+++ exam_02/inter/inter.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 18:15:09 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/27 20:51:42 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/27 20:55:39 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 
 int main(int argc, char *argv[])
 {
-    printf("str: %s\n", argv[argc - 1]);
-    return (0);
+	char *arg1;
+	char *arg2;
+	if (argc != 3)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	arg1 = argv[1];
+	arg2 = argv[2];
+	printf("arg1: %s agc2: %s\n", arg1, arg2);
+	return (0);
 }
