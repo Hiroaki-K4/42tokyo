7ae655702cbfcdbf7f71e3f221068e8e007ed22e
Hiroaki-K4 <49no19@gmail.com>
2021-03-27 20:51:05 +0900

    edit

--- exam_02/inter/inter.c
+++ exam_02/inter/inter.c
@@ -6,9 +6,11 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 18:15:09 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/27 20:50:42 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/27 20:51:01 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
