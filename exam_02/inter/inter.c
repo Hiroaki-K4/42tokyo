b0f9ac452a6d9b873543febe1eea471fb6b6e7a0
Hiroaki-K4 <49no19@gmail.com>
2021-03-30 21:45:11 +0900

    edit

--- exam_02/inter/inter.c
+++ exam_02/inter/inter.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 18:15:09 by hkubo             #+#    #+#             */
-/*   Updated: 2021/03/27 21:18:14 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/03/30 21:43:48 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,25 @@ char	*ft_strchr(const char *s, int c)
 
 	ptr_s = (unsigned char const *)s;
 	i = 0;
-	while (ptr_s[i] != '\0')
+	while (ptr_s[i])
+	{
+		if (ptr_s[i] == (unsigned char)c)
+			return (char *)(s + i);
+		i++;
+	}
+	if (ptr_s[i] == '\0' && ptr_s[i] == (unsigned char)c)
+		return (char *)(s + i);
+	return (NULL);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char const *ptr_s;
+	int					i;
+	
+	ptr_s = (unsigned char const *)s;
+	i = 0;
+	while (ptr_s[i])
 	{
 		if (ptr_s[i] == (unsigned char)c)
 			return (char *)(s + i);
