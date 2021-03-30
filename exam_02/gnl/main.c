22b9abbab002b07a5bfc62362ec0a9ca5e63fdb1
Hiroaki-K4 <49no19@gmail.com>
2021-03-30 18:36:25 +0900

    edit

---
+++ exam_02/gnl/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/28 18:47:34 by hkubo             #+#    #+#             */
+/*   Updated: 2021/03/28 18:48:36 by hkubo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+int main(void)
+{
+	int		r;
+	char	*line;
+
+	line = NULL;
+	while ((r = get_next_line(&line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+		line = NULL;
+	}
+	printf("%s", line);
+	free(line);
+	line = NULL;
+}
---
+++ ft_server/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ..
+sh git.sh
+cd ft_server
