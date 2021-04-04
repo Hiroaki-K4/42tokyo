c4b5a95dfa155677535c84e61f90af9d5ed25f72
Hiroaki-K4 <49no19@gmail.com>
2021-04-04 09:47:22 +0900

    edit

--- exam_02/gnl/get_next_line.c
+++ exam_02/gnl/get_next_line.c
@@ -6,33 +6,33 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/03 12:04:45 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/04 09:47:03 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_from_store(char *store, char **line)
-{
-	int		i;
-	int		size;
-	char	*tmp;
+// char	*get_from_store(char *store, char **line)
+// {
+// 	int		i;
+// 	int		size;
+// 	char	*tmp;
 
-	tmp = NULL;
-	i = 0;
-	size = 0;
-	while (store[i] != '\n' && store[i] != '\0')
-		i++;
-	if (!(*line = (char *)malloc(sizeof(char) * (i + 1))))
-		return (NULL);
-	ft_strlcpy(*line, store, i + 1);
-	size = (int)ft_strlen(&store[i + 1]) + 1;
-	if (!(tmp = (char *)malloc(sizeof(char) * size)))
-		return (NULL);
-	ft_strlcpy(tmp, &store[i + 1], ft_strlen(&store[i + 1]) + 1);
-	free(store);
-	return (tmp);
-}
+// 	tmp = NULL;
+// 	i = 0;
+// 	size = 0;
+// 	while (store[i] != '\n' && store[i] != '\0')
+// 		i++;
+// 	if (!(*line = (char *)malloc(sizeof(char) * (i + 1))))
+// 		return (NULL);
+// 	ft_strlcpy(*line, store, i + 1);
+// 	size = (int)ft_strlen(&store[i + 1]) + 1;
+// 	if (!(tmp = (char *)malloc(sizeof(char) * size)))
+// 		return (NULL);
+// 	ft_strlcpy(tmp, &store[i + 1], size);
+// 	free(store);
+// 	return (tmp);
+// }
 
 // char	*save_new_line(char *store, char **line, char *buf)
 // {
@@ -60,32 +60,6 @@ char	*get_from_store(char *store, char **line)
 // 	free(buf);
 // 	return (tmp);
 // }
-
-char	*save_new_line(char *store, char **line, char *buf)
-{
-	int		i;
-	int		j;
-	char	*tmp;
-
-	i = 0;
-	while (buf[i] != '\n')
-		i++;
-	if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(store) + i + 1))))
-		return (NULL);
-	ft_strlcpy(tmp, store, ft_strlen(store) + 1);
-	j = -1;
-	while (buf[++j] && j < i)
-		tmp[ft_strlen(store) + j] = buf[j];
-	tmp[ft_strlen(store) + j] = '\0';
-	if (!(*line = ft_strdup(tmp)))
-		return (NULL);
-	free(tmp);
-	if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(&buf[i + 1] + 1)))))
-		return (NULL);
-	ft_strlcpy(tmp, &buf[i + 1], ft_strlen(&buf[i + 1]) + 1);
-	free(buf);
-	return (tmp);
-}
 
 // int		read_line(int fd, char **store, char **line, int i)
 // {
