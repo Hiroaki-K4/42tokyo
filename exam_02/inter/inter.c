3b1c15ae241b60a744ed2267a004cf0034c94fe1
Hiroaki-k4 <kubo@globalwalkers.co.jp>
2021-04-06 13:24:09 +0900

    edit

--- exam_02/inter/inter.c
+++ exam_02/inter/inter.c
@@ -12,84 +12,31 @@
 
 #include <unistd.h>
 
-// char	*ft_strchr(const char *s, int c)
-// {
-// 	unsigned char const	*ptr_s;
-// 	int					i;
-
-// 	ptr_s = (unsigned char const *)s;
-// 	i = 0;
-// 	while (ptr_s[i])
-// 	{
-// 		if (ptr_s[i] == (unsigned char)c)
-// 			return (char *)(s + i);
-// 		i++;
-// 	}
-// 	if (ptr_s[i] == '\0' && ptr_s[i] == (unsigned char)c)
-// 		return (char *)(s + i);
-// 	return (NULL);
-// }
-
-char *ft_strchr(const char *str, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	unsigned const char *arg_s;
-	int i;
+	unsigned char const	*ptr_s;
+	int					i;
 
+	ptr_s = (unsigned char const *)s;
 	i = 0;
-	arg_s = (unsigned const char *)str;
-	while (arg_s[i])
+	while (ptr_s[i])
 	{
-		if (arg_s[i] == c)
-			return (char *)(str + i);
-		i++; 
+		if (ptr_s[i] == (unsigned char)c)
+			return (char *)(s + i);
+		i++;
 	}
-	if (arg_s[i] == '\0' && c == '\0')
-		return (char *)(str + i);
+	if (ptr_s[i] == '\0' && ptr_s[i] == (unsigned char)c)
+		return (char *)(s + i);
 	return (NULL);
 }
 
-// int main(int argc, char *argv[])
-// {
-// 	char	*arg1;
-// 	char	*arg2;
-// 	int		i;
-// 	int		j;
-// 	int		flag;
-
-// 	if (argc == 3)
-// 	{
-// 		arg1 = argv[1];
-// 		arg2 = argv[2];
-// 		i = 0;
-// 		while (arg1[i])
-// 		{
-// 			if (ft_strchr(arg2, arg1[i]))
-// 			{
-// 				j = i;
-// 				flag = 1;
-// 				while (j > 0)
-// 				{
-// 					if (arg1[j - 1] == arg1[i])
-// 						flag = 0;
-// 					j--;
-// 				}
-// 				if (flag == 1)
-// 					write(1, &arg1[i], 1);
-// 			}
-// 			i++;
-// 		}
-// 	}
-// 	write(1, "\n", 1);
-// 	return (0);
-// }
-
 int main(int argc, char *argv[])
 {
-	int	i;
-	int j;
-	int flag;
-	char *arg1;
-	char *arg2;
+	char	*arg1;
+	char	*arg2;
+	int		i;
+	int		j;
+	int		flag;
 
 	if (argc == 3)
 	{
@@ -98,20 +45,19 @@ int main(int argc, char *argv[])
 		i = 0;
 		while (arg1[i])
 		{
-			flag = 0;
-			if (ft_strchr(arg2, arg1[i]) != NULL)
+			if (ft_strchr(arg2, arg1[i]))
 			{
+				j = i;
 				flag = 1;
-				j = i - 1;
-				while (j >= 0)
+				while (j > 0)
 				{
-					if (arg1[j] == arg1[i])
+					if (arg1[j - 1] == arg1[i])
 						flag = 0;
 					j--;
 				}
+				if (flag == 1)
+					write(1, &arg1[i], 1);
 			}
-			if (flag == 1)
-				write(1, &arg1[i], 1);
 			i++;
 		}
 	}
