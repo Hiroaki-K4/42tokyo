b497af3ba6472aceeacd79503133c7bda999835e
Hiroaki-K4 <49no19@gmail.com>
2021-04-06 22:11:15 +0900

    edit

--- exam_02/gnl/get_next_line_utils.c
+++ exam_02/gnl/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:07:04 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/05 22:39:58 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/05 22:40:11 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,34 +84,16 @@ size_t	ft_strlen(const char *str)
 	return (count);
 }
 
-// size_t	ft_strlcpy(char *dest, const char *src, size_t n)
-// {
-// 	size_t	i;
-// 	size_t	ans;
-
-// 	ans = ft_strlen(src);
-// 	if (n == 0)
-// 		return (ans);
-// 	i = 0;
-// 	while (i < n - 1 && src[i] != '\0')
-// 	{
-// 		dest[i] = src[i];
-// 		i++;
-// 	}
-// 	dest[i] = '\0';
-// 	return (ans);
-// }
-
-size_t		ft_strlcpy(char *dest, const char *src, size_t n)
+size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
-	size_t i;
-	size_t ans;
+	size_t	i;
+	size_t	ans;
 
 	ans = ft_strlen(src);
 	if (n == 0)
 		return (ans);
 	i = 0;
-	while (i < n - 1 && src[i])
+	while (i < n - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
