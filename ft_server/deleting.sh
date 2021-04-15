b2a018dec6be7f0c8ca52c7aa689ae4042e3ac41
Hiroaki-k4 <kubo@globalwalkers.co.jp>
2021-04-15 16:45:40 +0900

    edit

--- ft_server/deleting.sh
+++
@@ -1,6 +0,0 @@
-#docker rm --force ft_server
-docker stop ft_server
-#docker rm ft_server
-docker rm $(docker ps -qa)
-docker system prune --force
-#docker rmi $(docker images -a -q)
--- ft_server/testing.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#docker build -t ft_server . --build-arg autoindex=off
-docker build -t ft_server . 
-docker run --rm --name ft_server -d -p 80:80 -p 443:443 ft_server
-docker exec -it ft_server bash
