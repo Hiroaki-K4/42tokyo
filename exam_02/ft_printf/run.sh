8b651a8106b5a301d32a913aa665e3d0724d1436
Hiroaki-K4 <49no19@gmail.com>
2021-04-18 17:49:21 +0900

    edit

--- exam_02/ft_printf/run.sh
+++ exam_02/ft_printf/run.sh
@@ -5,7 +5,6 @@
 # echo "~~printf test~~ "
 # ./a.out
 gcc -Wall -Werror -Wextra ft_printf.c main.c -o ft_printf
-./ft_printf
 ./ft_printf > yy
 cat -e yy > y
 gcc -Wall -Werror -Wextra -D REAL main.c -o printf
