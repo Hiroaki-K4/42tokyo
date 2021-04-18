b3dd022f95785fdea9d5bade9360ff5c98161948
Hiroaki-K4 <49no19@gmail.com>
2021-04-18 17:01:34 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -326,6 +326,35 @@ int	field_precision(int num, char *str, t_plist flag_list, int i)
 	return (flag_list.field);
 }
 
+int		field_no_precision_sub(long num, char *str, t_plist flag_list, int i)
+{
+	char *tmp;
+
+	if (num < 0 && flag_list.flag[0] != 1 && flag_list.flag[1] == 1)
+	{
+		write(1, "-", 1);
+		while ((flag_list.field - (int)ft_strlen(str) - (i++)) > 0)
+			write(1, "0", 1);
+		num *= -1;
+		if (!(tmp = ft_itoa(num)))
+			return (-1);
+		write(1, tmp, ft_strlen(tmp));
+		free(tmp);
+	}
+	else
+	{
+		while ((flag_list.field - (int)ft_strlen(str) - (i++)) > 0)
+		{
+			if (flag_list.flag[1] == 1)
+				write(1, "0", 1);
+			else
+				write(1, " ", 1);
+		}
+		write(1, str, ft_strlen(str));
+	}
+	return (0);
+}
+
 int	field_no_precision(int num, char *str, t_plist flag_list, int i)
 {
 	if (flag_list.flag[0] == 1)
