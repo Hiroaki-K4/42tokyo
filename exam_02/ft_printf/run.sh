9c0d59b317e5824ef5b040cc043ce1d136b8e14e
Hiroaki-K4 <49no19@gmail.com>
2021-04-18 17:46:00 +0900

    edit

--- exam_02/ft_printf/run.sh
+++ exam_02/ft_printf/run.sh
@@ -11,7 +11,8 @@ cat -e yy > y
 gcc -Wall -Werror -Wextra -D REAL main.c -o printf
 ./printf | cat -e > r
 echo "~~~~~~~~~~~diff~~~~~~~~~~~"
-diff -y --suppress-common-lines r y
+diff r y
+# diff -y --suppress-common-lines r y
 rm -rf yy y r ft_printf printf
 cd ../../
 sh git.sh
