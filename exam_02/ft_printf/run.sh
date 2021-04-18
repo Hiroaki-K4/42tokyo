dbf785244f0239f577d849ce66f4e2735854fe77
Hiroaki-K4 <49no19@gmail.com>
2021-04-18 17:36:28 +0900

    edit

--- exam_02/ft_printf/run.sh
+++ exam_02/ft_printf/run.sh
@@ -5,12 +5,13 @@
 # echo "~~printf test~~ "
 # ./a.out
 gcc -Wall -Werror -Wextra ft_printf.c main.c -o ft_printf
-./ft_printf > yy
-cat -e yy > y
-gcc -Wall -Werror -Wextra -D REAL main.c -o printf
-./printf | cat -e > r
-diff -y --suppress-common-lines r y
-rm -rf yy y r ft_printf printf
+./ft_printf
+# ./ft_printf > yy
+# cat -e yy > y
+# gcc -Wall -Werror -Wextra -D REAL main.c -o printf
+# ./printf | cat -e > r
+# diff -y --suppress-common-lines r y
+# rm -rf yy y r ft_printf printf
 cd ../../
 sh git.sh
 cd exam_02/printf
