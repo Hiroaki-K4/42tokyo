81df3360a4adede32c0f05136505944791944a53
Hiroaki-K4 <49no19@gmail.com>
2021-04-18 15:50:19 +0900

    edit

--- exam_02/ft_printf/run.sh
+++ exam_02/ft_printf/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-make re
-gcc main.c -L. -lftprintf
-echo "~~printf test~~ "
-./a.out
+# make re
+# gcc main.c -L. -lftprintf
+# echo "~~printf test~~ "
+# ./a.out
+gcc -Wall -Werror -Wextra ft_printf.c main.c -o ft_printf
 cd ../../
 sh git.sh
 cd exam_02/printf
