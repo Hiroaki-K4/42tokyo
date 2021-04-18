a30d75132cfde337c5afba89763feffc2ead9f2d
Hiroaki-K4 <49no19@gmail.com>
2021-04-18 16:12:34 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -186,6 +186,50 @@ int	print_string(va_list *ap, t_plist flag_list)
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~print int~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+static	int		digit_size(int number)
+{
+	int		count;
+
+	count = 0;
+	if (number <= 0)
+		count++;
+	while (number != 0)
+	{
+		number = number / 10;
+		count++;
+	}
+	return (count);
+}
+
+char			*ft_itoa(int n)
+{
+	int		len;
+	char	flag;
+	char	*ans;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	len = digit_size(n);
+	if (!(ans = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	flag = '+';
+	if (n < 0)
+	{
+		n = n * (-1);
+		flag = '-';
+	}
+	ans[len] = '\0';
+	len--;
+	while (len >= 0)
+	{
+		ans[len--] = '0' + (n % 10);
+		n = n / 10;
+	}
+	if (flag == '-')
+		ans[0] = '-';
+	return (ans);
+}
+
 int	print_int(va_list *ap, t_plist flag_list)
 {
 	int		num;
