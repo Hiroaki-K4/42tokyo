9b158b1acced48e7c83bdedd5d79f528f0a3944c
Hiroaki-K4 <49no19@gmail.com>
2021-04-22 22:22:44 +0900

    edit

--- cub3D/main.c
+++ cub3D/main.c
@@ -6,9 +6,11 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 22:14:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/22 22:14:56 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/22 22:21:41 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line/get_next_line.h"
 
 int	main(int argc, char *argv[])
 {
