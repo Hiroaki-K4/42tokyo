0bc7f9378197e108713c236aca4d53e58d029cb8
Hiroaki-K4 <49no19@gmail.com>
2021-04-22 09:20:02 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/22 09:12:59 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/22 09:20:00 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -293,7 +293,16 @@ int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 	if (flag_list.precision == 0 && num == 0)
 	{
 		free(str_num);
-		return (pre_arg_zero(flag_list));
+		if (flag_list.field != -1)
+		{
+			while (flag_list.field - len > 0)
+			{
+				write(1, " ", 1);
+				len++;
+			}
+		}
+		return (len);
+		// return (pre_arg_zero(flag_list));
 	}
 	if (flag_list.field > (int)ft_strlen(str_num))
 	{
