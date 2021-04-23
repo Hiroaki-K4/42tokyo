e8f70a1f6ef5dfa75a08ab7fd3f9f221b394c8e4
Hiroaki-K4 <49no19@gmail.com>
2021-04-23 21:29:45 +0900

    edit

--- cub3D/main.c
+++ cub3D/main.c
@@ -6,14 +6,22 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 22:14:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/23 21:02:57 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/23 21:16:38 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line/get_next_line.h"
+#include "libft/libft.h"
+#include <stdio.h>
 
 int	main(int argc, char *argv[])
 {
+	char *str;
+	int i;
+
+	str = "aaa";
+	i = ft_strlen(str);
 	argv[0] = "aa";
+	printf("len: %d\n", i);
 	return (argc);
 }
