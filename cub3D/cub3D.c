1eb3a35e005a3765094159ab0cce8fc9611f8e5f
Hiroaki-K4 <49no19@gmail.com>
2021-04-23 21:48:05 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 22:14:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/23 21:45:57 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/23 21:47:57 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 int	main(int argc, char *argv[])
 {
-	char *str;
-	int i;
+	char *cub_path;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (-1);
 	}
-	str = "aaa";
-	i = ft_strlen(str);
+	cub_path = argv[1];
+	printf("path: %s\n", cub_path);
 	argv[0] = "aa";
-	printf("len: %d\n", i);
 	return (argc);
 }
