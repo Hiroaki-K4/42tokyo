1c4ffce4dca7bdfdba4ea27eef92e538b33aaa7b
Hiroaki-K4 <49no19@gmail.com>
2021-04-24 11:07:59 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,11 +6,27 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 22:14:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/24 10:56:33 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/24 11:04:50 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+typedef struct  s_data {
+    void        *img;
+    char        *addr;
+    int         bits_per_pixel;
+    int         line_length;
+    int         endian;
+}               t_data;
+
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
 
 int	main(int argc, char *argv[])
 {
@@ -20,7 +36,8 @@ int	main(int argc, char *argv[])
 	char *line;
 	char *cub_path;
 	void    *mlx;
-    void    *img;
+	void    *mlx_win;
+	t_data  img;
 
 	if (argc != 2)
 	{
@@ -41,6 +58,12 @@ int	main(int argc, char *argv[])
 	printf("path: %s\n", cub_path);
 	argv[0] = "aa";
 	mlx = mlx_init();
-	img = mlx_new_image(mlx, 1920, 1080);
-	return (argc);
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+    img.img = mlx_new_image(mlx, 1920, 1080);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+                                &img.endian);
+    my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    mlx_loop(mlx);
+	// return (argc);
 }
