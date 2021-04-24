766b5ea752c50883394910d01335402a3352860a
Hiroaki-K4 <49no19@gmail.com>
2021-04-24 14:36:45 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 22:14:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/24 11:04:50 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/24 14:36:40 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,22 @@ typedef struct  s_data {
     int         line_length;
     int         endian;
 }               t_data;
+
+typedef struct  s_vars {
+    void        *mlx;
+    void        *win;
+}               t_vars;
+
+int             key_hook(int keycode, t_vars *vars)
+{
+    printf("Hello from key_hook!\n");
+}
+
+int             close(int keycode, t_vars *vars)
+{
+    mlx_destroy_window(vars->mlx, vars->win);
+}
+
 
 void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -38,6 +54,7 @@ int	main(int argc, char *argv[])
 	void    *mlx;
 	void    *mlx_win;
 	t_data  img;
+	t_vars      vars;
 
 	if (argc != 2)
 	{
@@ -57,13 +74,24 @@ int	main(int argc, char *argv[])
 	printf("len: %i\n", i);
 	printf("path: %s\n", cub_path);
 	argv[0] = "aa";
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-    img.img = mlx_new_image(mlx, 1920, 1080);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-                                &img.endian);
-    my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-    mlx_loop(mlx);
+	// mlx = mlx_init();
+	// mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+    // img.img = mlx_new_image(mlx, 1920, 1080);
+    // img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+    //                             &img.endian);
+    // my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	// my_mlx_pixel_put(&img, 10, 10, 0x00FFFF00);
+    // mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    // mlx_loop(mlx);
+	
+	// vars.mlx = mlx_init();
+    // vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+    // mlx_key_hook(vars.win, key_hook, &vars);
+    // mlx_loop(vars.mlx);
+	
+	vars.mlx = mlx_init();
+    vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+    mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+    mlx_loop(vars.mlx);
 	// return (argc);
 }
