4aa258ae542eeaddc2309cf49bf6c69cdd9f05ab
Hiroaki-K4 <49no19@gmail.com>
2021-04-25 11:59:36 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/25 11:58:36 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/25 11:59:34 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -166,6 +166,7 @@ char *save_new_line(char *store, char **line, char *buf)
 	while (buf[++j] && j < i)
 		tmp[ft_strlen(store) + j] = buf[j];
 	tmp[ft_strlen(store) + j] = '\0';
+	free(store);
 	if (!(*line = ft_strdup(tmp)))
 		return (NULL);
 	free(tmp);
