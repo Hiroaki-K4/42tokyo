d69b67d762f7768cab00b7d83a8ecc477a6b86c1
Hiroaki-K4 <49no19@gmail.com>
2021-04-25 12:05:30 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/25 11:59:34 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/25 12:05:28 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -155,7 +155,7 @@ char *save_new_line(char *store, char **line, char *buf)
 	int i;
 	int j;
 	char *tmp;
-	
+
 	i = 0;
 	while (buf[i] != '\n')
 		i++;
