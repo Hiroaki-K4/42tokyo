e1900267ea7cb18193acd2bee3998080bef969de
Hiroaki-K4 <49no19@gmail.com>
2021-04-25 22:26:44 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/25 22:26:18 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/25 22:26:43 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,23 +83,23 @@ size_t	ft_strlen(const char *str)
 	return (count);
 }
 
-// size_t	ft_strlcpy(char *dest, const char *src, size_t n)
-// {
-// 	size_t	i;
-// 	size_t	ans;
+size_t	ft_strlcpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+	size_t	ans;
 
-// 	ans = ft_strlen(src);
-// 	if (n == 0)
-// 		return (ans);
-// 	i = 0;
-// 	while (i < n - 1 && src[i] != '\0')
-// 	{
-// 		dest[i] = src[i];
-// 		i++;
-// 	}
-// 	dest[i] = '\0';
-// 	return (ans);
-// }
+	ans = ft_strlen(src);
+	if (n == 0)
+		return (ans);
+	i = 0;
+	while (i < n - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ans);
+}
 
 // char *get_from_store(char *store, char **line)
 // {
