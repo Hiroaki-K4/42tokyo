8de8d15904004b9fd8e8b304af111732a10bdf65
Hiroaki-K4 <49no19@gmail.com>
2021-04-25 10:20:43 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/25 10:20:04 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/25 10:20:41 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -150,38 +150,38 @@ char	*save_new_line(char *store, char **line, char *buf)
 	return (tmp);
 }
 
-// int		read_line(int fd, char **store, char **line)
-// {
-// 	int		i;
-// 	char	*buf;
-// 	char	*tmp;
-// 	int		buffer_size;
+int		read_line(int fd, char **store, char **line)
+{
+	int		i;
+	char	*buf;
+	char	*tmp;
+	int		buffer_size;
 
-// 	buffer_size = 128;
-// 	if (!(buf = (char *)malloc(sizeof(char) * ((size_t)buffer_size + 1))))
-// 		return (-1);
-// 	i = 1;
-// 	while (i > 0)
-// 	{
-// 		if ((i = read(fd, buf, buffer_size)) == -1)
-// 			return (-1);
-// 		buf[i] = '\0';
-// 		if (ft_strchr(buf, '\n') != NULL)
-// 		{
-// 			if (!(store[fd] = save_new_line(store[fd], line, buf)))
-// 				return (-1);
-// 			return (1);
-// 		}
-// 		if (!(tmp = ft_strjoin(store[fd], buf)))
-// 			return (-1);
-// 		free(store[fd]);
-// 		store[fd] = tmp;
-// 	}
-// 	if (!(*line = ft_strdup(store[fd])))
-// 		return (-1);
-// 	free(buf);
-// 	return (0);
-// }
+	buffer_size = 128;
+	if (!(buf = (char *)malloc(sizeof(char) * ((size_t)buffer_size + 1))))
+		return (-1);
+	i = 1;
+	while (i > 0)
+	{
+		if ((i = read(fd, buf, buffer_size)) == -1)
+			return (-1);
+		buf[i] = '\0';
+		if (ft_strchr(buf, '\n') != NULL)
+		{
+			if (!(store[fd] = save_new_line(store[fd], line, buf)))
+				return (-1);
+			return (1);
+		}
+		if (!(tmp = ft_strjoin(store[fd], buf)))
+			return (-1);
+		free(store[fd]);
+		store[fd] = tmp;
+	}
+	if (!(*line = ft_strdup(store[fd])))
+		return (-1);
+	free(buf);
+	return (0);
+}
 
 
 
