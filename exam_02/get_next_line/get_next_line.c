4e0d5ad519178916d2d73469997a7ee6848210ce
Hiroaki-K4 <49no19@gmail.com>
2021-04-25 22:03:33 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/25 22:02:31 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/25 22:03:30 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -209,7 +209,7 @@ int read_line(int fd, char **store, char **line)
 		free(store[fd]);
 		store[fd] = tmp;
 	}
-	if (!(line = ft_strdup(store[fd])))
+	if (!(*line = ft_strdup(store[fd])))
 		return (-1);
 	free(buf);
 	return (0);
