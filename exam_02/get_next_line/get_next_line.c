77f5bb654c64ea0b03a80567feb0c0fbeb786669
Hiroaki-K4 <49no19@gmail.com>
2021-04-26 21:50:04 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/26 21:48:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/26 21:50:02 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -251,7 +251,7 @@ char *save_new_line(char *store, char **line, char *buf)
 	tmp[ft_strlen(store) + j] = '\0';
 	if (!(*line = ft_strdup(tmp)))
 		return (NULL);
-	free(store);
+	// free(store);
 	free(tmp);
 	if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(&buf[i + 1]) + 1))))
 		return (NULL);
