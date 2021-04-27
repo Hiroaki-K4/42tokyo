aa2a5c38afef02e9675c7c9aefaddfbadbd407a8
Hiroaki-K4 <49no19@gmail.com>
2021-04-27 21:34:43 +0900

    edit

--- exam_02/get_next_line/get_next_line.h
+++ exam_02/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/27 21:24:08 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/27 21:33:11 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,5 @@ char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
