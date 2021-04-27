c9753aec31b3c1c0b7a97a4673c3f2b515a2eaed
Hiroaki-K4 <49no19@gmail.com>
2021-04-27 10:01:22 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/27 10:00:13 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/27 10:01:20 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -259,6 +259,7 @@ char *save_new_line(char *store, char **line, char *buf)
 		return (NULL);
 	ft_strlcpy(tmp, &buf[i + 1], ft_strlen(&buf[i + 1]) + 1);
 	free(buf);
+	printf("tmp: %s\n", tmp);
 	return (tmp);
 }
 
