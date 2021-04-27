aedd0231099facff4bb670911dbf56000f2d6169
Hiroaki-K4 <49no19@gmail.com>
2021-04-27 12:48:40 +0900

    edit

--- exam_02/union/union.c
+++ exam_02/union/union.c
@@ -6,36 +6,18 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 18:17:24 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/27 12:47:48 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/27 12:48:38 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-// char	*ft_strchr(const char *s, int c)
-// {
-// 	unsigned char const	*ptr_s;
-// 	int					i;
-
-// 	ptr_s = (unsigned char const *)s;
-// 	i = 0;
-// 	while (ptr_s[i])
-// 	{
-// 		if (ptr_s[i] == (unsigned char)c)
-// 			return (char *)(s + i);
-// 		i++;
-// 	}
-// 	if (ptr_s[i] == '\0' && ptr_s[i] == (unsigned char)c)
-// 		return (char *)(s + i);
-// 	return (NULL);
-// }
-
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	unsigned const char *ptr_s;
-	int i;
+	unsigned char const	*ptr_s;
+	int					i;
 
-	ptr_s = (unsigned const char *)s;
+	ptr_s = (unsigned char const *)s;
 	i = 0;
 	while (ptr_s[i])
 	{
@@ -48,64 +30,14 @@ char *ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-// int main(int argc, char *argv[])
-// {
-// 	int		i;
-// 	int		j;
-// 	int		flag;
-// 	char	*arg1;
-// 	char	*arg2;
-
-// 	if (argc == 3)
-// 	{
-// 		arg1 = argv[1];
-// 		arg2 = argv[2];
-// 		i = 0;
-// 		while (arg1[i])
-// 		{
-// 			flag = 1;
-// 			j = i;
-// 			while (j > 0)
-// 			{
-// 				if (arg1[j - 1] == arg1[i])
-// 					flag = 0;
-// 				j--;
-// 			}
-// 			if (flag == 1)
-// 				write(1, &arg1[i], 1);
-// 			i++;
-// 		}
-// 		i = 0;
-// 		while (arg2[i])
-// 		{
-// 			if (ft_strchr(arg1, arg2[i]) == NULL)
-// 			{
-// 				flag = 1;
-// 				j = i;
-// 				while (j > 0)
-// 				{
-// 					if (arg2[j - 1] == arg2[i])
-// 						flag = 0;
-// 					j--;
-// 				}
-// 				if (flag == 1)
-// 					write(1, &arg2[i], 1);
-// 			}
-// 			i++;
-// 		}
-// 	}
-// 	write(1, "\n", 1);
-// 	return (0);
-// }
-
 int main(int argc, char *argv[])
 {
-	int i;
-	int j;
-	int flag;
-	char *arg1;
-	char *arg2;
-	
+	int		i;
+	int		j;
+	int		flag;
+	char	*arg1;
+	char	*arg2;
+
 	if (argc == 3)
 	{
 		arg1 = argv[1];
@@ -113,8 +45,8 @@ int main(int argc, char *argv[])
 		i = 0;
 		while (arg1[i])
 		{
-			j = i;
 			flag = 1;
+			j = i;
 			while (j > 0)
 			{
 				if (arg1[j - 1] == arg1[i])
@@ -130,8 +62,8 @@ int main(int argc, char *argv[])
 		{
 			if (ft_strchr(arg1, arg2[i]) == NULL)
 			{
-				j = i;
 				flag = 1;
+				j = i;
 				while (j > 0)
 				{
 					if (arg2[j - 1] == arg2[i])
