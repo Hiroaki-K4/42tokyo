9268451fd1e3624a23cb4295dc60ab01a5c1f12c
Hiroaki-K4 <49no19@gmail.com>
2021-04-27 12:39:36 +0900

    edit

--- exam_02/union/union.c
+++ exam_02/union/union.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 18:17:24 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/27 12:28:14 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/04/27 12:39:35 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,13 +48,63 @@ char *ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
+// int main(int argc, char *argv[])
+// {
+// 	int		i;
+// 	int		j;
+// 	int		flag;
+// 	char	*arg1;
+// 	char	*arg2;
+
+// 	if (argc == 3)
+// 	{
+// 		arg1 = argv[1];
+// 		arg2 = argv[2];
+// 		i = 0;
+// 		while (arg1[i])
+// 		{
+// 			flag = 1;
+// 			j = i;
+// 			while (j > 0)
+// 			{
+// 				if (arg1[j - 1] == arg1[i])
+// 					flag = 0;
+// 				j--;
+// 			}
+// 			if (flag == 1)
+// 				write(1, &arg1[i], 1);
+// 			i++;
+// 		}
+// 		i = 0;
+// 		while (arg2[i])
+// 		{
+// 			if (ft_strchr(arg1, arg2[i]) == NULL)
+// 			{
+// 				flag = 1;
+// 				j = i;
+// 				while (j > 0)
+// 				{
+// 					if (arg2[j - 1] == arg2[i])
+// 						flag = 0;
+// 					j--;
+// 				}
+// 				if (flag == 1)
+// 					write(1, &arg2[i], 1);
+// 			}
+// 			i++;
+// 		}
+// 	}
+// 	write(1, "\n", 1);
+// 	return (0);
+// }
+
 int main(int argc, char *argv[])
 {
-	int		i;
-	int		j;
-	int		flag;
-	char	*arg1;
-	char	*arg2;
+	int i;
+	int j;
+	int flag = 1;
+	char *arg1;
+	char *arg2;
 
 	if (argc == 3)
 	{
