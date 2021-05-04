8c57f3e711abfbac5911fcdd1b9c3f597ca6942c
Hiroaki-K4 <49no19@gmail.com>
2021-05-04 15:12:01 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/04 15:11:25 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/04 15:11:59 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -329,7 +329,7 @@ int get_next_line(int fd, char **line)
     }
     else
     {
-        if (!(store[fd] = get__from_store(store[fd], line)))
+        if (!(store[fd] = get_from_store(store[fd], line)))
             return (-1);
         return (1);
     }
