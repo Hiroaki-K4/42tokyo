3ab770da9b27f1074b1e704475f028e1e69bf476
Hiroaki-K4 <49no19@gmail.com>
2021-05-04 15:05:55 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/04 14:56:10 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/04 15:05:52 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -210,6 +210,55 @@
 //     }
 //     return (0);
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+char *save_new_line(char *store, char **line, char *buf)
+{
+    int i;
+    int j;
+    char *tmp;
+    
+    i = 0;
+    while (buf[i] && buf[i] != '\n')
+        i++;
+    if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(store) + i + 1))))
+        return (NULL);
+    ft_strlcpy(tmp, store, ft_strlen(store) + 1);
+    j = -1;
+    while (buf[++j] && j < i)
+        tmp[ft_strlen(store) + j] = buf[j];
+    tmp[ft_strlen(store) + j] = '\0';
+    free(store[fd]);
+    if (!(*line = ft_strdup(tmp)))
+        return (NULL);
+    free(tmp);
+    if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(&buf[i + 1]) + 1))))
+        return (NULL);
+    ft_strlcpy(tmp, &buf[i + 1], ft_strlen(&buf[i + 1]) + 1);
+    free(buf);
+    return (tmp);
+}
 
 int read_line(int fd, char **store, char **line)
 {
