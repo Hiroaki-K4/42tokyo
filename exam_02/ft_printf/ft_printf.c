ac0c47e80ad90bce90fbb128519281bc00ba2225
Hiroaki-K4 <49no19@gmail.com>
2021-05-05 14:20:53 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/05 14:20:38 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/05 14:20:51 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -429,27 +429,6 @@ int		ft_printf_per(const char *arg, int *i, va_list *ap)
 // 		k = print_string(ap, flag_list);
 // }
 
-int ft_printf_per(const char *arg, int *i, va_list *ap)
-{
-	int k;
-	int num;
-	int keta;
-	unsigned int hex_num;
-	char *str_num;
-	t_plist flag_list;
-
-	(*i)++;
-	flag_list.field = str_to_num(&arg[*i], i, ap, 0);
-	flag_list.precision = -1;
-	if (arg[*i] == '.')
-	{
-		(*i)++;
-		flag_list.precision = str_to_num(&arg[*i], i, ap, 1);
-	}
-	flag_list.format = ft_strchr_place("sdx", &arg[*i], i);
-	if (flag_list.format == 0)
-		k = print_string(ap, flag_list);
-}
 
 // int		ft_printf_str(const char *arg, int *i)
 // {
