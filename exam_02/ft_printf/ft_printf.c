e0d9df62b1e7f964c57a07dd30847a13f1794708
Hiroaki-K4 <49no19@gmail.com>
2021-05-05 11:51:25 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/05 11:49:47 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/05 11:51:24 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -470,6 +470,7 @@ int ft_printf(const char *arg, ...)
 	int j;
 	int print_len;
 
+	va_start(ap, arg);
 	i = 0;
 	print_len = 0;
 	if (arg == NULL)
