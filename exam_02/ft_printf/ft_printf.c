8af25860ce58b4dbde9fdc58fa3648852977be48
Hiroaki-K4 <49no19@gmail.com>
2021-05-08 14:09:28 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/08 11:53:48 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/08 14:09:26 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -346,10 +346,29 @@ int	print_string(va_list *ap, t_plist flag_list)
 	return (len);
 }
 
-int		str_to_num(const char *arg, int *i, va_list *ap, int flag)
+// int		str_to_num(const char *arg, int *i, va_list *ap, int flag)
+// {
+// 	int	num;
+// 	int	j;
+
+// 	if (ft_isdigit(arg[0]) == 0 && flag == 1)
+// 		return (0);
+// 	num = ft_atoi(arg);
+// 	if (num >= 0)
+// 	{
+// 		j = 0;
+// 		while (ft_isdigit(arg[j++]))
+// 			(*i)++;
+// 	}
+// 	else
+// 		num = -1;
+// 	return (num);
+// }
+
+int str_to_num(const char *arg, int *i, va_list *ap, int flag)
 {
-	int	num;
-	int	j;
+	int num;
+	int j;
 
 	if (ft_isdigit(arg[0]) == 0 && flag == 1)
 		return (0);
