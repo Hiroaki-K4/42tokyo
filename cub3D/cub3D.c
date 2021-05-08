89816a1b330cad4a305ebd06646e5b344eed15c5
Hiroaki-K4 <49no19@gmail.com>
2021-05-08 21:37:54 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 12:12:25 by yohan             #+#    #+#             */
-/*   Updated: 2021/05/08 21:36:33 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/08 21:37:25 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@
 #define X_EVENT_KEY_EXIT	17
 #define mapWidth 24
 #define mapHeight 24
-#define width 640
-#define height 480
+#define width 1080
+#define height 1920
 #define K_W 119
 #define K_S 115
 #define K_D 100
