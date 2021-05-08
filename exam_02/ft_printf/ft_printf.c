bb768103e269116a93738f573b5142ecbfce16d1
Hiroaki-K4 <49no19@gmail.com>
2021-05-08 15:05:14 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/08 15:04:51 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/08 15:05:12 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -381,22 +381,22 @@ int print_string(va_list *ap, t_plist flag_list)
 			len = (int)ft_strlen(str) + i;
 		}
 	}
-	// else
-	// {
-	// 	if (flag_list.precision != -1)
-	// 	{
-	// 		while (len < flag_list.precision && str[len])
-	// 		{
-	// 			write(1, &str[len], 1);
-	// 			len++;
-	// 		}
-	// 	}
-	// 	else
-	// 	{
-	// 		write(1, str, ft_strlen(str));
-	// 		len = ft_strlen(str);
-	// 	}
-	// }
+	else
+	{
+		if (flag_list.precision != -1)
+		{
+			while (len < flag_list.precision && str[len])
+			{
+				write(1, &str[len], 1);
+				len++;
+			}
+		}
+		else
+		{
+			write(1, str, ft_strlen(str));
+			len = ft_strlen(str);
+		}
+	}
 	return (len);
 }
 
