6349115374f574c97de9f4d2d976c5eba7eca7a4
Hiroaki-K4 <49no19@gmail.com>
2021-05-08 11:53:40 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/08 11:53:07 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/08 11:53:38 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -440,7 +440,7 @@ int ft_printf_per(const char *arg, int *i, va_list *ap)
 		hex_num = va_arg(*ap, unsigned int);
 		if (!(str_num = ft_itoa_hex(hex_num, "0123456789abcdfe")))
 			return (-1);
-		len = print_digit(flag_list, str_num, hex_num, (int)ft_strlen(hex_num));
+		len = print_digit(flag_list, str_num, hex_num, (int)ft_strlen(str_num));
 	}
 	return (len);
 }
