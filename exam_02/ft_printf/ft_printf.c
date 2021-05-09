f38c0c38441b0e789c11c9ad4095318a7cc957f1
Hiroaki-K4 <49no19@gmail.com>
2021-05-09 14:26:55 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/09 14:23:36 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/09 14:26:49 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -369,7 +369,7 @@ int str_to_num(const char *arg, int *i, va_list *ap, int flag)
 {
 	int num;
 	int j;
-	
+
 	if (ft_isdigit(arg[0]) == 0 && flag == 1)
 		return (0);
 	num = ft_atoi(arg);
