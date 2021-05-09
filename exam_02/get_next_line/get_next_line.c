400f113563cf4803ce3d8fe2341349d38ebd41ad
Hiroaki-K4 <49no19@gmail.com>
2021-05-09 10:29:50 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/09 10:26:33 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/09 10:29:47 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -210,6 +210,35 @@
 //     }
 //     return (0);
 // }
+
+size_t ft_strlen(const char *src)
+{
+    size_t i;
+    
+    while (src[i])
+        i++;
+    return (i);
+}
+
+
+char *ft_strdup(const char *src)
+{
+    size_t i;
+    char *dst;
+
+    if (!src)
+        return (NULL);
+    if (!(dst = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1))))
+        return (NULL);
+    i = 0;
+    while (src[i])
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    retn (dst);
+}
 
 char *get_from_store(char *store, char **line)
 {
