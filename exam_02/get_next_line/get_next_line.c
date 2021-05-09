f3cf5995184d823abb5306ae6481c3cfa43ecf3e
Hiroaki-K4 <49no19@gmail.com>
2021-05-09 10:46:28 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/09 10:35:24 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/09 10:46:26 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -220,6 +220,24 @@ size_t ft_strlen(const char *src)
     return (i);
 }
 
+char *ft_strchr(const char *src, int c)
+{
+    unsigned const char *ptr_s;
+    int i;
+
+    ptr_s = (unsigned const char *)src;
+    i = 0;
+    while (ptr_s[i])
+    {
+        if (ptr_s[i] == (unsigned char)c)
+            return (char *)(src + i);
+        i++;
+    }
+    if (ptr_s[i] == '\0' && ptr_s[i] == (unsigned char)c)
+        return (char *)(src + i);
+    return (NULL);
+}
+
 size_t ft_strlcpy(char *dst, const char *src, size_t n)
 {
     size_t ans;
@@ -363,7 +381,7 @@ int read_line(int fd, char **store, char **line)
 
 int get_next_line(int fd, char **line)
 {
-    int i;
+    // int i;
     static char *store[256];
 
     *line = NULL;
@@ -371,15 +389,15 @@ int get_next_line(int fd, char **line)
         return (-1);
     if (store[fd] == NULL)
         store[fd] = ft_strdup("");
-    if (ft_strchr(store[fd], '\n') == NULL)
-    {
-        if ((i = read_line(fd, store, line)) == 0)
-        {
-            free(store[fd]);
-            store[fd] = NULL;
-        }
-        return (i);
-    }
+    // if (ft_strchr(store[fd], '\n') == NULL)
+    // {
+    //     if ((i = read_line(fd, store, line)) == 0)
+    //     {
+    //         free(store[fd]);
+    //         store[fd] = NULL;
+    //     }
+    //     return (i);
+    // }
     else
     {
         if (!(store[fd] = get_from_store(store[fd], line)))
