b1e1eb4b394fc242799022d8b4f1bee84e1ceefc
Hiroaki-K4 <49no19@gmail.com>
2021-05-11 09:22:06 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/11 09:20:55 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/11 09:22:04 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -210,6 +210,18 @@
 //     }
 //     return (0);
 // }
+
+size_t ft_strlen(const char *src)
+{
+    size_t i;
+
+    i = 0;
+    while (src[i])
+        i++;
+    return (i);
+}
+
+// char *ft_str
 
 char *get_from_store(char *store, char **line)
 {
