e7834b80510df387e584ffc572f026ed37f7e16c
Hiroaki-K4 <49no19@gmail.com>
2021-05-13 22:29:02 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/13 22:28:17 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/13 22:28:59 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -602,7 +602,8 @@ int line_check(char **line, t_cub *cub_list)
 			map_line[i] = line[i];
 			i++;
 		}
-		printf("line: %s\n", map_line);
+		printf("line: %s\n", line);
+		printf("map_line: %s\n", map_line);
 	}
 	// printf("line: %s\n", line);
 	// printf("dst: %s\n", line_split[0]);
