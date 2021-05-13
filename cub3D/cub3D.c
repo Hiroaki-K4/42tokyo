bebf8b996aac2c31fde377d02462de4788108864
Hiroaki-k4 <kubo@globalwalkers.co.jp>
2021-05-13 18:36:57 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -547,12 +547,12 @@ int line_check(char **line, t_cub *cub_list)
 	{
 		if (!(rgb_split = ft_split(line, ',')))
 			return (-1);
-		cub_list->rgb.red = rgb_split[0];
-		cub_list->rgb.green = rgb_split[1];
-		cub_list->rgb.blue = rgb_split[2];
-		printf("Floor_r: %s\n", cub_list->rgb.red);
-		printf("Floor_g: %s\n", cub_list->rgb.green);
-		printf("Floor_b: %s\n", cub_list->rgb.blue);
+		cub_list->rgb.red = ft_atoi(rgb_split[0]);
+		cub_list->rgb.green = ft_atoi(rgb_split[1]);
+		cub_list->rgb.blue = ft_atoi(rgb_split[2]);
+		printf("Floor_r: %d\n", cub_list->rgb.red);
+		printf("Floor_g: %d\n", cub_list->rgb.green);
+		printf("Floor_b: %d\n", cub_list->rgb.blue);
 	}
 	else if (i = ft_strcmp("C", line_split[0]) == 0)
 	{
