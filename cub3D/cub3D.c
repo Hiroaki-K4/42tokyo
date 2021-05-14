13e0e6a665eca5105935708a1fb527d6f5cdf1f0
Hiroaki-K4 <49no19@gmail.com>
2021-05-14 16:07:18 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/13 23:00:36 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/14 16:07:15 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -594,7 +594,7 @@ int line_check(char **line, t_cub *cub_list)
 	{
 		if (!(map_line = ft_strdup(line)))
 			return (-1);
-		printf("befo: %s\n", map_line);
+		// printf("befo: %s\n", map_line);
 		i = 0;
 		while (map_line[i])
 		{
