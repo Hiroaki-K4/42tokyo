9354495b5b4324f94fe541d3fdf35c68690a9f55
Hiroaki-K4 <49no19@gmail.com>
2021-05-15 17:01:36 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/15 17:01:12 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/15 17:01:34 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -615,9 +615,9 @@ int line_check(char **line, t_info *info)
 		if (!(tmp[i] = (char **)malloc(sizeof(char) * (ft_strlen(map_line) + 1))))
 				return (-1);
 		ft_strlcpy(tmp[i], map_line, ft_strlen(map_line) + 1);
-		// free(info->cub_list.map);
+		free(info->cub_list.map);
 		info->cub_list.map = tmp;
-		printf("line: %s\n", info->cub_list.map[info->cub_list.count - 1]);
+		// printf("line: %s\n", info->cub_list.map[info->cub_list.count - 1]);
 		// info->cub_list.count++;
 		// i = 0;
 		// while (map_line[i])
