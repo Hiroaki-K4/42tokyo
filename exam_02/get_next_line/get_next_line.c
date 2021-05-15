45c45552e0a556b9bc4e56de44f58c603d1c0b1d
Hiroaki-K4 <49no19@gmail.com>
2021-05-15 11:20:45 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/15 11:10:06 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/15 11:20:44 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -212,7 +212,37 @@
 // }
 
 
-
+int read_line(int fd, char **store, char **line)
+{
+    int buffer_size;
+    int i;
+    char *buf;
+    char *tmp;
+    
+    buffer_size = 128;
+    if (!(buf = (char *)malloc(sizeof(char) * (buffer_size + 1))))
+        return (-1);
+    i = 1;
+    while (i > 0)
+    {
+        if ((i = read(fd, buf, buffer_size)) == -1)
+            return (-1);
+        buf[i] = '\0';
+        if (ft_strchr(buf, '\n') != NULL)
+        {
+            if (!(store[fd] = save_new_line(store[fd], line, buf)))
+                return (-1);
+            return (1);
+        }
+        if (!(tmp = ft_strjoin(store[fd], buf)))
+            return (-1);
+        free(store[fd]);
+        store[fd] = tmp;
+    }
+    if (!(*line = ft_strdup(store[fd])))
+        return (-1);
+    return (0);
+}
 
 int get_next_line(int fd, char **line)
 {
@@ -224,7 +254,6 @@ int get_next_line(int fd, char **line)
         return (-1);
     if (store[fd] == NULL)
         store[fd] = ft_strdup("");
-    i = 1;
     if (ft_strchr(store[fd], '\n') == NULL)
     {
         if ((i = read_line(fd, store, line)) == 0)
