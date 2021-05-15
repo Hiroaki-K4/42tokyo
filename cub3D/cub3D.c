58e93db246b8101eb050f10ffbe29b5fd9e77f64
Hiroaki-K4 <49no19@gmail.com>
2021-05-15 17:23:02 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/15 17:22:28 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/15 17:23:00 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -617,7 +617,7 @@ int line_check(char **line, t_info *info)
 		while (i < info->cub_list.count && 1 < info->cub_list.count)
 		{
 			// free(info->cub_list.map[i]);
-			printf("befo: %s\n", info->cub_list.map[info->cub_list.count - 1]);
+			printf("befo: %s\n", info->cub_list.map[i]);
 			i++;
 		}
 		free(info->cub_list.map);
