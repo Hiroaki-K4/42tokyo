c08d885157f92404d0348aac2e751f2bb22ac52e
Hiroaki-K4 <49no19@gmail.com>
2021-05-15 22:02:49 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/15 22:01:56 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/15 22:02:47 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -311,6 +311,7 @@ void	calc(t_info *info)
 			}
 			//Check if ray has hit a wall
 			printf("mapx: %d mapy: %d\n", mapX, mapY);
+			printf("point: %d\n", info->cub_list.map_matrix[mapX][mapY]);
 			if (info->cub_list.map_matrix[mapX][mapY] > 0) hit = 1;
 			// if (info->cub_list.map_matrix[mapY][mapX] > 0) hit = 1;
 		}
