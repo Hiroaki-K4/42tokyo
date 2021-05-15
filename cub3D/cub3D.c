f64d4ddd9dc0e161592a2c12786b1dc754ce448b
Hiroaki-K4 <49no19@gmail.com>
2021-05-15 16:36:59 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/15 16:36:22 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/15 16:36:57 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -599,7 +599,7 @@ int line_check(char **line, t_info *info)
 		info->cub_list.count++;
 		if (!(map_line = ft_strdup(line)))
 			return (-1);
-		info->cub_list.size += ft_strlen(map_line) + 1;
+		// info->cub_list.size += ft_strlen(map_line) + 1;
 		tmp = info->cub_list.map;
 		free(info->cub_list.map);
 		if (!(info->cub_list.map = (char **)malloc(sizeof(char *) * (info->cub_list.count))))
