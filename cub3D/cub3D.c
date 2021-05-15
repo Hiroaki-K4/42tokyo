592b7edc1bd0324ecd475f44e357d26d672a480a
Hiroaki-K4 <49no19@gmail.com>
2021-05-15 20:52:53 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/15 20:52:22 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/15 20:52:51 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -694,7 +694,10 @@ int convert_int_matrix(t_info *info)
 			{
 				printf("word: %c\n", info->cub_list.map[i][j]);
 				if (info->cub_list.map[i][j] == ' ')
+				{
 					info->cub_list.map_matrix[i][j] = 3;
+					printf("under1\n");
+				}
 				else
 				{
 					info->cub_list.map_matrix[i][j] = ft_atoi(info->cub_list.map[i][j]);
