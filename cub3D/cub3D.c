d551cb30e25ef2fcb6d3022c45a6f18bf04eabe4
Hiroaki-K4 <49no19@gmail.com>
2021-05-15 20:55:49 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/15 20:54:10 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/15 20:55:46 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -697,7 +697,7 @@ int convert_int_matrix(t_info *info)
 					info->cub_list.map_matrix[i][j] = 3;
 				else
 				{
-					info->cub_list.map_matrix[i][j] = ft_atoi(&info->cub_list.map[i][j]);
+					info->cub_list.map_matrix[i][j] = info->cub_list.map[i][j] - '0';
 					printf("under\n");
 				}
 				printf("after: %d\n", info->cub_list.map_matrix[i][j]);
