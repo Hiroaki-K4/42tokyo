cab307e0a0be0172403233611b7511f0f3871604
Hiroaki-K4 <49no19@gmail.com>
2021-05-15 17:40:05 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/15 17:39:45 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/15 17:40:03 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -655,7 +655,6 @@ int	main(int argc, char *argv[])
 	buffer_size = 10;
     fd = open(argv[1], O_RDONLY);
 	// init_cub_list(&cub_list);
-	info.cub_list.map_y = 0;
 	info.cub_list.map_y = 0;
 	info.cub_list.size = 0;
 	i = 1;
