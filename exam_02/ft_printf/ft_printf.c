ba03f963eb4a288e265835c61f2751bff6f5dc98
Hiroaki-K4 <49no19@gmail.com>
2021-05-15 14:35:05 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/15 14:25:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/15 14:35:03 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -400,6 +400,59 @@ int print_string(va_list *ap, t_plist flag_list)
 	return (len);
 }
 
+int print_string(va_list *ap, t_plist flag_list)
+{
+	int i;
+	int len;
+	char *str;
+
+	len = 0;
+	str = va_arg(*ap, char*);
+	if (str == NULL)
+		str = "(null)";
+	if (flag_list.field != -1)
+	{
+		if (flag_list.precision != -1 && flag_list.precision < (int)ft_strlen(str))
+		{
+			i = flag_list.precision;
+			while ((flag_list.field - i) > 0)
+			{
+				write(1, " ", 1);
+				i++;
+			}
+			write(1, str, flag_list.precision);
+			len = i;
+		}
+		else
+		{
+			i = 0;
+			while ((flag_list.field - (int)ft_strlen(str) - i) > 0)
+			{
+				write(1, " ", 1);
+				i++;
+			}
+			write(1, str, ft_strlen(str));
+			len = ft_strlen(str) + i;
+		}
+	}
+	else
+	{
+		if (flag_list.precision != -1)
+		{
+			while (str[len] && len < flag_list.precision)
+			{
+				write(1, &str[len], 1);
+				len++;
+			}
+		}
+		else
+		{
+			write(1, str, ft_strlen(str));
+			len = ft_strlen(str);
+		}
+	}
+	return (len);
+}
 
 
 // int		str_to_num(const char *arg, int *i, int flag)
