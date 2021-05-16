bff0c83b9a68101915b7896a6fde376b7d22af3a
Hiroaki-K4 <49no19@gmail.com>
2021-05-16 16:06:24 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/16 16:06:05 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/16 16:06:20 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -797,10 +797,10 @@ int	main(int argc, char *argv[])
 	convert_int_matrix(&info);
 	info.mlx = mlx_init();
 
-	// info.posX = 22.0;
-	info.posX = 1.0;
-	// info.posY = 11.5;
-	info.posY = 6.0;
+	info.posX = 22.0;
+	// info.posX = 1.0;
+	info.posY = 11.5;
+	// info.posY = 6.0;
 	info.dirX = -1.0;
 	info.dirY = 0.0;
 	info.planeX = 0.0;
