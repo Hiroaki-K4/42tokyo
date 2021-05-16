b33ad33da3bd690bff1143d5c56b49ac184edc4d
Hiroaki-K4 <49no19@gmail.com>
2021-05-16 20:29:29 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/16 20:28:53 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/16 20:29:27 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -648,6 +648,7 @@ int hex_to_decimal(char *hex)
 	
 	ans = 0;
 	printf("hex: %s\n", hex);
+	printf("len: %d\n", (int)ft_strlen(hex));
 	while (i < ft_strlen(hex))
 	{
 		hex_pow = 1;
