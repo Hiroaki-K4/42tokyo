33886b3a6c769d512804033dbf5ee3bc1f0d11b4
Hiroaki-K4 <49no19@gmail.com>
2021-05-16 11:25:42 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/16 11:24:03 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/16 11:25:39 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -491,12 +491,12 @@ int ft_printf_per(const char *arg, int *i, va_list *ap)
 	t_plist flag_list;
 
 	(*i)++;
-	flag_list.field = str_to_num(arg, i, 0);
+	flag_list.field = str_to_num(&arg[*i], i, 0);
 	flag_list.precision = -1;
 	if (arg[*i] == '.')
 	{
 		(*i)++;
-		flag_list.precision = str_to_num(arg, i, 0);
+		flag_list.precision = str_to_num(&arg[*i], i, 0);
 	}
 	flag_list.format = ft_strchr_place("sdx", &arg[*i], i);
 	if (flag_list.format == 0)
