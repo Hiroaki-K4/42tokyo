bb718ad2d8ddef91932786294c735c5505f109cd
Hiroaki-K4 <49no19@gmail.com>
2021-05-16 20:56:13 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/16 20:51:26 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/16 20:56:10 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -802,8 +802,8 @@ int convert_int_matrix(t_info *info)
 				if (info->cub_list.map[i][j] == 'N' || info->cub_list.map[i][j] == 'S' || info->cub_list.map[i][j] == 'W' || info->cub_list.map[i][j] == 'E')
 				{
 					printf("i: %d j: %d\n", i, j);
-					info->posX = (double)i + 1.0;
-					info->posY = (double)j + 1.0;
+					// info->posX = (double)i + 1.0;
+					// info->posY = (double)j + 1.0;
 				}
 				if (info->cub_list.map[i][j] == 'N')
 					info->cub_list.map_matrix[i][j] = 4;
@@ -902,8 +902,8 @@ int	main(int argc, char *argv[])
 
 	// info.posX = 1.0;
 	// info.posY = 11.5;
-	// info.posX = 3.0;
-	// info.posY = 13.0;
+	info.posX = 3.0;
+	info.posY = 13.0;
 	info.dirX = -1.0;
 	info.dirY = 0.0;
 	info.planeX = 0.0;
