b16e59fbbd30253f23998c34cc3b851d715aeb45
Hiroaki-K4 <49no19@gmail.com>
2021-05-16 21:04:28 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/16 21:03:56 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/16 21:04:26 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -803,8 +803,8 @@ int convert_int_matrix(t_info *info)
 				if (info->cub_list.map[i][j] == 'N' || info->cub_list.map[i][j] == 'S' || info->cub_list.map[i][j] == 'W' || info->cub_list.map[i][j] == 'E')
 				{
 					printf("i: %d j: %d\n", i, j);
-					info->posX = (double)i;
-					info->posY = (double)j;
+					info->posX = (double)j;
+					info->posY = (double)i;
 				}
 				if (info->cub_list.map[i][j] == 'N')
 					info->cub_list.map_matrix[i][j] = 4;
