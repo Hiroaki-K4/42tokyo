a9142c4fa1a8bc5bc6c586e3f23778cf041c1706
Hiroaki-K4 <49no19@gmail.com>
2021-05-17 22:14:40 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/17 22:14:02 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/17 22:14:24 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -383,9 +383,9 @@ void	calc(t_info *info)
 				if (side == 0)
 				{
 					if (rayDirX > 0)
-						texNum = 1;
-					else
 						texNum = 4;
+					else
+						texNum = 1;
 				}
 				else
 					texNum = 3;
