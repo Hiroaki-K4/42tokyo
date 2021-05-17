34d4782a5d0fbbc08045e04ced28d31206efd97a
Hiroaki-K4 <49no19@gmail.com>
2021-05-17 22:14:19 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/17 22:13:11 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/17 22:14:02 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -380,16 +380,16 @@ void	calc(t_info *info)
 			else if (fabs(info->dirY) >= fabs(info->dirX) && 0 >= info->dirY)
 			{
 				printf("side: %d\n", side);
-				// if (side == 1)
-				// {
-				// 	if (rayDirX > 0)
-				// 		texNum = 1;
-				// 	else
-				// 		texNum = 4;
-				// }
-				// else
-				// 	texNum = 3;
-				texNum = 3;
+				if (side == 0)
+				{
+					if (rayDirX > 0)
+						texNum = 1;
+					else
+						texNum = 4;
+				}
+				else
+					texNum = 3;
+				// texNum = 3;
 			}
 			else if (fabs(info->dirY) >= fabs(info->dirX) && info->dirY >= 0)
 			{
