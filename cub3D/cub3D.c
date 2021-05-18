a33d9cf3bcdc0640cec9c7e938e7b36d53dd571d
Hiroaki-K4 <49no19@gmail.com>
2021-05-18 22:28:07 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/18 22:27:47 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/18 22:28:03 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -937,7 +937,6 @@ int get_sprite_pos(t_info *info)
 		}
 		i++;
 	}
-	printf("count: %d\n", count);
 	if (!(info->cub_list.sprites = (int **)malloc(sizeof(int *) * count)))
 		return (-1);
 	i = 0;
@@ -1004,7 +1003,7 @@ int	main(int argc, char *argv[])
 	}
 	convert_int_matrix(&info);
 	get_sprite_pos(&info);
-	// printf("spriteX: %d spriteY: %d\n", info.cub_list.sprites[0][0], info.cub_list.sprites[0][1]);
+	printf("spriteX: %d spriteY: %d\n", info.cub_list.sprites[0][0], info.cub_list.sprites[0][1]);
 	info.mlx = mlx_init();
 	sin = ft_itoa_hex(info.cub_list.floor.red, "0123456789ABCDEF");
 	printf("sin: %s\n", sin);
