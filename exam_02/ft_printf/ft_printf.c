2db602ca1f581a40f2fddfc14038658f85ccab5c
Hiroaki-K4 <49no19@gmail.com>
2021-05-22 10:56:00 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/22 10:55:11 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/22 10:55:59 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -611,46 +611,18 @@ int ft_printf_str(const char *arg, int *i)
 	return (len);
 }
 
-// int		ft_printf(const char *arg, ...)
-// {
-// 	va_list	ap;
-// 	int		i;
-// 	int		j;
-// 	int		print_len;
-
-// 	va_start(ap, arg);
-// 	i = 0;
-// 	if (arg == NULL)
-// 		i = -1;
-// 	print_len = 0;
-// 	while (i >= 0 && arg[i])
-// 	{
-// 		if (arg[i] != '%')
-// 			print_len += ft_printf_str(arg, &i);
-// 		else
-// 		{
-// 			j = ft_printf_per(arg, &i, &ap);
-// 			if (j == -1)
-// 				return (-1);
-// 			print_len += j;
-// 		}
-// 	}
-// 	va_end(ap);
-// 	return (print_len);
-// }
-
-int ft_printf(const char *arg, ...)
+int		ft_printf(const char *arg, ...)
 {
-	va_list ap;
-	int i;
-	int j;
-	int print_len;
-	
+	va_list	ap;
+	int		i;
+	int		j;
+	int		print_len;
+
 	va_start(ap, arg);
 	i = 0;
-	print_len = 0;
 	if (arg == NULL)
 		i = -1;
+	print_len = 0;
 	while (i >= 0 && arg[i])
 	{
 		if (arg[i] != '%')
@@ -666,3 +638,31 @@ int ft_printf(const char *arg, ...)
 	va_end(ap);
 	return (print_len);
 }
+
+// int ft_printf(const char *arg, ...)
+// {
+// 	va_list ap;
+// 	int i;
+// 	int j;
+// 	int print_len;
+	
+// 	va_start(ap, arg);
+// 	i = 0;
+// 	print_len = 0;
+// 	if (arg == NULL)
+// 		i = -1;
+// 	while (i >= 0 && arg[i])
+// 	{
+// 		if (arg[i] != '%')
+// 			print_len += ft_printf_str(arg, &i);
+// 		else
+// 		{
+// 			j = ft_printf_per(arg, &i, &ap);
+// 			if (j == -1)
+// 				return (-1);
+// 			print_len += j;
+// 		}
+// 	}
+// 	va_end(ap);
+// 	return (print_len);
+// }
