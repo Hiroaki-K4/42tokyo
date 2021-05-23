de54506c012282d9e21145dba17f15bdb950bfb0
Hiroaki-K4 <49no19@gmail.com>
2021-05-24 08:51:59 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/23 22:16:22 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/23 22:16:57 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -562,7 +562,6 @@ void	calc(t_info *info)
 		//calculate height of the sprite on screen
 		int spriteHeight = (int)fabs((info->cub_list.height / transformY) / vDiv); //using "transformY" instead of the real distance prevents fisheye
 		//calculate lowest and highest pixel to fill in current stripe
-		printf("vmove: %d\n", vMoveScreen);
 		int drawStartY = -spriteHeight / 2 + info->cub_list.height / 2 + vMoveScreen;
 		if (drawStartY < 0)
 			drawStartY = 0;
