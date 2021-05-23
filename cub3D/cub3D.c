7758c718858f5f509e23d3ff0f587a23b74028e8
Hiroaki-K4 <49no19@gmail.com>
2021-05-23 14:00:19 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/23 13:59:14 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/23 14:00:13 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -325,17 +325,17 @@ int write_data(int fd, t_info *info, int padding)
 			color = info->img.data[x + y * info->cub_list.width];
 			write(fd, &color, 3);
 			// printf("map: %d\n", info->img.data[i * info->cub_list.width + j]);
-			j++;
+			// j++;
 		}
-		j = 0;
+		i = -1;
 		// printf("digit: %d\n", (4 - (info->cub_list.width * 3) % 4) % 4);
-		while ((4 - (info->cub_list.width * 3) % 4) % 4 > i)
+		while (++i < (4 - (info->cub_list.width * 3) % 4) % 4)
 		{
 			write(fd, &zero, 1);
-			j++;
+			// j++;
 		}
 		// printf("ok\n");
-		i++;
+		// i++;
 	}
 	
 }
