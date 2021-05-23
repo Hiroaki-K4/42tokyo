573d58b46db09bfac29c3d188a1d7d05ca4b503a
Hiroaki-K4 <49no19@gmail.com>
2021-05-23 14:03:47 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/23 14:03:07 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/23 14:03:42 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -317,7 +317,7 @@ int write_data(int fd, t_info *info, int padding)
 	while (info->cub_list.height > j)
 	{
 		i = -1;
-		while (++i < info->cub_list.width)
+		while (info->cub_list.width > i)
 		{
 			x = i;
 			y = info->cub_list.height - 1 - j;
@@ -325,7 +325,7 @@ int write_data(int fd, t_info *info, int padding)
 			color = info->img.data[x + y * info->cub_list.width];
 			write(fd, &color, 3);
 			// printf("map: %d\n", info->img.data[i * info->cub_list.width + j]);
-			// j++;
+			i++;
 		}
 		i = -1;
 		// printf("digit: %d\n", (4 - (info->cub_list.width * 3) % 4) % 4);
