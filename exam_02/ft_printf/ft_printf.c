dc7b5e6adc5884ae4bcb9b077045246d4d28bd4a
Hiroaki-K4 <49no19@gmail.com>
2021-05-23 12:01:24 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/23 12:00:50 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/23 12:01:22 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -509,7 +509,7 @@ int str_to_num(const char *arg, int *i, int flag)
 // 	return (len);
 // }
 
-int ft_printf_str(const char *arg, int *i, va_list *ap)
+int ft_printf_per(const char *arg, int *i, va_list *ap)
 {
 	int len;
 	int keta;
