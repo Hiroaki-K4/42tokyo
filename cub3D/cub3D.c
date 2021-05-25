99f748655442cdeb2bf4b60de0f0a928b77ebf48
Hiroaki-K4 <49no19@gmail.com>
2021-05-25 17:19:06 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/25 17:12:37 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/25 17:18:54 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -601,8 +601,8 @@ int	key_press(int key, t_info *info)
 	if (key == K_D)
 	{
 		// if (!info->cub_list.map_matrix[(int)(info->posX)][(int)(info->posY - info->dirY * info->moveSpeed)])
-		info->posX -= info->dirX * info->moveSpeed;
-		info->posY += info->dirY * info->moveSpeed;
+		info->posX += info->planeX * info->moveSpeed;
+		info->posY += info->planeY * info->moveSpeed;
 	}
 	if (key == K_A)
 	{
