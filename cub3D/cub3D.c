5b653284bffdb5f4431bcb7b3e477f1b2b6801d4
Hiroaki-K4 <49no19@gmail.com>
2021-05-26 18:02:14 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:28:08 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/26 17:58:23 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/26 18:01:53 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,135 +68,6 @@ void	load_texture(t_info *info)
 		load_image(info, info->texture[4], info->cub_list.sprite, &img);
 }
 
-// int convert_int_matrix(t_info *info)
-// {
-// 	int i;
-// 	int j;
-	
-// 	if (!(info->cub_list.map_matrix = (int **)malloc(sizeof(int *) * (info->cub_list.map_y))))
-// 		return (-1);
-// 	i = 0;
-// 	while (i < info->cub_list.map_y)
-// 	{
-// 		if (!(info->cub_list.map_matrix[i] = (int *)malloc(sizeof(int) * (info->cub_list.map_x))))
-// 			return (-1);
-// 		i++;
-// 	}
-// 	i = 0;
-// 	while (i < info->cub_list.map_y)
-// 	{
-// 		j = 0;
-// 		if (ft_strlen(info->cub_list.map[i]) < info->cub_list.map_x)
-// 		{
-// 			while (j < ft_strlen(info->cub_list.map[i]))
-// 			{
-// 				if (info->cub_list.map[i][j] == 'N' || info->cub_list.map[i][j] == 'S' || info->cub_list.map[i][j] == 'W' || info->cub_list.map[i][j] == 'E')
-// 				{
-// 					info->posX = (double)i + 0.5;
-// 					info->posY = (double)j + 0.5;
-// 				}
-// 				if (info->cub_list.map[i][j] == 'N')
-// 				{
-// 					info->cub_list.map_matrix[i][j] = 0;
-// 					info->dirX = -1.0;
-// 					info->dirY = 0.0;
-// 					info->planeX = 0.0;
-// 					info->planeY = 0.66;
-// 				}
-// 				else if (info->cub_list.map[i][j] == 'S')
-// 				{
-// 					info->cub_list.map_matrix[i][j] = 0;
-// 					info->dirX = 1.0;
-// 					info->dirY = 0.0;
-// 					info->planeX = 0.0;
-// 					info->planeY = -0.66;
-// 				}
-// 				else if (info->cub_list.map[i][j] == 'W')
-// 				{
-// 					info->cub_list.map_matrix[i][j] = 0;
-// 					info->dirX = 0.0;
-// 					info->dirY = -1.0;
-// 					info->planeX = -0.66;
-// 					info->planeY = 0.0;
-// 				}
-// 				else if (info->cub_list.map[i][j] == 'E')
-// 				{
-// 					info->cub_list.map_matrix[i][j] = 0;
-// 					info->dirX = 0.0;
-// 					info->dirY = 1.0;
-// 					info->planeX = 0.66;
-// 					info->planeY = 0.0;
-// 				}
-// 				else if (info->cub_list.map[i][j] == ' ')
-// 					info->cub_list.map_matrix[i][j] = 3;
-// 				else
-// 					info->cub_list.map_matrix[i][j] = info->cub_list.map[i][j] - '0';
-// 				write(1, ft_itoa(info->cub_list.map_matrix[i][j]), 1);
-// 				j++;
-// 			}
-// 			while (j < info->cub_list.map_x)
-// 			{
-// 				info->cub_list.map_matrix[i][j] = 3;
-// 				write(1, ft_itoa(info->cub_list.map_matrix[i][j]), 1);
-// 				j++;
-// 			}
-// 		}
-// 		else
-// 		{
-// 			while (j < info->cub_list.map_x)
-// 			{
-// 				if (info->cub_list.map[i][j] == 'N' || info->cub_list.map[i][j] == 'S' || info->cub_list.map[i][j] == 'W' || info->cub_list.map[i][j] == 'E')
-// 				{
-// 					info->posX = (double)i + 0.5;
-// 					info->posY = (double)j + 0.5;
-// 				}
-// 				if (info->cub_list.map[i][j] == 'N')
-// 				{
-// 					info->cub_list.map_matrix[i][j] = 0;
-// 					info->dirX = -1.0;
-// 					info->dirY = 0.0;
-// 					info->planeX = 0.0;
-// 					info->planeY = 0.66;
-// 				}
-// 				else if (info->cub_list.map[i][j] == 'S')
-// 				{
-// 					info->cub_list.map_matrix[i][j] = 0;
-// 					info->dirX = 1.0;
-// 					info->dirY = 0.0;
-// 					info->planeX = 0.0;
-// 					info->planeY = -0.66;
-// 				}
-// 				else if (info->cub_list.map[i][j] == 'W')
-// 				{
-// 					info->cub_list.map_matrix[i][j] = 0;
-// 					info->dirX = 0.0;
-// 					info->dirY = -1.0;
-// 					info->planeX = -0.66;
-// 					info->planeY = 0.0;
-// 				}
-// 				else if (info->cub_list.map[i][j] == 'E')
-// 				{	
-// 					info->cub_list.map_matrix[i][j] = 0;
-// 					info->dirX = 0.0;
-// 					info->dirY = 1.0;
-// 					info->planeX = 0.66;
-// 					info->planeY = 0.0;
-// 				}
-// 				else if (info->cub_list.map[i][j] == ' ')
-// 					info->cub_list.map_matrix[i][j] = 3;
-// 				else
-// 					info->cub_list.map_matrix[i][j] = info->cub_list.map[i][j] - '0';
-// 				write(1, ft_itoa(info->cub_list.map_matrix[i][j]), 1);
-// 				j++;
-// 			}
-// 		}
-// 		write(1, "\n", 1);
-// 		i++;
-// 	}
-
-// 	return (0);
-// }
-
 int	main(int argc, char *argv[])
 {
 	t_info info;
@@ -224,25 +95,15 @@ int	main(int argc, char *argv[])
 	{
 		i = get_next_line(fd, &line, buffer_size);
 		cub_line_check(line, &info);
-		// printf("~~~fd: %d line: %s first: %c return: %d~~~\n", fd, line, line[0], i);
 		free(line);
 	}
-	printf("map_x: %d\n", info.cub_list.map_x);
-	i = 0;
-	while (i < info.cub_list.map_y)
-	{
-		printf("line: %s\n", info.cub_list.map[i]);
-		i++;
-	}
 	convert_int_matrix(&info);
-	printf("initX: %d initY: %d\n", (int)info.posX, (int)info.posY);
 	if (info.cub_list.sprite_flag == 1)
 		get_sprite_pos(&info);
 	info.mlx = mlx_init();
 	mlx_get_screen_size(info.mlx, &sizex, &sizey);
 	info.cub_list.width = sizex;
 	info.cub_list.height = sizey;
-	printf("sizex: %d sizey: %d\n", info.cub_list.width, info.cub_list.height);
 	info.save_flag = 0;
 	if (argc == 3)
 	{
