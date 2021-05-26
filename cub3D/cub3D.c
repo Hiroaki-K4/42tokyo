1b69231c0f8faccb0ee688267f43279d846e98a3
Hiroaki-K4 <49no19@gmail.com>
2021-05-26 17:39:14 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:28:08 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/26 17:37:48 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/26 17:39:02 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -274,64 +274,6 @@ int win_close()
 	exit(0);
 	return (0);
 }
-
-// int	key_press(int key, t_info *info)
-// {
-// 	if (key == K_W)
-// 	{
-// 		if (!info->cub_list.map_matrix[(int)(info->posX + info->dirX * info->moveSpeed)][(int)(info->posY)])
-// 			info->posX += info->dirX * info->moveSpeed;
-// 		if (!info->cub_list.map_matrix[(int)(info->posX)][(int)(info->posY + info->dirY * info->moveSpeed)])
-// 			info->posY += info->dirY * info->moveSpeed;
-// 	}
-// 	//move backwards if no wall behind you
-// 	if (key == K_S)
-// 	{
-// 		if (!info->cub_list.map_matrix[(int)(info->posX - info->dirX * info->moveSpeed)][(int)(info->posY)])
-// 			info->posX -= info->dirX * info->moveSpeed;
-// 		if (!info->cub_list.map_matrix[(int)(info->posX)][(int)(info->posY - info->dirY * info->moveSpeed)])
-// 			info->posY -= info->dirY * info->moveSpeed;
-// 	}
-// 	if (key == K_D)
-// 	{
-// 		if (!info->cub_list.map_matrix[(int)(info->posX + info->planeX * info->moveSpeed)][(int)(info->posY)])
-// 			info->posX += info->planeX * info->moveSpeed;
-// 		if (!info->cub_list.map_matrix[(int)(info->posX)][(int)(info->posY + info->planeY * info->moveSpeed)])
-// 			info->posY += info->planeY * info->moveSpeed;
-// 	}
-// 	if (key == K_A)
-// 	{
-// 		if (!info->cub_list.map_matrix[(int)(info->posX - info->planeX * info->moveSpeed)][(int)(info->posY)])
-// 			info->posX -= info->planeX * info->moveSpeed;
-// 		if (!info->cub_list.map_matrix[(int)(info->posX)][(int)(info->posY - info->planeY * info->moveSpeed)])
-// 			info->posY -= info->planeY * info->moveSpeed;
-// 	}
-// 	//rotate to the right
-// 	if (key == K_R)
-// 	{
-// 		//both camera direction and camera plane must be rotated
-// 		double oldDirX = info->dirX;
-// 		info->dirX = info->dirX * cos(-info->rotSpeed) - info->dirY * sin(-info->rotSpeed);
-// 		info->dirY = oldDirX * sin(-info->rotSpeed) + info->dirY * cos(-info->rotSpeed);
-// 		double oldPlaneX = info->planeX;
-// 		info->planeX = info->planeX * cos(-info->rotSpeed) - info->planeY * sin(-info->rotSpeed);
-// 		info->planeY = oldPlaneX * sin(-info->rotSpeed) + info->planeY * cos(-info->rotSpeed);
-// 	}
-// 	//rotate to the left
-// 	if (key == K_L)
-// 	{
-// 		//both camera direction and camera plane must be rotated
-// 		double oldDirX = info->dirX;
-// 		info->dirX = info->dirX * cos(info->rotSpeed) - info->dirY * sin(info->rotSpeed);
-// 		info->dirY = oldDirX * sin(info->rotSpeed) + info->dirY * cos(info->rotSpeed);
-// 		double oldPlaneX = info->planeX;
-// 		info->planeX = info->planeX * cos(info->rotSpeed) - info->planeY * sin(info->rotSpeed);
-// 		info->planeY = oldPlaneX * sin(info->rotSpeed) + info->planeY * cos(info->rotSpeed);
-// 	}
-// 	if (key == K_ESC)
-// 		exit(0);
-// 	return (0);
-// }
 
 void	load_image(t_info *info, int *texture, char *path, t_img *img)
 {
