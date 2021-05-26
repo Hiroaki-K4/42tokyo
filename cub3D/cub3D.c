673de4a3fd914b55d0898392ffa76a22d45f267c
Hiroaki-K4 <49no19@gmail.com>
2021-05-26 16:24:55 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 15:18:03 by yohlee            #+#    #+#             */
-/*   Updated: 2021/05/26 16:23:21 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/26 16:24:53 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -1033,6 +1033,9 @@ int	main(int argc, char *argv[])
 	info.cub_list.map_x = 0;
 	info.cub_list.map_y = 0;
 	info.cub_list.size = 0;
+	mlx_get_screen_size(info.mlx, &sizex, &sizey);
+	info.cub_list.width = sizex;
+	info.cub_list.height = sizey;
 	i = 1;
 	while (i > 0)
 	{
@@ -1088,9 +1091,9 @@ int	main(int argc, char *argv[])
 			info.texture[i][j] = 0;
 		}
 	}
-	mlx_get_screen_size(info.mlx, &sizex, &sizey);
-	info.cub_list.width = sizex;
-	info.cub_list.height = sizey;
+	// mlx_get_screen_size(info.mlx, &sizex, &sizey);
+	// info.cub_list.width = sizex;
+	// info.cub_list.height = sizey;
 	// if (info.cub_list.width > sizex)
 		// info.cub_list.width = sizex;
 	// if (info.cub_list.height > sizey)
