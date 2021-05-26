d6d3df9914e73f8067364c9ef24696646324218b
Hiroaki-K4 <49no19@gmail.com>
2021-05-26 21:06:27 +0900

    edit

--- cub3D/draw_sprite.c
+++ cub3D/draw_sprite.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:27:46 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/26 21:05:46 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/26 21:06:15 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -177,16 +177,18 @@ void draw_sprite(t_info *info)
 			//3) it's on the screen (right)
 			//4) ZBuffer, with perpendicular distance
 			if(transformY > 0 && stripe > 0 && stripe < info->cub_list.width && transformY < info->zBuffer[stripe])
-			// if(transformY > 0 && stripe > 0 && stripe < width && transformY < zBuffer[stripe])
+			{
+            // if(transformY > 0 && stripe > 0 && stripe < width && transformY < zBuffer[stripe])
 			// y = drawStartY;
-            for(int y = drawStartY; y < drawEndY; y++) //for every pixel of the current stripe
-            {
-				int d = (y-vMoveScreen) * 256 - info->cub_list.height * 128 + spriteHeight * 128; //256 and 128 factors to avoid floats
-				int texY = ((d * texHeight) / spriteHeight) / 256;
-				// int color = info->texture[sprite[spriteOrder[i]].texture][texWidth * texY + texX]; //get current color from the texture
-				int color = info->texture[info->cub_list.sprites[spriteOrder[i]].texture][texWidth * texY + texX]; //get current color from the texture
-				if((color & 0x00FFFFFF) != 0)
-					info->buf[y][stripe] = color; //paint pixel if it isn't black, black is the invisible color
+                for(int y = drawStartY; y < drawEndY; y++) //for every pixel of the current stripe
+                {
+                    int d = (y-vMoveScreen) * 256 - info->cub_list.height * 128 + spriteHeight * 128; //256 and 128 factors to avoid floats
+                    int texY = ((d * texHeight) / spriteHeight) / 256;
+                    // int color = info->texture[sprite[spriteOrder[i]].texture][texWidth * texY + texX]; //get current color from the texture
+                    int color = info->texture[info->cub_list.sprites[spriteOrder[i]].texture][texWidth * texY + texX]; //get current color from the texture
+                    if((color & 0x00FFFFFF) != 0)
+                        info->buf[y][stripe] = color; //paint pixel if it isn't black, black is the invisible color
+                }
             }
             stripe++;
 		}
