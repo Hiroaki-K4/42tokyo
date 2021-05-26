6dfa936d5cefa57e9a3d533cada5ecdf74d31efe
Hiroaki-K4 <49no19@gmail.com>
2021-05-26 21:46:28 +0900

    edit

--- cub3D/draw_sprite.c
+++ cub3D/draw_sprite.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:27:46 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/26 21:30:34 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/26 21:46:13 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,36 +139,22 @@ void draw_sprite(t_info *info)
         i++;
     }
 	sortSprites(spriteOrder, spriteDistance, info->cub_list.num_sprites);
-	//after sorting the sprites, do the projection and draw them
 	i = 0;
 	while (i < info->cub_list.num_sprites)
     {
-		//translate sprite position to relative to camera
 		spriteX = info->cub_list.sprites[spriteOrder[i]].x + 0.5 - info->posX;
 		spriteY = info->cub_list.sprites[spriteOrder[i]].y + 0.5 - info->posY;
-
-		//transform sprite with the inverse camera matrix
-		// [ planeX   dirX ] -1                                       [ dirY      -dirX ]
-		// [               ]       =  1/(planeX*dirY-dirX*planeY) *   [                 ]
-		// [ planeY   dirY ]                                          [ -planeY  planeX ]
-
 		invDet = 1.0 / (info->planeX * info->dirY - info->dirX * info->planeY); //required for correct matrix multiplication
 		transformX = invDet * (info->dirY * spriteX - info->dirX * spriteY);
 		transformY = invDet * (-info->planeY * spriteX + info->planeX * spriteY); //this is actually the depth inside the screen, that what Z is in 3D, the distance of sprite to player, matching sqrt(spriteDistance[i])
 		spriteScreenX = (int)((info->cub_list.width / 2) * (1 + transformX / transformY));
-		//parameters for scaling and moving the sprites
-		
-		//calculate height of the sprite on screen
 		spriteHeight = (int)fabs(info->cub_list.height / transformY); //using "transformY" instead of the real distance prevents fisheye
-		//calculate lowest and highest pixel to fill in current stripe
 		drawStartY = -spriteHeight / 2 + info->cub_list.height / 2;
 		if (drawStartY < 0)
 			drawStartY = 0;
 		drawEndY = spriteHeight / 2 + info->cub_list.height / 2;
 		if (drawEndY >= info->cub_list.height)
 			drawEndY = info->cub_list.height - 1;
-
-		//calculate width of the sprite
 		spriteWidth = (int)fabs(info->cub_list.height / transformY);
 		drawStartX = -spriteWidth / 2 + spriteScreenX;
 		if (drawStartX < 0)
@@ -176,17 +162,10 @@ void draw_sprite(t_info *info)
 		drawEndX = spriteWidth / 2 + spriteScreenX;
 		if (drawEndX >= info->cub_list.width)
             drawEndX = info->cub_list.width - 1;
-
-		//loop through every vertical stripe of the sprite on screen
 		stripe = drawStartX;
 		while (stripe < drawEndX)
         {
 			texX = (int)((256 * (stripe - (-spriteWidth / 2 + spriteScreenX)) * texWidth / spriteWidth) / 256);
-			//the conditions in the if are:
-			//1) it's in front of camera plane so you don't see things behind you
-			//2) it's on the screen (left)
-			//3) it's on the screen (right)
-			//4) ZBuffer, with perpendicular distance
 			if (transformY > 0 && stripe > 0 && stripe < info->cub_list.width && transformY < info->zBuffer[stripe])
 			{
                 y = drawStartY;
