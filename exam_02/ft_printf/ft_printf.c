aaec3ad9b237a3119b2a38ac2ae5dbc06570318f
Hiroaki-K4 <49no19@gmail.com>
2021-05-27 17:17:40 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -226,14 +226,16 @@ int		ft_isdigit(int c)
 
 int field_precision(t_plist flag_list, char *str_num, int num, int i)
 {
-	int j;
+	int len;
 	char *tmp;
 
+	len = 0;
 	if (num < 0)
 	{
 		if (!(tmp = (char *)malloc(sizeof(char) * (flag_list.precision + 2))))
 			return (-1);
 		tmp[0] = '-';
+		i = 1;
 		while ((flag_list.precision + 2 - (int)ft_strlen(str_num) - i) > 0)
 			tmp[i++] = '0';
 		j = 1;
@@ -250,7 +252,7 @@ int field_precision(t_plist flag_list, char *str_num, int num, int i)
 		tmp[i++] = str_num[j++];
 	tmp[i] = '\0';
 	i = 0;
-	while ((flag_list.field - (int)ft_strlen(tmp) - (i++)) > 0)
+	whlie ((flag_list.field - (int)ft_strlen(tmp) - (i++)) > 0)
 		write(1, " ", 1);
 	write(1, tmp, ft_strlen(tmp));
 	free(tmp);
