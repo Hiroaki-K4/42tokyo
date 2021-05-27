e5d8be95e9027bb7783792869de51806ece7dcae
Hiroaki-K4 <49no19@gmail.com>
2021-05-27 22:24:09 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:28:08 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/27 22:22:24 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/27 22:24:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ int	main(int argc, char *argv[])
 		printf("Map file is wrong\n");
 		exit(1);
 	}
-	if (&argv[1][i - 4] == ".cub")
+	if (strcmp(&argv[1][i - 4], ".cub") == 0)
 		printf("ok\n");
 	printf("filename: %d %s\n", i, &argv[1][i - 4]);
 	fd = open(argv[1], O_RDONLY);
