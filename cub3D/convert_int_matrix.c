6183cf781389c6275a0c5a7e3b5dcd94a501712e
Hiroaki-K4 <49no19@gmail.com>
2021-05-29 18:08:46 +0900

    edit

--- cub3D/convert_int_matrix.c
+++ cub3D/convert_int_matrix.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:58:03 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/29 18:08:28 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/29 18:08:41 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -269,12 +269,9 @@ int convert_int_matrix(t_info *info)
 		}
 		else if (i == 0 || i == info->cub_list.map_y - 1)
 		{
-			// if (last_no_wall_row != -1)
-			// {
 			printf("Error\n");
 			printf("Map file is wrong\n");
 			exit(1);
-			// }
 		}
 		else
 		{
