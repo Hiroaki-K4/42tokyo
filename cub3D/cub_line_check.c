9f74bbdee06528a89fa74109b9df581041acd6db
Hiroaki-K4 <49no19@gmail.com>
2021-05-29 21:50:28 +0900

    edit

--- cub3D/cub_line_check.c
+++ cub3D/cub_line_check.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:51:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/29 21:05:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/29 21:50:26 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,13 @@ int cub_line_check(char **line, int count, t_info *info)
 		info->cub_list.floor.red = ft_atoi(rgb_split[0]);
 		info->cub_list.floor.green = ft_atoi(rgb_split[1]);
 		info->cub_list.floor.blue = ft_atoi(rgb_split[2]);
+		if (info->cub_list.floor.red < 0 || info->cub_list.floor.green < 0 || info->cub_list.floor.blue < 0 || info->cub_list.floor.red > 255 
+			|| info->cub_list.floor.green > 255 || info->cub_list.floor.blue > 255)
+		{
+			printf("Error\n");
+			printf("Color is wrong\n");
+			exit(1);
+		}
 		info->cub_list.floor_dec = make_decimal_color(info->cub_list.floor);
 	}
 	else if (i = ft_strcmp("C", line_split[0]) == 0)
