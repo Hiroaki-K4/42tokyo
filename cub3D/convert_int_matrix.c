c47af51e15418c25252bb08cdfc3afd0ab0ebb00
Hiroaki-K4 <49no19@gmail.com>
2021-05-29 17:51:23 +0900

    edit

--- cub3D/convert_int_matrix.c
+++ cub3D/convert_int_matrix.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:58:03 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/29 17:50:53 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/29 17:51:19 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -288,7 +288,7 @@ int convert_int_matrix(t_info *info)
 		// 			exit(1);
 		// 		}
 		// 	}
-		}
+		// }
 		printf("fisrt: %d last: %d\n", first_no_wall_row, last_no_wall_row);
 		write(1, "\n", 1);
 		i++;
