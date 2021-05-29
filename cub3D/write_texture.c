2ec389b284c352022c6adf7438f5ffa9acffe253
Hiroaki-K4 <49no19@gmail.com>
2021-05-29 21:08:45 +0900

    edit

--- cub3D/write_texture.c
+++ cub3D/write_texture.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:40:26 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/26 22:10:54 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/29 21:08:41 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,11 @@ void	calc(t_info *info)
 	double texPos;
 
 	if (!(info->zBuffer = (double *)malloc(sizeof(double) * (info->cub_list.width))))
-		return (-1);
+	{
+		printf("Error\n");
+		printf("Malloc failed\n");
+		exit(1);
+	}
 	x = 0;
 	while (x < info->cub_list.width)
 	{
