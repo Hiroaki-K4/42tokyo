907ecbf8601246053e7cb4ccc430f1c91a098b14
Hiroaki-K4 <49no19@gmail.com>
2021-05-30 21:16:51 +0900

    edit

--- cub3D/cub3D.c
+++ cub3D/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:28:08 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/30 20:49:49 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/30 21:16:43 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,23 +40,23 @@ int	main_loop(t_info *info)
 
 void finish_free(t_info *info)
 {
-	int i;
-	// int j;
+	// int i;
+	// // int j;
 	
-	free(info->cub_list.n_texture);
-	free(info->cub_list.s_texture);
-	free(info->cub_list.e_texture);
-	free(info->cub_list.w_texture);
-	i = 0;
-	while (i < info->cub_list.map_y)
-	{
-		free(info->cub_list.map_matrix[i]);
-		info->cub_list.map_matrix[i] = NULL;
-		i++;
-	}
-	free(info->cub_list.map_matrix);
-	info->cub_list.map_matrix = NULL;
-	double_array_free(info->cub_list.map);
+	// free(info->cub_list.n_texture);
+	// free(info->cub_list.s_texture);
+	// free(info->cub_list.e_texture);
+	// free(info->cub_list.w_texture);
+	// i = 0;
+	// while (i < info->cub_list.map_y)
+	// {
+	// 	free(info->cub_list.map_matrix[i]);
+	// 	info->cub_list.map_matrix[i] = NULL;
+	// 	i++;
+	// }
+	// free(info->cub_list.map_matrix);
+	// info->cub_list.map_matrix = NULL;
+	// double_array_free(info->cub_list.map);
 }
 
 int win_close(t_info *info)
