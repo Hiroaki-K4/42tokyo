d074cbb650ceb2c0d604ef6e2581e9ab513bcff5
Hiroaki-K4 <49no19@gmail.com>
2021-05-30 17:09:19 +0900

    edit

--- cub3D/libft/ft_split.c
+++ cub3D/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 22:42:10 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/30 15:26:23 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/30 17:09:14 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,8 @@ char			**ft_split(char const *s, char c)
 		return (NULL);
 	if (!(ptr_ptr = (char **)malloc(sizeof(char *) * (num_str(s, c) + 1))))
 		return (NULL);
-	ft_strlcpy(div, &c, 2);
+	div[0] = c;
+	div[1] = '\0';
+	// ft_strlcpy(div, &c, 2);
 	return (main_div(s, c, div, ptr_ptr));
 }
