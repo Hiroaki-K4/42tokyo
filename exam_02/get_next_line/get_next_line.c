13288c1996874b76a5b89c740c3d27850460322f
Hiroaki-K4 <49no19@gmail.com>
2021-05-30 10:00:58 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/30 09:59:09 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/30 10:00:49 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -220,6 +220,25 @@ size_t ft_strlen(const char *src)
     while (src[i])
         i++;
     return (i);
+}
+
+char *ft_strdup(const char *src)
+{
+    size_t i;
+    char *dst;
+    
+    if (!src)
+        return (NULL);
+    if (!(dst = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1))))
+        return (NULL);
+    i = 0;
+    while (src[i])
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (dst);
 }
 
 char *get_from_store(char *store, char **line)
