b154c5c8f13989f0866d3f6c94cacf258755ddb8
Hiroaki-K4 <49no19@gmail.com>
2021-05-30 14:39:05 +0900

    edit

--- cub3D/cub_line_check.c
+++ cub3D/cub_line_check.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:51:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/05/30 14:32:38 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/05/30 14:33:55 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,11 +144,6 @@ int cub_line_check(char **line, int count, t_info *info)
 				i++;
 			}
 		}
-		// while (info->cub_list.map[i])
-		// {
-		// 	free(info->cub_list.map[i]);
-		// 	i++;
-		// }
 		free(info->cub_list.map);
 		info->cub_list.map = tmp;
 	}
