281413fc79435dc3c4b213f69e5f69d72da4d911
Hiroaki-K4 <49no19@gmail.com>
2021-06-02 22:52:07 +0900

    edit

--- cub3D/map_check.c
+++ cub3D/map_check.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 22:06:33 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/02 22:50:44 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/02 22:51:55 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,27 +39,27 @@ int	map_edge_check(t_info *info, int *i)
 
 int	col_edge_check(t_info *info, int *j)
 {
-    if (info->cub_list.l_nowall_col == -1)
-    {
-        (*j)++;
-        return (0);
-    }
-    if (*j == 0 || *j == info->cub_list.map_x - 1)
-        error_process("Map file is wrong");
-    else
-    {
-        if (info->cub_list.f_nowall_col == 0 || info->cub_list.l_nowall_col ==
+	if (info->cub_list.l_nowall_col == -1)
+	{
+		(*j)++;
+		return (0);
+	}
+	if (*j == 0 || *j == info->cub_list.map_x - 1)
+		error_process("Map file is wrong");
+	else
+	{
+		if (info->cub_list.f_nowall_col == 0 || info->cub_list.l_nowall_col ==
 			info->cub_list.map_y - 1)
-            error_process("Map file is wrong");
-        if (info->cub_list.f_nowall_col != -1)
-        {
-            if (info->cub_list.map_matrix[info->cub_list.f_nowall_col - 1][*j]
+			error_process("Map file is wrong");
+		if (info->cub_list.f_nowall_col != -1)
+		{
+			if (info->cub_list.map_matrix[info->cub_list.f_nowall_col - 1][*j]
 				!= 1 || info->cub_list.map_matrix[info->cub_list.l_nowall_col + 1][*j] != 1)
-                error_process("Map file is wrong");
-        }
-    }
-    (*j)++;
-    return (0);
+				error_process("Map file is wrong");
+		}
+	}
+	(*j)++;
+	return (0);
 }
 
 void	map_not_srrounded_check(t_info *info)
