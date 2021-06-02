48beb8e2eef4a83af31ad1460410d98738ea2776
Hiroaki-k4 <kubo@globalwalkers.co.jp>
2021-06-02 11:34:13 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -222,6 +222,24 @@ size_t ft_strlen(const char *src)
     return (i);
 }
 
+size_t ft_strlcpy(char *dst, const char *src, size_t n)
+{
+    size_t i;
+    size_t ans;
+
+    ans = ft_strlen(src);
+    if (n == 0)
+        return (ans);
+    i = 0;
+    while (src[i] && i < n - 1)
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (ans);
+}
+
 char *ft_strjoin(const char *s1, const char *s2)
 {
     size_t i;
