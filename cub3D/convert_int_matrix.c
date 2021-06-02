dc176ad6570477f85f4bd099a6f012540e84c06f
Hiroaki-k4 <kubo@globalwalkers.co.jp>
2021-06-02 19:20:31 +0900

    edit

--- cub3D/convert_int_matrix.c
+++ cub3D/convert_int_matrix.c
@@ -6,85 +6,11 @@
 /*   By: hiroaki-k4 <hiroaki-k4@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:58:03 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/02 19:19:35 by hiroaki-k4       ###   ########.fr       */
+/*   Updated: 2021/06/02 19:20:29 by hiroaki-k4       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
-
-// void	map_space_check(t_info *info)
-// {
-// 	int i;
-// 	int j;
-
-// 	i = 0;
-// 	while (i < info->cub_list.map_y)
-// 	{
-// 		j = 0;
-// 		while (j < info->cub_list.map_x)
-// 		{
-// 			if (info->cub_list.map_matrix[i][j] == 5)
-// 			{
-// 				if (i == 0)
-// 				{
-// 					if (j == 0)
-// 					{
-// 						if (info->cub_list.map_matrix[i][j + 1] == 0 || info->cub_list.map_matrix[i + 1][j] == 0 || info->cub_list.map_matrix[i][j + 1] == 2 || info->cub_list.map_matrix[i + 1][j] == 2)
-// 							error_process("Map file is wrong");
-// 					}
-// 					else if (j == info->cub_list.map_x - 1)
-// 					{
-// 						if (info->cub_list.map_matrix[i][j - 1] == 0 || info->cub_list.map_matrix[i + 1][j] == 0 || info->cub_list.map_matrix[i][j - 1] == 2 || info->cub_list.map_matrix[i + 1][j] == 2)
-// 							error_process("Map file is wrong");
-// 					}
-// 					else
-// 					{
-// 						if (info->cub_list.map_matrix[i][j - 1] == 0 || info->cub_list.map_matrix[i + 1][j] == 0 || info->cub_list.map_matrix[i][j + 1] == 0 || info->cub_list.map_matrix[i][j - 1] == 2 || info->cub_list.map_matrix[i + 1][j] == 2 || info->cub_list.map_matrix[i][j + 1] == 2)
-// 							error_process("Map file is wrong");
-// 					}
-// 				}
-// 				else if (i == info->cub_list.map_y - 1)
-// 				{
-// 					if (j == 0)
-// 					{
-// 						if (info->cub_list.map_matrix[i][j + 1] == 0 || info->cub_list.map_matrix[i - 1][j] == 0 || info->cub_list.map_matrix[i][j + 1] == 2 || info->cub_list.map_matrix[i - 1][j] == 2)
-// 							error_process("Map file is wrong");
-// 					}
-// 					else if (j == info->cub_list.map_x - 1)
-// 					{
-// 						if (info->cub_list.map_matrix[i][j - 1] == 0 || info->cub_list.map_matrix[i - 1][j] == 0 || info->cub_list.map_matrix[i][j - 1] == 2 || info->cub_list.map_matrix[i - 1][j] == 2)
-// 							error_process("Map file is wrong");
-// 					}
-// 					else
-// 					{
-// 						if (info->cub_list.map_matrix[i][j - 1] == 0 || info->cub_list.map_matrix[i - 1][j] == 0 || info->cub_list.map_matrix[i][j + 1] == 0 || info->cub_list.map_matrix[i][j - 1] == 2 || info->cub_list.map_matrix[i - 1][j] == 2 || info->cub_list.map_matrix[i][j + 1] == 2)
-// 							error_process("Map file is wrong");
-// 					}
-// 				}
-// 				else
-// 				{
-// 					if (j == 0)
-// 					{
-// 						if (info->cub_list.map_matrix[i][j + 1] == 0 || info->cub_list.map_matrix[i + 1][j] == 0 || info->cub_list.map_matrix[i - 1][j] == 0 || info->cub_list.map_matrix[i][j + 1] == 2 || info->cub_list.map_matrix[i + 1][j] == 2 || info->cub_list.map_matrix[i - 1][j] == 2)
-// 							error_process("Map file is wrong");
-// 					}
-// 					else if (j == info->cub_list.map_x - 1)
-// 					{
-// 						if (info->cub_list.map_matrix[i][j - 1] == 0 || info->cub_list.map_matrix[i + 1][j] == 0 || info->cub_list.map_matrix[i - 1][j] == 0 || info->cub_list.map_matrix[i][j - 1] == 2 || info->cub_list.map_matrix[i + 1][j] == 2 || info->cub_list.map_matrix[i - 1][j] == 2)
-// 							error_process("Map file is wrong");
-// 					}
-// 					else
-// 					{
-// 						if (info->cub_list.map_matrix[i][j - 1] == 0 || info->cub_list.map_matrix[i + 1][j] == 0 || info->cub_list.map_matrix[i][j + 1] == 0 || info->cub_list.map_matrix[i - 1][j] == 0 || info->cub_list.map_matrix[i][j - 1] == 2 || info->cub_list.map_matrix[i + 1][j] == 2 || info->cub_list.map_matrix[i][j + 1] == 2 || info->cub_list.map_matrix[i - 1][j] == 2)
-// 							error_process("Map file is wrong");
-// 					}
-// 				}
-// 			}
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// }
 
 void	init_dir_plane(t_info *info, double x, double y, int type)
 {
