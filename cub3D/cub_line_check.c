e219900b8b82b7c7b36b81da4badfa8b9a54e4d4
Hiroaki-K4 <49no19@gmail.com>
2021-06-03 21:03:53 +0900

    edit

--- cub3D/cub_line_check.c
+++ cub3D/cub_line_check.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:51:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/03 20:57:38 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/03 21:03:20 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,6 +128,32 @@ int	ceiling_check(t_info *info, char **line_split, char **rgb_split)
 	return (0);
 }
 
+int	map_dir_check(t_info *info, char **line_split)
+{
+	int i;
+
+	if (line_split[0] == NULL)
+	{
+		double_array_free(line_split);
+		return (1);
+	}
+	i = ft_strcmp("NO", line_split[0]);
+	if (i == 0)
+	{
+		info->cub_list.n_texture = ft_strdup(line_split[1]);
+		double_array_free(line_split);
+		return (1);
+	}
+	i = ft_strcmp("SO", line_split[0]);
+	if (i == 0)
+	{
+		info->cub_list.s_texture = ft_strdup(line_split[1]);
+		double_array_free(line_split);
+		return (1);
+	}
+	return (0);
+}
+
 int	cub_line_check(char **line, int count, t_info *info)
 {
 	int		i;
@@ -138,25 +164,28 @@ int	cub_line_check(char **line, int count, t_info *info)
 	line_split = ft_split(line, ' ');
 	if (line_split == NULL)
 		error_process("ft_split failed");
-	if (line_split[0] == NULL)
-	{
-		double_array_free(line_split);
+	i = map_dir_check(info, line_split);
+	if (i == 1)
 		return (0);
-	}
-	i = ft_strcmp("NO", line_split[0]);
-	if (i == 0)
-	{
-		info->cub_list.n_texture = ft_strdup(line_split[1]);
-		double_array_free(line_split);
-		return (0);
-	}
-	i = ft_strcmp("SO", line_split[0]);
-	if (i == 0)
-	{
-		info->cub_list.s_texture = ft_strdup(line_split[1]);
-		double_array_free(line_split);
-		return (0);
-	}
+	// if (line_split[0] == NULL)
+	// {
+	// 	double_array_free(line_split);
+	// 	return (0);
+	// }
+	// i = ft_strcmp("NO", line_split[0]);
+	// if (i == 0)
+	// {
+	// 	info->cub_list.n_texture = ft_strdup(line_split[1]);
+	// 	double_array_free(line_split);
+	// 	return (0);
+	// }
+	// i = ft_strcmp("SO", line_split[0]);
+	// if (i == 0)
+	// {
+	// 	info->cub_list.s_texture = ft_strdup(line_split[1]);
+	// 	double_array_free(line_split);
+	// 	return (0);
+	// }
 	i = ft_strcmp("WE", line_split[0]);
 	if (i == 0)
 	{
