044bf41df682d6d0027ad23b1ee39f6b73f106df
Hiroaki-K4 <49no19@gmail.com>
2021-06-03 21:44:24 +0900

    edit

--- cub3D/key_press.c
+++ cub3D/key_press.c
@@ -6,17 +6,14 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:37:55 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/03 21:40:42 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/03 21:44:14 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int	key_press(int key, t_info *info)
+void	key_ws(t_info *info, int key)
 {
-	double	oldDirX;
-	double	oldPlaneX;
-
 	if (key == K_W)
 	{
 		if (!info->cub_list.map_matrix[(int)(info->posX + info->dirX
@@ -35,6 +32,32 @@ int	key_press(int key, t_info *info)
 				- info->dirY * info->moveSpeed)])
 			info->posY -= info->dirY * info->moveSpeed;
 	}
+}
+
+int	key_press(int key, t_info *info)
+{
+	double	oldDirX;
+	double	oldPlaneX;
+
+	// if (key == K_W)
+	// {
+	// 	if (!info->cub_list.map_matrix[(int)(info->posX + info->dirX
+	// 			* info->moveSpeed)][(int)(info->posY)])
+	// 		info->posX += info->dirX * info->moveSpeed;
+	// 	if (!info->cub_list.map_matrix[(int)(info->posX)][(int)(info->posY
+	// 			+ info->dirY * info->moveSpeed)])
+	// 		info->posY += info->dirY * info->moveSpeed;
+	// }
+	// if (key == K_S)
+	// {
+	// 	if (!info->cub_list.map_matrix[(int)(info->posX - info->dirX
+	// 			* info->moveSpeed)][(int)(info->posY)])
+	// 		info->posX -= info->dirX * info->moveSpeed;
+	// 	if (!info->cub_list.map_matrix[(int)(info->posX)][(int)(info->posY
+	// 			- info->dirY * info->moveSpeed)])
+	// 		info->posY -= info->dirY * info->moveSpeed;
+	// }
+	key_ws(info, key);
 	if (key == K_D)
 	{
 		if (!info->cub_list.map_matrix[(int)(info->posX + info->planeX
