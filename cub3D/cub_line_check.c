610b0e0edb0a45891127efc7a19633be8d8d639f
Hiroaki-K4 <49no19@gmail.com>
2021-06-03 14:09:44 +0900

    edit

--- cub3D/cub_line_check.c
+++ cub3D/cub_line_check.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:51:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/03 14:09:30 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/03 14:09:41 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ int	cub_line_check(char **line, int count, t_info *info)
 		double_array_free(line_split);
 		return (0);
 	}
-	else if (i = ft_strcmp("NO", line_split[0]) && i == 0)
+	else if (i = ft_strcmp("NO", line_split[0]) == 0)
 	{
 		info->cub_list.n_texture = ft_strdup(line_split[1]);
 		double_array_free(line_split);
