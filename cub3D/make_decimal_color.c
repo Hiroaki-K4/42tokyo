6aed8ece86e222f55d81d31e088813729c2a6c3e
Hiroaki-K4 <49no19@gmail.com>
2021-06-03 22:16:50 +0900

    edit

--- cub3D/make_decimal_color.c
+++ cub3D/make_decimal_color.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:55:56 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/03 22:10:53 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/03 22:16:44 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,40 +52,12 @@ char	*convert_color(int color)
 	return (rgb);
 }
 
-char	*make_decimal_color(t_rgb color)
+int	unite_color(char *red, char *green, char *blue)
 {
-	// char	*int_to_hex;
 	char	*hex;
-	char	*red;
-	char	*green;
-	char	*blue;
 	char	*tmp;
 	int		ans;
 
-	// int_to_hex = ft_itoa_hex(color.red, "0123456789ABCDEF");
-	// if (!int_to_hex)
-	// 	error_process("Malloc failed");
-	// red = ft_strdup(int_to_hex);
-	// if (!red)
-	// 	error_process("Malloc failed");
-	// free(int_to_hex);
-	red = convert_color(color.red);
-	// int_to_hex = ft_itoa_hex(color.green, "0123456789ABCDEF");
-	// if (!int_to_hex)
-	// 	error_process("Malloc failed");
-	// green = ft_strdup(int_to_hex);
-	// if (!green)
-	// 	error_process("Malloc failed");
-	// free(int_to_hex);
-	green = convert_color(color.green);
-	// int_to_hex = ft_itoa_hex(color.blue, "0123456789ABCDEF");
-	// if (!int_to_hex)
-	// 	error_process("Malloc failed");
-	// blue = ft_strdup(int_to_hex);
-	// if (!blue)
-	// 	error_process("Malloc failed");
-	// free(int_to_hex);
-	blue = convert_color(color.blue);
 	hex = ft_strjoin(red, green);
 	if (!hex)
 		error_process("Malloc failed");
@@ -102,5 +74,37 @@ char	*make_decimal_color(t_rgb color)
 	free(tmp);
 	ans = hex_to_decimal(hex);
 	free(hex);
+	return (ans);
+}
+
+char	*make_decimal_color(t_rgb color)
+{
+	char	*hex;
+	char	*red;
+	char	*green;
+	char	*blue;
+	char	*tmp;
+	int		ans;
+
+	red = convert_color(color.red);
+	green = convert_color(color.green);
+	blue = convert_color(color.blue);
+	// hex = ft_strjoin(red, green);
+	// if (!hex)
+	// 	error_process("Malloc failed");
+	// tmp = ft_strdup(hex);
+	// if (!tmp)
+	// 	error_process("Malloc failed");
+	// free(hex);
+	// hex = ft_strjoin(tmp, blue);
+	// if (!hex)
+	// 	error_process("Malloc failed");
+	// free(red);
+	// free(green);
+	// free(blue);
+	// free(tmp);
+	// ans = hex_to_decimal(hex);
+	// free(hex);
+	ans = unite_color(red, green, blue);
 	return (ans);
 }
