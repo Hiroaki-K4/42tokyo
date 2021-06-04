254c6ce46954cebd1cf0e0363546d0f704f84695
Hiroaki-K4 <49no19@gmail.com>
2021-06-04 22:15:02 +0900

    edit

--- cub3D/draw_sprite.c
+++ cub3D/draw_sprite.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:27:46 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/04 22:12:36 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/04 22:14:43 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,48 +109,48 @@ void	sortSprites(int *order, double *dist, int amount)
 	free(sprites);
 }
 
-void draw_sprite(t_info *info)
+void	draw_sprite(t_info *info)
 {
-    int		i;
-    int		y;
-    int		d;
-    int		stripe;
-    int		spriteScreenX;
-    int		spriteHeight;
-    int		drawStartY;
-    int		drawEndY;
-    int		drawStartX;
-    int		drawEndX;
-    int		spriteWidth;
-    int		texX;
-    int		texY;
-    int		color;
-    double	spriteX;
-    double	spriteY;
+	int		i;
+	int		y;
+	int		d;
+	int		stripe;
+	int		spriteScreenX;
+	int		spriteHeight;
+	int		drawStartY;
+	int		drawEndY;
+	int		drawStartX;
+	int		drawEndX;
+	int		spriteWidth;
+	int		texX;
+	int		texY;
+	int		color;
+	double	spriteX;
+	double	spriteY;
 	double	invDet;
-    double	transformX;
-    double	transformY;
-    int		spriteOrder[info->cub_list.num_sprites];
+	double	transformX;
+	double	transformY;
+	int		spriteOrder[info->cub_list.num_sprites];
 	double	spriteDistance[info->cub_list.num_sprites];
-	
-    i = 0;
+
+	i = 0;
 	while (i < info->cub_list.num_sprites)
-    {
+	{
 		spriteOrder[i] = i;
 		spriteDistance[i] = ((info->posX - info->cub_list.sprites[i].x) * (info->posX - info->cub_list.sprites[i].x) + (info->posY - info->cub_list.sprites[i].y) * (info->posY - info->cub_list.sprites[i].y)); //sqrt not taken, unneeded
-        i++;
-    }
+		i++;
+	}
 	sortSprites(spriteOrder, spriteDistance, info->cub_list.num_sprites);
 	i = 0;
 	while (i < info->cub_list.num_sprites)
-    {
+	{
 		spriteX = info->cub_list.sprites[spriteOrder[i]].x + 0.5 - info->posX;
 		spriteY = info->cub_list.sprites[spriteOrder[i]].y + 0.5 - info->posY;
-		invDet = 1.0 / (info->planeX * info->dirY - info->dirX * info->planeY); //required for correct matrix multiplication
+		invDet = 1.0 / (info->planeX * info->dirY - info->dirX * info->planeY);
 		transformX = invDet * (info->dirY * spriteX - info->dirX * spriteY);
-		transformY = invDet * (-info->planeY * spriteX + info->planeX * spriteY); //this is actually the depth inside the screen, that what Z is in 3D, the distance of sprite to player, matching sqrt(spriteDistance[i])
+		transformY = invDet * (-info->planeY * spriteX + info->planeX * spriteY);
 		spriteScreenX = (int)((info->cub_list.width / 2) * (1 + transformX / transformY));
-		spriteHeight = (int)fabs(info->cub_list.height / transformY); //using "transformY" instead of the real distance prevents fisheye
+		spriteHeight = (int)fabs(info->cub_list.height / transformY);
 		drawStartY = -spriteHeight / 2 + info->cub_list.height / 2;
 		if (drawStartY < 0)
 			drawStartY = 0;
@@ -160,29 +160,29 @@ void draw_sprite(t_info *info)
 		spriteWidth = (int)fabs(info->cub_list.height / transformY);
 		drawStartX = -spriteWidth / 2 + spriteScreenX;
 		if (drawStartX < 0)
-            drawStartX = 0;
+			drawStartX = 0;
 		drawEndX = spriteWidth / 2 + spriteScreenX;
 		if (drawEndX >= info->cub_list.width)
-            drawEndX = info->cub_list.width - 1;
+			drawEndX = info->cub_list.width - 1;
 		stripe = drawStartX;
 		while (stripe < drawEndX)
-        {
+		{
 			texX = (int)((256 * (stripe - (-spriteWidth / 2 + spriteScreenX)) * texWidth / spriteWidth) / 256);
 			if (transformY > 0 && stripe > 0 && stripe < info->cub_list.width && transformY < info->zBuffer[stripe])
 			{
-                y = drawStartY;
-                while (y < drawEndY)
-                {
-                    d = y * 256 - info->cub_list.height * 128 + spriteHeight * 128; //256 and 128 factors to avoid floats
-                    texY = ((d * texHeight) / spriteHeight) / 256;
-                    color = info->texture[info->cub_list.sprites[spriteOrder[i]].texture][texWidth * texY + texX]; //get current color from the texture
-                    if ((color & 0x00FFFFFF) != 0)
-                        info->buf[y][stripe] = color; //paint pixel if it isn't black, black is the invisible color
-                    y++;
-                }
-            }
-            stripe++;
+				y = drawStartY;
+				while (y < drawEndY)
+				{
+					d = y * 256 - info->cub_list.height * 128 + spriteHeight * 128;
+					texY = ((d * texHeight) / spriteHeight) / 256;
+					color = info->texture[info->cub_list.sprites[spriteOrder[i]].texture][texWidth * texY + texX];
+					if ((color & 0x00FFFFFF) != 0)
+						info->buf[y][stripe] = color;
+					y++;
+				}
+			}
+			stripe++;
 		}
-        i++;
+		i++;
 	}
 }
