122a68f9d53a0bbb0d82b71c654ff6b084a0e0a3
Hiroaki-K4 <49no19@gmail.com>
2021-06-05 11:34:06 +0900

    edit

--- cub3D/draw_sprite.c
+++ cub3D/draw_sprite.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:27:46 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/05 11:28:23 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/05 11:31:52 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,15 +124,18 @@ void	insert_pixel_loop(t_info *info, int sprite, int *spriteOrder, int i)
 	int		y;
 	int		j;
 
-	info->texX = (int)((256 * (sprite - (-info->spriteW / 2 + info->spriteScreenX)) * texWidth / info->spriteW) / 256);
-	if (info->transformY > 0 && sprite > 0 && sprite < info->cub_list.width && info->transformY < info->zBuffer[sprite])
+	info->texX = (int)((256 * (sprite - (-info->spriteW / 2
+					+ info->spriteScreenX)) * texWidth / info->spriteW) / 256);
+	if (info->transformY > 0 && sprite > 0 && sprite < info->cub_list.width
+		&& info->transformY < info->zBuffer[sprite])
 	{
 		y = info->drawStartY;
 		while (y < info->drawEndY)
 		{
 			j = y * 256 - info->cub_list.height * 128 + info->spriteH * 128;
 			info->texY = ((j * texHeight) / info->spriteH) / 256;
-			color = info->texture[info->cub_list.sprites[spriteOrder[i]].texture][texWidth * info->texY + info->texX];
+			color = info->texture[info->cub_list.sprites
+			[spriteOrder[i]].texture][texWidth * info->texY + info->texX];
 			if ((color & 0x00FFFFFF) != 0)
 				info->buf[y][sprite] = color;
 			y++;
@@ -143,28 +146,11 @@ void	insert_pixel_loop(t_info *info, int sprite, int *spriteOrder, int i)
 void	insert_pixel(t_info *info, int *spriteOrder, int i)
 {
 	int		sprite;
-	// int		color;
-	// int		y;
-	// int		j;
 
 	sprite = info->drawStartX;
 	while (sprite < info->drawEndX)
 	{
 		insert_pixel_loop(info, sprite, spriteOrder, i);
-		// info->texX = (int)((256 * (sprite - (-info->spriteW / 2 + info->spriteScreenX)) * texWidth / info->spriteW) / 256);
-		// if (info->transformY > 0 && sprite > 0 && sprite < info->cub_list.width && info->transformY < info->zBuffer[sprite])
-		// {
-		// 	y = info->drawStartY;
-		// 	while (y < info->drawEndY)
-		// 	{
-		// 		j = y * 256 - info->cub_list.height * 128 + info->spriteH * 128;
-		// 		info->texY = ((j * texHeight) / info->spriteH) / 256;
-		// 		color = info->texture[info->cub_list.sprites[spriteOrder[i]].texture][texWidth * info->texY + info->texX];
-		// 		if ((color & 0x00FFFFFF) != 0)
-		// 			info->buf[y][sprite] = color;
-		// 		y++;
-		// 	}
-		// }
 		sprite++;
 	}
 }
