cd0689c3dd1a8d2075ddb2e24a34e02f32aa9fe9
Hiroaki-K4 <49no19@gmail.com>
2021-06-05 17:08:16 +0900

    edit

--- cub3D/libft/ft_strnstr.c
+++ cub3D/libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 13:24:16 by hkubo             #+#    #+#             */
-/*   Updated: 2020/11/28 19:30:20 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/05 17:07:54 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,12 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 	n = ft_strlen(little);
 	if (n == 0)
-	{
-		return (char *)(big);
-	}
+		return ((char *)(big));
 	i = 0;
 	while (big[i] != '\0' && i < len)
 	{
 		if (ft_strncmp(big + i, little, n) == 0 && i + n <= len)
-		{
-			return (char *)(big + i);
-		}
+			return ((char *)(big + i));
 		i++;
 	}
 	return (NULL);
