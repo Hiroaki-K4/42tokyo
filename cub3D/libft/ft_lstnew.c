03647b8ab9f88cddb9ac79880f3ccc8483f9927b
Hiroaki-K4 <49no19@gmail.com>
2021-06-05 17:09:49 +0900

    edit

--- cub3D/libft/ft_lstnew.c
+++ cub3D/libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/29 10:17:07 by hkubo             #+#    #+#             */
-/*   Updated: 2020/11/29 11:09:05 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/05 17:09:24 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*new_list;
 
-	if (!(new_list = malloc(sizeof(t_list))))
+	new_list = malloc(sizeof(t_list));
+	if (!new_list)
 		return (NULL);
 	new_list->content = content;
 	new_list->next = NULL;
