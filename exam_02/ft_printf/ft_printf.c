7b063c626162d1b4d4224e064d48a4e319ff7060
Hiroaki-K4 <49no19@gmail.com>
2021-06-09 10:05:22 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -372,19 +372,16 @@ int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 	len = 0;
 	if (flag_list.precision == 0 && num == 0)
 	{
-		while (flag_list.field - (int)ft_strlen(str_num) - len > 0)
-		{
+		while (flag_list.field - (len++) > 0)
 			write(1, " ", 1);
-			len++;
-		}
 	}
 	else if (flag_list.field > (int)ft_strlen(str_num))
 	{
 		if (flag_list.precision > keta)
-			len = no_field_int(flag_list, str_num, num, 0);
+			len = field_precision(flag_list, str_num, num, 0);
 		else
 		{
-			while (flag_list.field - (int)ft_strlen(str_num) - (len++) > 0)
+			while (flag_list.field - (int)ft_strlen(str_num) - (i++) > 0)
 				write(1, " ", 1);
 			write(1, str_num, ft_strlen(str_num));
 			len = flag_list.field;
