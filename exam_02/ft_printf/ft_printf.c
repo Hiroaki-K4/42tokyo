93b24afb72b34285873d061c86f3473f0fdaedb3
Hiroaki-K4 <49no19@gmail.com>
2021-06-17 21:45:23 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/17 21:43:43 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/17 21:45:21 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -192,6 +192,20 @@ int ft_atoi(const char *arg)
 	while (arg[i] == ' ' || arg[i] == '\r' || arg[i] == '\n' ||
 			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
 		i++;
+	if (arg[i] == '-' || arg[i] == '+')
+	{
+		if (arg[i] == '-')
+			flag = -1;
+		i++;
+	}
+	ans = 0;
+	while (arg[i] >= '0' && arg[i] <= '9')
+	{
+		ans = (ans * 10) + (arg[i] - '0');
+		i++;
+	}
+	ans = ans * flag;
+	return (ans);
 }
 
 // int		ft_isdigit(int c)
