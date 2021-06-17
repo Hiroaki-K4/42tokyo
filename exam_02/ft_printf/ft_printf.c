56c3b01c51a7c4c5c7c2e2bd63400122d7ba2e10
Hiroaki-K4 <49no19@gmail.com>
2021-06-17 21:43:24 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/17 21:40:54 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/17 21:43:22 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -154,50 +154,52 @@ char			*ft_itoa_hex(unsigned int n, char *arg)
 	return (ans);
 }
 
-int		ft_atoi(const char *arg)
-{
-	int		i;
-	int		flag;
-	int		ans;
-
-	flag = 1;
-	i = 0;
-	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
-			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
-		i++;
-	if (arg[i] == '-' || arg[i] == '+')
-	{
-		if (arg[i] == '-')
-			flag = -1;
-		i++;
-	}
-	ans = 0;
-	while (arg[i] >= '0' && arg[i] <= '9')
-	{
-		ans = (ans * 10) + (arg[i] - '0');
-		i++;
-	}
-	ans = ans * flag;
-	return (ans);
-}
-
-// int ft_atoi(const char *arg)
+// int		ft_atoi(const char *arg)
 // {
-// 	int i;
-// 	int flag;
-// 	int ans;
-	
+// 	int		i;
+// 	int		flag;
+// 	int		ans;
+
 // 	flag = 1;
 // 	i = 0;
-// 	while (nptr)
+// 	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
+// 			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
+// 		i++;
+// 	if (arg[i] == '-' || arg[i] == '+')
+// 	{
+// 		if (arg[i] == '-')
+// 			flag = -1;
+// 		i++;
+// 	}
+// 	ans = 0;
+// 	while (arg[i] >= '0' && arg[i] <= '9')
+// 	{
+// 		ans = (ans * 10) + (arg[i] - '0');
+// 		i++;
+// 	}
+// 	ans = ans * flag;
+// 	return (ans);
 // }
 
-// int		ft_isdigit(int c)
-// {
-// 	if (c >= '0' && c <= '9')
-// 		return (1);
-// 	return (0);
-// }
+int ft_atoi(const char *arg)
+{
+	int i;
+	int flag;
+	int ans;
+	
+	flag = 1;
+	i = 0;
+	while (arg[i] == ' ' || arg[i] == '\r' || arg[i] == '\n' ||
+			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
+		i++;
+}
+
+int		ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
 
 int ft_isdigit(int c)
 {
