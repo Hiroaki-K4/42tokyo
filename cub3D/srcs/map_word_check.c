a5dc3e86f3f52494a7d5d8a71899f12627261677
Hiroaki-K4 <49no19@gmail.com>
2021-06-19 22:51:19 +0900

    edit

--- cub3D/srcs/map_word_check.c
+++ cub3D/srcs/map_word_check.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 21:10:23 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/19 22:50:11 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/19 22:51:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char **split_rgb(t_info *info, char **line_split, char **rgb_split, char *line)
 {
-	int i;
+	int	i;
 
 	rgb_split = ft_split(line_split[1], ',');
 	if (rgb_split == NULL)
@@ -41,25 +41,6 @@ char **split_rgb(t_info *info, char **line_split, char **rgb_split, char *line)
 void	floor_check(t_info *info, char **line_split, char **rgb_split,
 	char *line)
 {
-	// rgb_split = ft_split(line_split[1], ',');
-	// if (rgb_split == NULL)
-	// 	error_process(info, "ft_split failed");
-	// if (info->cub_list.line_num != 0)
-	// {
-	// 	double_array_free(rgb_split);
-	// 	map_wrong_process(info, line_split, line);
-	// }
-	// double_array_free(line_split);
-	// j = 0;
-	// while (rgb_split[j])
-	// 	j++;
-	// if (j != 3)
-	// {
-	// 	double_array_free(rgb_split);
-	// 	get_next_line(info->cub_list.fd, &line, -1);
-	// 	free(line);
-	// 	error_process(info, "Color is wrong");
-	// }
 	rgb_split = split_rgb(info, line_split, rgb_split, line);
 	info->cub_list.floor.red = ft_atoi(rgb_split[0]);
 	info->cub_list.floor.green = ft_atoi(rgb_split[1]);
@@ -87,24 +68,6 @@ int	ceiling_check(t_info *info, char **line_split, char **rgb_split, char *line)
 	if (i == 0)
 	{
 		rgb_split = split_rgb(info, line_split, rgb_split, line);
-		// rgb_split = ft_split(line_split[1], ',');
-		// if (rgb_split == NULL)
-		// 	error_process(info, "ft_split failed");
-		// if (info->cub_list.line_num != 0)
-		// {
-		// 	double_array_free(rgb_split);
-		// 	map_wrong_process(info, line_split, line);
-		// }
-		// double_array_free(line_split);
-		// while (rgb_split[i])
-		// 	i++;
-		// if (i != 3)
-		// {
-		// 	double_array_free(rgb_split);
-		// 	get_next_line(info->cub_list.fd, &line, -1);
-		// 	free(line);
-		// 	error_process(info, "Color is wrong");
-		// }
 		info->cub_list.ceil.red = ft_atoi(rgb_split[0]);
 		info->cub_list.ceil.green = ft_atoi(rgb_split[1]);
 		info->cub_list.ceil.blue = ft_atoi(rgb_split[2]);
