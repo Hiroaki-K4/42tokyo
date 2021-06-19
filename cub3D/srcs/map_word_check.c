a76c28f511d70f951596afe6001ad1be06c97128
Hiroaki-K4 <49no19@gmail.com>
2021-06-19 22:49:34 +0900

    edit

--- cub3D/srcs/map_word_check.c
+++ cub3D/srcs/map_word_check.c
@@ -6,16 +6,15 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 21:10:23 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/19 22:45:43 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/19 22:49:23 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void	floor_check(t_info *info, char **line_split, char **rgb_split,
-	char *line)
+char *split_rgb(t_info *info, char **line_split, char **rgb_split, char *line)
 {
-	int	j;
+	int i;
 
 	rgb_split = ft_split(line_split[1], ',');
 	if (rgb_split == NULL)
@@ -26,16 +25,43 @@ void	floor_check(t_info *info, char **line_split, char **rgb_split,
 		map_wrong_process(info, line_split, line);
 	}
 	double_array_free(line_split);
-	j = 0;
-	while (rgb_split[j])
-		j++;
-	if (j != 3)
+	i = 0;
+	while (rgb_split[i])
+		i++;
+	if (i != 3)
 	{
 		double_array_free(rgb_split);
 		get_next_line(info->cub_list.fd, &line, -1);
 		free(line);
 		error_process(info, "Color is wrong");
 	}
+}
+
+void	floor_check(t_info *info, char **line_split, char **rgb_split,
+	char *line)
+{
+	int	j;
+
+	// rgb_split = ft_split(line_split[1], ',');
+	// if (rgb_split == NULL)
+	// 	error_process(info, "ft_split failed");
+	// if (info->cub_list.line_num != 0)
+	// {
+	// 	double_array_free(rgb_split);
+	// 	map_wrong_process(info, line_split, line);
+	// }
+	// double_array_free(line_split);
+	// j = 0;
+	// while (rgb_split[j])
+	// 	j++;
+	// if (j != 3)
+	// {
+	// 	double_array_free(rgb_split);
+	// 	get_next_line(info->cub_list.fd, &line, -1);
+	// 	free(line);
+	// 	error_process(info, "Color is wrong");
+	// }
+	rgb_split = split_rgb(info, line_split, rgb_split, line);
 	info->cub_list.floor.red = ft_atoi(rgb_split[0]);
 	info->cub_list.floor.green = ft_atoi(rgb_split[1]);
 	info->cub_list.floor.blue = ft_atoi(rgb_split[2]);
@@ -52,7 +78,6 @@ void	floor_check(t_info *info, char **line_split, char **rgb_split,
 	if (info->cub_list.f_count++ == 0)
 		info->cub_list.floor_dec = make_decimal_color(info,
 				info->cub_list.floor);
-	// info->cub_list.f_count++;
 }
 
 int	ceiling_check(t_info *info, char **line_split, char **rgb_split, char *line)
@@ -62,24 +87,25 @@ int	ceiling_check(t_info *info, char **line_split, char **rgb_split, char *line)
 	i = ft_strcmp("C", line_split[0]);
 	if (i == 0)
 	{
-		rgb_split = ft_split(line_split[1], ',');
-		if (rgb_split == NULL)
-			error_process(info, "ft_split failed");
-		if (info->cub_list.line_num != 0)
-		{
-			double_array_free(rgb_split);
-			map_wrong_process(info, line_split, line);
-		}
-		double_array_free(line_split);
-		while (rgb_split[i])
-			i++;
-		if (i != 3)
-		{
-			double_array_free(rgb_split);
-			get_next_line(info->cub_list.fd, &line, -1);
-			free(line);
-			error_process(info, "Color is wrong");
-		}
+		rgb_split = split_rgb(info, line_split, rgb_split, line);
+		// rgb_split = ft_split(line_split[1], ',');
+		// if (rgb_split == NULL)
+		// 	error_process(info, "ft_split failed");
+		// if (info->cub_list.line_num != 0)
+		// {
+		// 	double_array_free(rgb_split);
+		// 	map_wrong_process(info, line_split, line);
+		// }
+		// double_array_free(line_split);
+		// while (rgb_split[i])
+		// 	i++;
+		// if (i != 3)
+		// {
+		// 	double_array_free(rgb_split);
+		// 	get_next_line(info->cub_list.fd, &line, -1);
+		// 	free(line);
+		// 	error_process(info, "Color is wrong");
+		// }
 		info->cub_list.ceil.red = ft_atoi(rgb_split[0]);
 		info->cub_list.ceil.green = ft_atoi(rgb_split[1]);
 		info->cub_list.ceil.blue = ft_atoi(rgb_split[2]);
@@ -96,7 +122,6 @@ int	ceiling_check(t_info *info, char **line_split, char **rgb_split, char *line)
 		if (info->cub_list.c_count++ == 0)
 			info->cub_list.ceiling_dec = make_decimal_color
 				(info, info->cub_list.ceil);
-		// info->cub_list.c_count++;
 		return (1);
 	}
 	return (0);
