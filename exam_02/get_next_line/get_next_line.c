873cd40d6a7d773b1c6247e84a5daa79385382ae
Hiroaki-K4 <49no19@gmail.com>
2021-06-21 21:34:06 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/21 21:30:10 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/21 21:33:52 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -219,6 +219,24 @@ size_t ft_strlen(const char *arg)
     while (arg[i])
         i++;
     return (i);
+}
+
+char *ft_strchr(const char *src, int n)
+{
+    unsigned const char *ptr_s;
+    int i;
+
+    ptr_s = (unsigned const char *)src;
+    i = 0;
+    while (ptr_s[i])
+    {
+        if (ptr_s[i] == (unsigned const char)n)
+            return (char *)(src + i);
+        i++;
+    }
+    if (ptr_s[i] == '\0' && ptr_s[i] == (unsigned const char)n)
+        return (char *)(src + i);
+    return (NULL);
 }
 
 size_t ft_strlcpy(char *dst, const char *src, size_t n)
