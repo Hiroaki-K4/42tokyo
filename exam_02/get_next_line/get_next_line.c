68611fb9cabdaddf04af0387f078f70b4bbe577a
Hiroaki-K4 <49no19@gmail.com>
2021-06-21 21:23:03 +0900

    edit

--- exam_02/get_next_line/get_next_line.c
+++ exam_02/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:06:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/21 21:18:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/21 21:23:01 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -219,6 +219,31 @@ size_t ft_strlen(const char *arg)
     while (arg[i])
         i++;
     return (i);
+}
+
+char *ft_strjoin(const char *s1, const char *s2)
+{
+    char *dst;
+    int i;
+    int j;
+
+    if (!(dst = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+        return (NULL);
+    i = 0;
+    while (s1[i])
+    {
+        dst[i] = s1[i];
+        i++;
+    }
+    j = 0;
+    while (s2[j])
+    {
+        dst[i] = s2[j];
+        i++;
+        j++;
+    }
+    dst[i] = '\0';
+    return (dst);
 }
 
 char *ft_strdup(const char *src)
