14cf3307c3cf50576fad1df2b67f2685739adad5
Hiroaki-K4 <49no19@gmail.com>
2021-06-23 11:34:57 +0900

    edit

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,11 +6,12 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/23 11:30:56 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/23 11:34:55 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "push_swap.h"
 
 int main(int argc, char *argv[])
 {
