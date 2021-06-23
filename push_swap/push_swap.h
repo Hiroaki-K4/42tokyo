2d9351364dbbbfefe96d7c069b9a60765b54a93e
Hiroaki-K4 <49no19@gmail.com>
2021-06-23 11:34:36 +0900

    edit

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,13 +6,13 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 11:32:18 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/23 11:34:10 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/23 11:34:35 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "../libft/libft.h"
+# include "libft/libft.h"
 
 #endif
