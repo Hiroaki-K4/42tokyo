ad667c3375024c46d75e4bc97796da811c75ec91
Hiroaki-K4 <49no19@gmail.com>
2021-06-23 09:13:07 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/23 09:04:22 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/23 09:13:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -530,14 +530,14 @@ int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 
 int print_string(va_list *ap, t_plist flag_list)
 {
-	int len;
 	int i;
+	int len;
 	char *str;
-	
-	len = 0;
+
 	str = va_arg(*ap, char *);
 	if (str == NULL)
 		str = "(null)";
+	len = 0;
 	if (flag_list.field != -1)
 	{
 		if (flag_list.precision != -1 && flag_list.precision < (int)ft_strlen(str))
@@ -554,11 +554,8 @@ int print_string(va_list *ap, t_plist flag_list)
 		else
 		{
 			i = 0;
-			while (flag_list.field - (int)ft_strlen(str) - i > 0)
-			{
+			while (flag_list.field - (int)ft_strlen(str) - (i++) > 0)
 				write(1, " ", 1);
-				i++;
-			}
 			write(1, str, ft_strlen(str));
 			len = ft_strlen(str) + i;
 		}
