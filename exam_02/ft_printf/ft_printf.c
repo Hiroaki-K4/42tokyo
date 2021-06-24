988d8184d28a23d536076d208cb444287e1fc802
Hiroaki-K4 <49no19@gmail.com>
2021-06-24 21:52:27 +0900

    edit

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/23 09:44:06 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/24 21:52:20 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -306,7 +306,7 @@ int field_precision(t_plist flag_list, char *str_num, int num, int i)
 {
 	int j;
 	char *tmp;
-
+	
 	if (num < 0)
 	{
 		if (!(tmp = (char *)malloc(sizeof(char) * (flag_list.precision + 2))))
@@ -314,7 +314,7 @@ int field_precision(t_plist flag_list, char *str_num, int num, int i)
 		tmp[0] = '-';
 		i = 1;
 		while (flag_list.precision + 2 - (int)ft_strlen(str_num) - i > 0)
-			tmp[i] = '0';
+			tmp[i++] = '0';
 		j = 1;
 	}
 	else
@@ -322,20 +322,20 @@ int field_precision(t_plist flag_list, char *str_num, int num, int i)
 		if (!(tmp = (char *)malloc(sizeof(char) * (flag_list.precision + 1))))
 			return (-1);
 		while (flag_list.precision - (int)ft_strlen(str_num) - i > 0)
-			tmp[i] = '0';
+			tmp[i++] = '0';
 		j = 0;
 	}
 	while (ft_strlen(str_num) - j > 0)
 		tmp[i++] = str_num[j++];
 	tmp[i] = '\0';
 	i = 0;
-	while (flag_list.field - (int)ft_strlen(tmp) - (i++) > 0)
+	while (flag_list.field - ft_strlen(str_num) - (i++) > 0)
 		write(1, " ", 1);
 	write(1, tmp, ft_strlen(tmp));
-	free(tmp);
 	if (flag_list.precision > flag_list.field)
 		return (flag_list.precision);
 	return (flag_list.field);
+	
 }
 
 // int	no_field_int(t_plist flag_list, char *str_num, int num, int len)
