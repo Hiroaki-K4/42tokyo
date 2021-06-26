bf729c9801b4e5a2c5b64121bd8384d25a7cf474
Hiroaki-K4 <49no19@gmail.com>
2021-06-26 15:08:48 +0900

    add struc

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 11:32:18 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/26 15:06:59 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/26 15:08:44 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 # include "libft/libft.h"
 
-struct	s_list
+struct	s_bi_list
 {
     int data;
-    struct t_list *prev;
-    struct t_list *next;
-}	t_list;
+    struct t_bi_list *prev;
+    struct t_bi_list *next;
+}	t_bi_list;
 
 #endif
