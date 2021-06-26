819a9aec237c4adfd511fd3232c88f6eb1a41a8b
Hiroaki-K4 <49no19@gmail.com>
2021-06-26 12:24:07 +0900

    test

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/26 12:23:48 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/26 12:24:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -337,48 +337,11 @@ int field_precision(t_plist flag_list, char *str_num, int num, int i)
 	return (flag_list.field);
 }
 
-// int	no_field_int(t_plist flag_list, char *str_num, int num, int len)
-// {
-// 	int		keta;
-// 	char	*tmp;
-	
-// 	keta = ft_strlen(str_num);
-// 	if (num < 0)
-// 		keta--;
-// 	if (flag_list.precision > keta)
-// 	{
-// 		len = flag_list.precision;
-// 		if (num < 0)
-// 		{
-// 			if (!(tmp = ft_itoa(num * (-1))))
-// 				return (-1);
-// 			write(1, "-", 1);
-// 			while (flag_list.precision - (keta++) > 0)
-// 				write(1, "0", 1);
-// 			write(1, tmp, ft_strlen(tmp));
-// 			free(tmp);
-// 			len = flag_list.precision + 1;
-// 		}
-// 		else
-// 		{
-// 			while (flag_list.precision - (keta++) > 0)
-// 				write(1, "0", 1);
-// 			write(1, str_num, ft_strlen(str_num));
-// 		}
-// 	}
-// 	else
-// 	{
-// 		write(1, str_num, ft_strlen(str_num));
-// 		len = ft_strlen(str_num);
-// 	}
-// 	return (len);
-// }
-
-int no_field_int(t_plist flag_list, char *str_num, int num, int len)
+int	no_field_int(t_plist flag_list, char *str_num, int num, int len)
 {
-	int keta;
-	char *tmp;
-
+	int		keta;
+	char	*tmp;
+	
 	keta = ft_strlen(str_num);
 	if (num < 0)
 		keta--;
@@ -387,17 +350,18 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 		len = flag_list.precision;
 		if (num < 0)
 		{
-			write(1, "-", 1);
-			if (!(tmp = ft_itoa(num)))
+			if (!(tmp = ft_itoa(num * (-1))))
 				return (-1);
+			write(1, "-", 1);
 			while (flag_list.precision - (keta++) > 0)
 				write(1, "0", 1);
 			write(1, tmp, ft_strlen(tmp));
+			free(tmp);
 			len = flag_list.precision + 1;
 		}
 		else
 		{
-			while(flag_list.precision - (keta++) > 0)
+			while (flag_list.precision - (keta++) > 0)
 				write(1, "0", 1);
 			write(1, str_num, ft_strlen(str_num));
 		}
@@ -409,6 +373,42 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 	}
 	return (len);
 }
+
+// int no_field_int(t_plist flag_list, char *str_num, int num, int len)
+// {
+// 	int keta;
+// 	char *tmp;
+
+// 	keta = ft_strlen(str_num);
+// 	if (num < 0)
+// 		keta--;
+// 	if (flag_list.precision > keta)
+// 	{
+// 		len = flag_list.precision;
+// 		if (num < 0)
+// 		{
+// 			write(1, "-", 1);
+// 			if (!(tmp = ft_itoa(num)))
+// 				return (-1);
+// 			while (flag_list.precision - (keta++) > 0)
+// 				write(1, "0", 1);
+// 			write(1, tmp, ft_strlen(tmp));
+// 			len = flag_list.precision + 1;
+// 		}
+// 		else
+// 		{
+// 			while(flag_list.precision - (keta++) > 0)
+// 				write(1, "0", 1);
+// 			write(1, str_num, ft_strlen(str_num));
+// 		}
+// 	}
+// 	else
+// 	{
+// 		write(1, str_num, ft_strlen(str_num));
+// 		len = ft_strlen(str_num);
+// 	}
+// 	return (len);
+// }
 
 // int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 // {
