43fb0b7e6f4762375ddd503ca3a31a3cc9c8f5d3
Hiroaki-K4 <49no19@gmail.com>
2021-06-26 15:07:13 +0900

    add struc

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,61 +6,61 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/24 22:54:53 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/26 15:03:48 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "push_swap.h"
 
-int digit_check(const char *arg)
+int	digit_check(const char *arg)
 {
-    int i;
-    int flag;
-    size_t num;
+	int i;
+	int flag;
+	size_t num;
 
-    flag = 1;
-    i = 0;
-    while (arg[i] == '-' || arg[i] == '+')
-    {
-        if (arg[i] == '-')
-            flag = flag * (-1);
-        if (i == (int)ft_strlen(arg) - 1)
-            return (-1);
-        i++;
-    }
-    num = 0;
-    while (arg[i])
-    {
-        if (arg[i] < '0' || arg[i] > '9')
-            return (-1);
-        num = (10 * num) + (arg[i] - '0');
-        i++;
-    }
-    if ((flag == 1 && num > 2147483647) || (flag == -1 && num > 2147483648))
-        return (-1);
-    return (0);
+	flag = 1;
+	i = 0;
+	while (arg[i] == '-' || arg[i] == '+')
+	{
+		if (arg[i] == '-')
+			flag = flag * (-1);
+		if (i == (int)ft_strlen(arg) - 1)
+			return (-1);
+		i++;
+	}
+	num = 0;
+	while (arg[i])
+	{
+		if (arg[i] < '0' || arg[i] > '9')
+			return (-1);
+		num = (10 * num) + (arg[i] - '0');
+		i++;
+	}
+	if ((flag == 1 && num > 2147483647) || (flag == -1 && num > 2147483648))
+		return (-1);
+	return (0);
 }
 
-void    error_process()
+void	error_process()
 {
-    write(1, "Error\n", 6);
-    exit(0);
+	write(1, "Error\n", 6);
+	exit(0);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int i;
-    int num;
+	int i;
+	int num;
 
-    i = 1;
-    while (i < argc)
-    {
-        if (digit_check(argv[i]) == -1)
-            error_process();
-        num = ft_atoi(argv[i]);
-        printf("arg: %d\n", num);
-        i++;
-    }
-    return (0);
+	i = 1;
+	while (i < argc)
+	{
+		if (digit_check(argv[i]) == -1)
+			error_process();
+		num = ft_atoi(argv[i]);
+		printf("arg: %d\n", num);
+		i++;
+	}
+	return (0);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 11:32:18 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/23 11:34:35 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/26 15:06:59 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 # define PUSH_SWAP_H
 
 # include "libft/libft.h"
+
+struct	s_list
+{
+    int data;
+    struct t_list *prev;
+    struct t_list *next;
+}	t_list;
 
 #endif
