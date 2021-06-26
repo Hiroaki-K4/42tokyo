c5a2fd9045ad9dd7e615919e8cb995fab5a67f2f
Hiroaki-K4 <49no19@gmail.com>
2021-06-26 12:18:18 +0900

    test

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/26 12:18:07 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/26 12:18:16 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -410,37 +410,6 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 	return (len);
 }
 
-int print_digit(t_plist flag_list, char *str_num, int num, int keta)
-{
-	int len;
-	
-	len = 0;
-	if (flag_list.precision == 0 && num == 0)
-	{
-		while (flag_list.field - len > 0)
-		{
-			write(1, " ", 1);
-			len++;
-		}
-	}
-	else if (flag_list.field > (int)ft_strlen(str_num))
-	{
-		if (flag_list.precision > keta)
-			len = field_precision(flag_list, str_num, num, 0);
-		else
-		{
-			while ((flag_list.field - (int)ft_strlen(str_num) - (len++)) > 0)
-				write(1, " ", 1);
-			write(1, str_num, ft_strlen(str_num));
-			len = flag_list.field;
-		}
-	}
-	else
-		len = no_field_int(flag_list, str_num, num, 0);
-	free(str_num);
-	return (len);
-}
-
 // int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 // {
 // 	int len;
@@ -460,7 +429,7 @@ int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 // 			len = field_precision(flag_list, str_num, num, 0);
 // 		else
 // 		{
-// 			while (flag_list.field - (int)ft_strlen(str_num) - (len++) > 0)
+// 			while ((flag_list.field - (int)ft_strlen(str_num) - (len++)) > 0)
 // 				write(1, " ", 1);
 // 			write(1, str_num, ft_strlen(str_num));
 // 			len = flag_list.field;
@@ -471,6 +440,37 @@ int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 // 	free(str_num);
 // 	return (len);
 // }
+
+int print_digit(t_plist flag_list, char *str_num, int num, int keta)
+{
+	int len;
+	
+	len = 0;
+	if (flag_list.precision == 0 && num == 0)
+	{
+		while (flag_list.field - len > 0)
+		{
+			write(1, " ", 1);
+			len++;
+		}
+	}
+	else if (flag_list.field > (int)ft_strlen(str_num))
+	{
+		if (flag_list.precision > keta)
+			len = field_precision(flag_list, str_num, num, 0);
+		else
+		{
+			while (flag_list.field - (int)ft_strlen(str_num) - (len++) > 0)
+				write(1, " ", 1);
+			write(1, str_num, ft_strlen(str_num));
+			len = flag_list.field;
+		}
+	}
+	else
+		len = no_field_int(flag_list, str_num, num, 0);
+	free(str_num);
+	return (len);
+}
 
 // int	print_string(va_list *ap, t_plist flag_list)
 // {
