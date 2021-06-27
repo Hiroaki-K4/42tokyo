2511894a78e62404d9703f55dd5e21929218813e
Hiroaki-K4 <49no19@gmail.com>
2021-06-27 12:02:20 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/27 11:58:48 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/27 12:02:17 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -305,26 +305,51 @@ int	no_field_int(t_plist flag_list, char *str_num, int num, int len)
 	return (len);
 }
 
+// int print_digit(t_plist flag_list, char *str_num, int num, int keta)
+// {
+// 	int len;
+	
+// 	len = 0;
+// 	if (flag_list.precision == 0 && num == 0)
+// 	{
+// 		while (flag_list.field - len > 0)
+// 		{
+// 			write(1, " ", 1);
+// 			len++;
+// 		}
+// 	}
+// 	else if (flag_list.field > (int)ft_strlen(str_num))
+// 	{
+// 		if (flag_list.precision > keta)
+// 			len = field_precision(flag_list, str_num, num, 0);
+// 		else
+// 		{
+// 			while ((flag_list.field - (int)ft_strlen(str_num) - (len++)) > 0)
+// 				write(1, " ", 1);
+// 			write(1, str_num, ft_strlen(str_num));
+// 			len = flag_list.field;
+// 		}
+// 	}
+// 	else
+// 		len = no_field_int(flag_list, str_num, num, 0);
+// 	free(str_num);
+// 	return (len);
+// }
+
 int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 {
 	int len;
-	
+
 	len = 0;
 	if (flag_list.precision == 0 && num == 0)
-	{
-		while (flag_list.field - len > 0)
-		{
-			write(1, " ", 1);
-			len++;
-		}
-	}
+		return (0);
 	else if (flag_list.field > (int)ft_strlen(str_num))
 	{
 		if (flag_list.precision > keta)
 			len = field_precision(flag_list, str_num, num, 0);
 		else
 		{
-			while ((flag_list.field - (int)ft_strlen(str_num) - (len++)) > 0)
+			while (flag_list.field - (int)ft_strlen(str_num) - (len++) > 0)
 				write(1, " ", 1);
 			write(1, str_num, ft_strlen(str_num));
 			len = flag_list.field;
