303fd871524f42ba5ded3a11487877d757fad538
Hiroaki-K4 <49no19@gmail.com>
2021-06-27 14:08:34 +0900

    make swap_a function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/27 14:08:22 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/27 14:08:32 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,7 +111,7 @@ int	duplicate_check(int argc, char *argv[])
 void	top_del(t_bi_list *list)
 {
 	free(list);
-	printf("top: %d\n", list->data);
+	printf("top: %d\n", list->next->data);
 }
 
 void	swap_a(t_bi_list *stack_a, t_bi_list *stack_b)
