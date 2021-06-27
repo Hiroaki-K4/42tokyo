ca5d81d79f07d5317954e361171931be717521a9
Hiroaki-K4 <49no19@gmail.com>
2021-06-27 11:58:55 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/27 10:17:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/27 11:58:48 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -336,14 +336,68 @@ int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 	return (len);
 }
 
-int	print_string(va_list *ap, t_plist flag_list)
+// int	print_string(va_list *ap, t_plist flag_list)
+// {
+// 	int		i;
+// 	int		len;
+// 	char	*str;
+
+// 	len = 0;
+// 	str = va_arg(*ap, char*);
+// 	if (str == NULL)
+// 		str = "(null)";
+// 	if (flag_list.field != -1)
+// 	{
+// 		if (flag_list.precision != -1 && flag_list.precision < (int)ft_strlen(str))
+// 		{
+// 			i = flag_list.precision;
+// 			while ((flag_list.field - i) > 0)
+// 			{
+// 				write(1, " ", 1);
+// 				i++;
+// 			}
+// 			write(1, str, flag_list.precision);
+// 			len = i;
+// 		}
+// 		else
+// 		{
+// 			i = 0;
+// 			while ((flag_list.field - (int)ft_strlen(str) - i) > 0)
+// 			{
+// 				write(1, " ", 1);
+// 				i++;
+// 			}
+// 			write(1, str, ft_strlen(str));
+// 			len = (int)ft_strlen(str) + i;
+// 		}
+// 	}
+// 	else
+// 	{
+// 		if (flag_list.precision != -1)
+// 		{
+// 			while (len < flag_list.precision && str[len])
+// 			{
+// 				write(1, &str[len], 1);
+// 				len++;
+// 			}
+// 		}
+// 		else
+// 		{
+// 			write(1, str, ft_strlen(str));
+// 			len = ft_strlen(str);
+// 		}
+// 	}
+// 	return (len);
+// }
+
+int print_string(va_list *ap, t_plist flag_list)
 {
-	int		i;
-	int		len;
-	char	*str;
+	int i;
+	int len;
+	char *str;
 
 	len = 0;
-	str = va_arg(*ap, char*);
+	str = va_arg(*ap, char *);
 	if (str == NULL)
 		str = "(null)";
 	if (flag_list.field != -1)
@@ -351,7 +405,7 @@ int	print_string(va_list *ap, t_plist flag_list)
 		if (flag_list.precision != -1 && flag_list.precision < (int)ft_strlen(str))
 		{
 			i = flag_list.precision;
-			while ((flag_list.field - i) > 0)
+			while (flag_list.field - i > 0)
 			{
 				write(1, " ", 1);
 				i++;
@@ -362,13 +416,10 @@ int	print_string(va_list *ap, t_plist flag_list)
 		else
 		{
 			i = 0;
-			while ((flag_list.field - (int)ft_strlen(str) - i) > 0)
-			{
+			while (flag_list.field - (int)ft_strlen(str) - (i++) > 0)
 				write(1, " ", 1);
-				i++;
-			}
 			write(1, str, ft_strlen(str));
-			len = (int)ft_strlen(str) + i;
+			len = ft_strlen(str) + i;
 		}
 	}
 	else
