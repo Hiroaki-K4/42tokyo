40302f02d3f964ad8747da6204bd4c85db4ce917
Hiroaki-K4 <49no19@gmail.com>
2021-06-27 15:37:38 +0900

    make top_del function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/27 15:37:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/27 15:37:36 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,7 +129,7 @@ void	swap_a(t_bi_list *stack_a, t_bi_list *stack_b)
 	else
 		last_list(stack_b)->next = stack_a;
 	top_del(stack_a);
-	show_list(stack_b);
+	show_list(stack_a);
 }
 
 int	main(int argc, char *argv[])
