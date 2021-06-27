a008e447065e1523eed9ce24ffddca1dbd99b6aa
Hiroaki-K4 <49no19@gmail.com>
2021-06-27 14:56:47 +0900

    make top_del function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/27 14:51:35 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/27 14:56:45 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,6 +113,7 @@ void	top_del(t_bi_list *list)
 	if (list->next)
 	{
 		printf("top: %d\n", list->next->data);
+		free(list->next->prev);
 		list->next->prev = NULL;
 	}
 	show_list(list);
