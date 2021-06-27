e7af0a1e37cf61e46657b06cced81c9967e476fc
Hiroaki-K4 <49no19@gmail.com>
2021-06-27 14:26:14 +0900

    make top_del function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/27 14:25:49 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/27 14:26:13 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,9 +110,9 @@ int	duplicate_check(int argc, char *argv[])
 
 void	top_del(t_bi_list *list)
 {
-	printf("ok\n");
 	while (list != NULL)
 	{
+		printf("ok\n");
 		list = list->next;
 		if (!list->prev)
 			free(list->prev);
