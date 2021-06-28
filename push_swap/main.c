42f88f6bef21430c4555215d42b975f1038b0aa1
Hiroaki-K4 <49no19@gmail.com>
2021-06-28 22:30:31 +0900

    edit push_b function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/06/27 18:28:30 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/06/28 22:30:18 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -193,8 +193,8 @@ int	main(int argc, char *argv[])
 	// show_list(&stack_a);
 	// push_b(stack_a, stack_b);
 	// swap_a(&stack_a);
-	show_list(&stack_a);
-	show_list(&stack_b);
+	// show_list(&stack_a);
+	// show_list(&stack_b);
 	// show_list(stack_b);
 	return (0);
 }
