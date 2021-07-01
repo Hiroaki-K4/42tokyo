99141332c0db78f7b622e7a0eeeb924268ea559a
Hiroaki-K4 <49no19@gmail.com>
2021-07-01 23:14:39 +0900

    make push_b function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/01 23:14:28 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/01 23:14:36 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,7 +133,7 @@ void	push_b(t_bi_list *stack_a, t_bi_list *stack_b)
 	
 	// last_list(stack_b)->next = stack_a;
 	// stack_a = top_del(stack_a);
-	top_del(stack_a);
+	top_del(&stack_a);
 	show_list(stack_a);
 	show_list(stack_b);
 	write(1, "pb\n", 3);
