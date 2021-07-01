1cb7ca24d60271087e1a305085e98998d74a5712
Hiroaki-K4 <49no19@gmail.com>
2021-07-01 22:39:41 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/01 22:39:10 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/01 22:39:41 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,63 +151,63 @@ int digit_size(int num)
 	return (len);
 }
 
-// char			*ft_itoa(int n)
-// {
-// 	int		len;
-// 	int		flag;
-// 	char	*ans;
-
-// 	if (n == -2147483648)
-// 		return (ft_strdup("-2147483648"));
-// 	len = digit_size(n);
-// 	if (!(ans = (char *)malloc(sizeof(char) * (len + 1))))
-// 		return (NULL);
-// 	flag = 1;
-// 	if (n < 0)
-// 	{
-// 		n = n * (-1);
-// 		flag = -1;
-// 	}
-// 	ans[len] = '\0';
-// 	len--;
-// 	while (len >= 0)
-// 	{
-// 		ans[len--] = '0' + (n % 10);
-// 		n = n / 10;
-// 	}
-// 	if (flag == -1)
-// 		ans[0] = '-';
-// 	return (ans);
-// }
-
-char *ft_itoa(int num)
+char			*ft_itoa(int n)
 {
-	char *ans;
-	int flag;
-	int len;
-	
-	if (num == -2147483648)
+	int		len;
+	int		flag;
+	char	*ans;
+
+	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-	len = digit_size(num);
+	len = digit_size(n);
 	if (!(ans = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	flag = 1;
-	if (num < 0)
+	if (n < 0)
 	{
+		n = n * (-1);
 		flag = -1;
-		num = num * (-1);
 	}
 	ans[len] = '\0';
 	len--;
 	while (len >= 0)
 	{
-		ans[len--] = num % 10 + '0';
-		num = num / 10;
+		ans[len--] = '0' + (n % 10);
+		n = n / 10;
 	}
 	if (flag == -1)
 		ans[0] = '-';
 	return (ans);
 }
+
+// char *ft_itoa(int num)
+// {
+// 	char *ans;
+// 	int flag;
+// 	int len;
+	
+// 	if (num == -2147483648)
+// 		return (ft_strdup("-2147483648"));
+// 	len = digit_size(num);
+// 	if (!(ans = (char *)malloc(sizeof(char) * (len + 1))))
+// 		return (NULL);
+// 	flag = 1;
+// 	if (num < 0)
+// 	{
+// 		flag = -1;
+// 		num = num * (-1);
+// 	}
+// 	ans[len] = '\0';
+// 	len--;
+// 	while (len >= 0)
+// 	{
+// 		ans[len--] = num % 10 + '0';
+// 		num = num / 10;
+// 	}
+// 	if (flag == -1)
+// 		ans[0] = '-';
+// 	return (ans);
+// }
 
 // int		ft_atoi(const char *arg)
 // {
