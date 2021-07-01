06e0f7c134a05b4d13b5281bd4af8db9c4ff6b81
Hiroaki-K4 <49no19@gmail.com>
2021-07-01 23:18:00 +0900

    make push_b function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/01 23:17:44 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/01 23:17:56 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -195,7 +195,7 @@ int	main(int argc, char *argv[])
 		error_process();
 	push_b(&stack_a, &stack_b);
 	// show_list(&stack_b);
-	show_list(&stack_a);
+	show_list(stack_a);
 	// push_b(stack_a, stack_b);
 	// swap_a(&stack_a);
 	// show_list(&stack_a);
