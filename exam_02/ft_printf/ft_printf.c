c87882e93f741d8a1dccbb75d10f72d2e0711f5d
Hiroaki-K4 <49no19@gmail.com>
2021-07-01 22:53:35 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/01 22:41:05 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/01 22:53:34 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,8 +89,8 @@ char			*ft_itoa_hex(unsigned int num, char *str)
 	ans[len] = '\0';
 	i = num;
 	len--;
-	if (i == 0)
-		ans[0] = '0';
+	// if (i == 0)
+		// ans[0] = '0';
 	while (len >= 0)
 	{
 		ans[len--] = str[i % 16];
@@ -116,6 +116,12 @@ char			*ft_itoa_hex(unsigned int num, char *str)
 // 		return (NULL);
 // 	ans[len] = '\0';
 // 	i = num;
+// 	if (i == 0)
+// 		ans[0] = '0';
+// 	while (len >= 0)
+// 	{
+		
+// 	}
 // }
 
 // int		digit_size(int num)
