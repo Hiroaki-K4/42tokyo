3f8030f9c164e6e126568055bbeab1aee51c6441
Hiroaki-K4 <49no19@gmail.com>
2021-07-01 23:22:11 +0900

    make push_b function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/01 23:21:43 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/01 23:22:09 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -171,8 +171,8 @@ int	main(int argc, char *argv[])
 	t_bi_list *stack_a;
 	t_bi_list *stack_b;
 	
-	if (!(stack_a = (t_bi_list *)malloc(sizeof(t_bi_list))))
-		return (-1);
+	// if (!(stack_a = (t_bi_list *)malloc(sizeof(t_bi_list))))
+	// 	return (-1);
 	stack_a->prev = NULL;
 	stack_a->next = NULL;
 	stack_a->start = 1;
