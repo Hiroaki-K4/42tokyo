39a6a58d37755406d3707ea95ae925f837b73391
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 20:35:44 +0900

    fix swap_a function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 20:33:01 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 20:35:28 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,8 +108,9 @@ int	main(int argc, char *argv[])
 	}
 	if (duplicate_check(argc, argv) == 1)
 		error_process();
-	push_a(&stack_a, &stack_b);
+	// push_a(&stack_a, &stack_b);
 	// push_b(&stack_a, &stack_b);
-	show_list(stack_b);
+	swap_a(stack_a);
+	// show_list(stack_b);
 	return (0);
 }
