ff17ca06f5b2ad28c755db5372e200e0298da0b6
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 11:29:58 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 11:28:00 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 11:29:56 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -609,8 +609,8 @@ int str_to_num(const char *arg, int *i, int flag)
 	int num;
 	int j;
 
-	if (ft_isdigit(arg[0]) == 0 && flag == 1)
-		return (0);
+	// if (ft_isdigit(arg[0]) == 0 && flag == 1)
+	// 	return (0);
 	num = ft_atoi(arg);
 	if (num >= 0)
 	{
