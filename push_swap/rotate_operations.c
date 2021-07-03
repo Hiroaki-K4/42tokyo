3a6d1c8ca30680cd63d41c20b715e2631b7820dd
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 22:19:53 +0900

    make rotate_a function

--- push_swap/rotate_operations.c
+++ push_swap/rotate_operations.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 22:04:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 22:18:43 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 22:19:50 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	rotate_a(t_bi_list **stack_a)
 			last->next = (*stack_a)->next;
 			(*stack_a)->next->next = NULL;
 			(*stack_a)->next = tmp;
+			write(1, "ra\n", 3);
 		}
 	}
-	printf("top: %d\n", (*stack_a)->data);
 }
