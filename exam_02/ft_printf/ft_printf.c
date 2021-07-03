a51bf87b60d835016f3c61e3a2642282e1555391
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 11:45:20 +0900

    test pft_exam

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 11:44:30 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 11:45:11 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -355,13 +355,11 @@ int	print_string(va_list *ap, t_plist flag_list)
 	return (len);
 }
 
-// int		str_to_num(const char *arg, int *i, int flag)
+// int		str_to_num(const char *arg, int *i)
 // {
 // 	int	num;
 // 	int	j;
 
-// 	if (ft_isdigit(arg[0]) == 0 && flag == 1)
-// 		return (0);
 // 	num = ft_atoi(arg);
 // 	if (num >= 0)
 // 	{
@@ -379,8 +377,6 @@ int str_to_num(const char *arg, int *i)
 	int num;
 	int j;
 
-	// if (ft_isdigit(arg[0]) == 0 && flag == 1)
-		// return (0);
 	num = ft_atoi(arg);
 	if (num >= 0)
 	{
@@ -403,12 +399,12 @@ int str_to_num(const char *arg, int *i)
 // 	t_plist	flag_list;
 
 // 	(*i)++;
-// 	flag_list.field = str_to_num(&arg[*i], i, 0);
+// 	flag_list.field = str_to_num(&arg[*i], i);
 // 	flag_list.precision = -1;
 // 	if (arg[*i] == '.')
 // 	{
 // 		(*i)++;
-// 		flag_list.precision = str_to_num(&arg[*i], i, 1);
+// 		flag_list.precision = str_to_num(&arg[*i], i);
 // 	}
 // 	flag_list.format = ft_strchr_place("sdx", arg[*i], i);
 // 	if (flag_list.format == 0)
