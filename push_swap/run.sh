f8ddd385a71c58bdf97b4ed873b046cdd55f5ca9
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 10:42:34 +0900

    add valgrind

--- push_swap/run.sh
+++ push_swap/run.sh
@@ -2,7 +2,8 @@
 
 make re
 ./push_swap 1 2 3 4 5
-valgrind ./push_swap 1 2 3 4 5
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./push_swap 1 2 3 4 5 | grep "ERROR SUMMARY"
+
 make fclean
 cd ..
 sh git.sh $*
