edd30fce9af2c92bd53f3275e9d1799c8ff871b3
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 22:46:09 +0900

    make reverse_rotate_a function

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 11:32:18 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 22:28:20 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 22:46:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ void	stack_init(t_bi_list **stack_a, t_bi_list **stack_b);
 void	rotate_a(t_bi_list **stack_a);
 void	rotate_b(t_bi_list **stack_b);
 void	rotate_ab(t_bi_list **stack_a, t_bi_list **stack_b);
+void	reverse_rotate_a(t_bi_list **stack_a);
 int	    digit_check(const char *arg);
 int	    duplicate_check(int argc, char *argv[]);
 t_bi_list	*last_stack(t_bi_list *stack);
