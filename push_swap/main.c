0fc50bbcf131835bba22413488a9e6f618dc00a3
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 21:33:52 +0900

    fix swap function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 21:33:33 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 21:33:50 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,9 +84,9 @@ int	main(int argc, char *argv[])
 	// push_a(&stack_a, &stack_b);
 	// push_b(&stack_a, &stack_b);
 	// swap_b(stack_a);
-	swap_b(stack_b);
-	// swap_ab(&stack_a, &stack_b);
-	// show_list(stack_a);
+	// swap_b(stack_b);
+	swap_ab(&stack_a, &stack_b);
+	show_list(stack_a);
 	show_list(stack_b);
 	return (0);
 }
