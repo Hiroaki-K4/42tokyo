498e5756606911d2fbc72900be10853aebc504ae
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 21:36:35 +0900

    fix push function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 21:33:50 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 21:36:26 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,11 +81,11 @@ int	main(int argc, char *argv[])
 	}
 	if (duplicate_check(argc, argv) == 1)
 		error_process();
-	// push_a(&stack_a, &stack_b);
+	push_a(&stack_a, &stack_b);
 	// push_b(&stack_a, &stack_b);
 	// swap_b(stack_a);
 	// swap_b(stack_b);
-	swap_ab(&stack_a, &stack_b);
+	// swap_ab(&stack_a, &stack_b);
 	show_list(stack_a);
 	show_list(stack_b);
 	return (0);
