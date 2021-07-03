e87ea899324a09db8a0c493634c90751562192b2
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 20:38:00 +0900

    make swap_b function

--- push_swap/list_operations.c
+++ push_swap/list_operations.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 18:34:36 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 20:22:31 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 20:37:50 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,16 @@ void	swap_a(t_bi_list *stack_a)
 	write(1, "sa\n", 3);
 }
 
-void	push_b(t_bi_list **stack_a, t_bi_list **stack_b)
+void	swap_b(t_bi_list *stack_b)
 {
-	t_bi_list *top;
-
-	top = (t_bi_list *)malloc(sizeof(t_bi_list));
-	top->data = (*stack_a)->data;
-	top->prev = NULL;
-	top->next = (*stack_b);
-	(*stack_b)->prev = top;
-	(*stack_b) = top;
-	top_del(stack_a);
-	show_list(*stack_b);
-	write(1, "pb\n", 3);
+	int	tmp;
+	if (stack_b && stack_b->next)
+	{
+		tmp = stack_b->data;
+		stack_b->data = stack_b->next->data;
+		stack_b->next->data = tmp;
+	}
+	write(1, "sb\n", 3);
 }
 
 void	push_a(t_bi_list **stack_a, t_bi_list **stack_b)
@@ -52,4 +49,19 @@ void	push_a(t_bi_list **stack_a, t_bi_list **stack_b)
 	top_del(stack_b);
 	show_list(*stack_a);
 	write(1, "pa\n", 3);
+}
+
+void	push_b(t_bi_list **stack_a, t_bi_list **stack_b)
+{
+	t_bi_list *top;
+
+	top = (t_bi_list *)malloc(sizeof(t_bi_list));
+	top->data = (*stack_a)->data;
+	top->prev = NULL;
+	top->next = (*stack_b);
+	(*stack_b)->prev = top;
+	(*stack_b) = top;
+	top_del(stack_a);
+	show_list(*stack_b);
+	write(1, "pb\n", 3);
 }
