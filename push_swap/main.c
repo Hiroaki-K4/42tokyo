d9a8a96cf067d63478238ee5ee6ae09dedd406dd
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 21:06:18 +0900

    fix add_stack function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 21:05:38 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 21:06:15 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,7 @@ int	add_stack(t_bi_list **stack, int num)
 	// {
 	new->data = num;
 	last = last_stack(*stack);
+	printf("last: %d\n", last->data);
 	new->prev = last;
 	new->next = NULL;
 	(*stack)->next = new;
