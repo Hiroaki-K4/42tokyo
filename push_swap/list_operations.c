33f71a899fc21971c8b4672f275144ed6ca2b284
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 21:24:32 +0900

    fix add_stack function

--- push_swap/list_operations.c
+++ push_swap/list_operations.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 18:34:36 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 20:40:31 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 21:24:29 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ void	swap_a(t_bi_list *stack_a)
 	int	tmp;
 	if (stack_a && stack_a->next)
 	{
-		tmp = stack_a->data;
-		stack_a->data = stack_a->next->data;
-		stack_a->next->data = tmp;
+		if (stack_a->next->next)
+		{
+			tmp = stack_a->data;
+			stack_a->data = stack_a->next->data;
+			stack_a->next->data = tmp;
+		}
 	}
 	write(1, "sa\n", 3);
 }
