eb7d4f3dcf565d8a2d8eed6a47453629d2bf97e5
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 21:50:54 +0900

    fix push function

--- push_swap/list_operations.c
+++ push_swap/list_operations.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 18:34:36 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 21:42:06 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 21:50:52 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,11 @@ void	push_a(t_bi_list **stack_a, t_bi_list **stack_b)
 	{
 		top = (t_bi_list *)malloc(sizeof(t_bi_list));
 		top->data = (*stack_b)->next->data;
-		top->prev = NULL;
-		top->next = (*stack_a);
+		top->prev = stack_a;
+		top->next = (*stack_a)->next;
 		printf("top: %d\n", top->data);
-		(*stack_a)->prev = top;
-		(*stack_a) = top;
+		(*stack_a)->next->prev = top;
+		// (*stack_a) = top;
 		top_del(stack_b);
 		show_list(*stack_a);
 		write(1, "pa\n", 3);
