7f819ec7b0064ca9638a5b27050931e88eaf6c4d
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 11:26:05 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 11:24:48 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 11:26:04 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -680,7 +680,7 @@ int ft_printf_per(const char *arg, int *i, va_list *ap)
 		(*i)++;
 		flag_list.precision = str_to_num(&arg[*i], i, 0);
 	}
-	flag_list.format = ft_printf_per("sdx", arg[*i], i);
+	flag_list.format = ft_strchr_place("sdx", arg[*i], i);
 	if (flag_list.format == 0)
 		len = print_string(ap, flag_list);
 	else if (flag_list.format == 1)
