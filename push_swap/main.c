a232145493003efc4bec41b72243e9df0bcfa5f2
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 17:27:11 +0900

    make push_b function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 17:26:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 17:27:07 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -168,17 +168,18 @@ int	main(int argc, char *argv[])
 {
 	int i;
 	int num;
-	t_bi_list stack_a_sub;
-	t_bi_list *stack_a = &stack_a_sub;
+	// t_bi_list stack_a_sub;
+	// t_bi_list *stack_a = &stack_a_sub;
+	t_bi_list *stack_a;
 	t_bi_list *stack_b;
 	
-	// if (!(stack_a = (t_bi_list *)malloc(sizeof(t_bi_list))))
-		// return (-1);
+	if (!(stack_a = (t_bi_list *)malloc(sizeof(t_bi_list))))
+		return (-1);
 	// stack_a = NULL;
 	// stack_b = NULL;
 	// stack_a = &stack_a_sub;
-	// stack_a->prev = NULL;
-	// stack_a->next = NULL;
+	stack_a->prev = NULL;
+	stack_a->next = NULL;
 	stack_a->start = 1;
 	stack_a->data = 0;
 	printf("ok\n");
