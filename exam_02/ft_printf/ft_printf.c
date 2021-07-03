ef30e95eb37b17c5b35412bbf2f482e9ac30c232
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 11:18:33 +0900

    add valgrind

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 11:16:29 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 11:18:32 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -722,7 +722,7 @@ int ft_printf_str(const char *arg, int *i)
 	int len;
 
 	len = 0;
-	while (arg[*i] != '%' && arg[*i])
+	while (arg[*i] && arg[*i] != '%')
 	{
 		write(1, &arg[*i], 1);
 		(*i)++;
