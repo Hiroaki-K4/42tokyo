aa7832c5a00123738477a7b4d3b2ff24dbdacbeb
Hiroaki-K4 <49no19@gmail.com>
2021-07-03 18:10:30 +0900

    add init process

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/03 18:10:22 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/03 18:10:29 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,7 +133,7 @@ void	push_b(t_bi_list **stack_a, t_bi_list **stack_b)
 	
 	// last_list(stack_b)->next = stack_a;
 	// stack_a = top_del(stack_a);
-	(*stack_b)->prev = (*stack_a));
+	(*stack_b)->prev = (*stack_a);
 	top_del(stack_a);
 	show_list(*stack_a);
 	show_list(*stack_b);
