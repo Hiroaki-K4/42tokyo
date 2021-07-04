75a1764644d8ef65e86f5ea2885afbcbf857a21c
Hiroaki-K4 <49no19@gmail.com>
2021-07-04 14:12:23 +0900

    make branch_process function

--- push_swap/run.sh
+++ push_swap/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 make re
-./push_swap 1 2 3 4 5
+# ./push_swap 1 2 3 4 5
+./push_swap
 # valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./push_swap 1 2 3 4 5 | grep "ERROR SUMMARY"
 
 make fclean
