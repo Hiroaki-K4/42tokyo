ee946645fe7bab991d9583a0698d95c8b3266cc3
Hiroaki-K4 <49no19@gmail.com>
2021-07-04 14:45:43 +0900

    make sort_three function

--- push_swap/run.sh
+++ push_swap/run.sh
@@ -2,7 +2,7 @@
 
 make re
 # ./push_swap 1 2 3 4 5
-./push_swap 3 2 1
+./push_swap 3 1 2
 # valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./push_swap 1 2 3 4 5 | grep "ERROR SUMMARY"
 
 make fclean
