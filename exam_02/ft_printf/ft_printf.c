959a7b1aebd06bffbd38c2f889243aae449a638a
Hiroaki-K4 <49no19@gmail.com>
2021-07-04 11:13:35 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/04 11:09:11 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/04 11:13:32 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -156,11 +156,38 @@ char			*ft_itoa(int n)
 	return (ans);
 }
 
-int		ft_atoi(const char *arg)
+// int		ft_atoi(const char *arg)
+// {
+// 	int		i;
+// 	int		flag;
+// 	int		ans;
+
+// 	flag = 1;
+// 	i = 0;
+// 	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
+// 			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
+// 		i++;
+// 	if (arg[i] == '-' || arg[i] == '+')
+// 	{
+// 		if (arg[i] == '-')
+// 			flag = -1;
+// 		i++;
+// 	}
+// 	ans = 0;
+// 	while (arg[i] >= '0' && arg[i] <= '9')
+// 	{
+// 		ans = (ans * 10) + (arg[i] - '0');
+// 		i++;
+// 	}
+// 	ans = ans * flag;
+// 	return (ans);
+// }
+
+int ft_atoi(const char *arg)
 {
-	int		i;
-	int		flag;
-	int		ans;
+	int ans;
+	int flag;
+	int i;
 
 	flag = 1;
 	i = 0;
