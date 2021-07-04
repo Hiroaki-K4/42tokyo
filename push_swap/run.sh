fcc214ab1e9bf7f6a85389f7f0cdb3045d3e43e9
Hiroaki-K4 <49no19@gmail.com>
2021-07-04 15:59:14 +0900

    make min_to_top function

--- push_swap/run.sh
+++ push_swap/run.sh
@@ -2,7 +2,7 @@
 
 make re
 # ./push_swap 1 2 3 4 5
-./push_swap 1 2 3
+./push_swap 5 1 3 4 2
 # valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./push_swap 1 2 3 4 5 | grep "ERROR SUMMARY"
 
 make fclean
