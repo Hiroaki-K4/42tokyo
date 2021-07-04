cc0368d2b3c5abacb21f1c5762d8948b698f5397
Hiroaki-K4 <49no19@gmail.com>
2021-07-04 16:28:58 +0900

    make min_to_top function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/04 16:27:43 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/04 16:28:56 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,11 +51,12 @@ int	min_data(t_bi_list *stack)
 void	min_to_top(t_bi_list **stack_a)
 {
 	int	min;
-	// while (stack_a)
-	
-	min = min_data((*stack_a)->next);
-	if ((*stack_a)->next->data != min)
-		rotate_a(stack_a);
+	while (stack_a->next->next != NULL)
+	{
+		min = min_data((*stack_a)->next);
+		if ((*stack_a)->next->data != min)
+			rotate_a(stack_a);
+	}
 	printf("min: %d\n", min);
 	show_list(*stack_a);
 }
