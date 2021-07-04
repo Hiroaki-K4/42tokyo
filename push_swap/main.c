1efdc991232bde25fbd3d36798a0aa8c1878bac8
Hiroaki-K4 <49no19@gmail.com>
2021-07-04 16:43:55 +0900

    make under_six function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/04 16:43:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/04 16:43:53 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ void	under_six(t_bi_list **stack_a, t_bi_list **stack_b, int argc)
 	i = 4;
 	while (i < argc)
 	{
-		min_to_top(stack_a);
+		// min_to_top(stack_a);
 		// push_b(stack_a, stack_b);
 		i++;
 	}
