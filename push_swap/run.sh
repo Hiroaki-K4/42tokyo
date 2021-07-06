d6c78646f878424d83d22024a8fe6ebaf39259df
Hiroaki-K4 <49no19@gmail.com>
2021-07-06 22:00:29 +0900

    quick_sort function

--- push_swap/run.sh
+++ push_swap/run.sh
@@ -2,7 +2,7 @@
 
 make re
 # ./push_swap 1 2 3 4 5
-./push_swap 1 3 2 8 4 5 9 6 7 
+./push_swap 3 2 8 4 5 9 6 7 1
 # valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./push_swap 1 2 3 4 5 | grep "ERROR SUMMARY"
 
 make fclean
