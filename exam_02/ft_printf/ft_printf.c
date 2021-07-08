a8072dbde479a105ed75df46061441e554655c6c
Hiroaki-K4 <49no19@gmail.com>
2021-07-08 23:16:44 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/08 23:16:28 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/08 23:16:43 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -739,7 +739,7 @@ int ft_printf_str(const char *arg, int *i)
 	len = 0;
 	while (arg[*i] != '%' && arg[*i])
 	{
-		write(1, &arg[*i], i);
+		write(1, &arg[*i], 1);
 		(*i)++;
 		len++;
 	}
