a07ca796ac8fee37423661f2ef798599611a65b2
Hiroaki-K4 <49no19@gmail.com>
2021-07-08 23:24:20 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/08 23:16:43 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/08 23:24:18 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -704,7 +704,7 @@ int ft_printf_per(const char *arg, int *i, va_list *ap)
 		if (!(str_num = ft_itoa(num)))
 			return (-1);
 		keta = ft_strlen(str_num);
-		if (num < 0)
+		if (keta < 0)
 			keta--;
 		len = print_digit(flag_list, str_num, num, keta);
 	}
