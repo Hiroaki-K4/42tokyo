d87b448fef4888721840e5677c99ba80c6d8f05b
Hiroaki-K4 <49no19@gmail.com>
2021-07-09 23:01:07 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/09 22:59:00 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/09 23:01:05 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -225,38 +225,11 @@ char *ft_itoa(int num)
 	return (ans);
 }
 
-// int		ft_atoi(const char *arg)
-// {
-// 	int		i;
-// 	int		flag;
-// 	int		ans;
-
-// 	flag = 1;
-// 	i = 0;
-// 	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
-// 			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
-// 		i++;
-// 	if (arg[i] == '-' || arg[i] == '+')
-// 	{
-// 		if (arg[i] == '-')
-// 			flag = -1;
-// 		i++;
-// 	}
-// 	ans = 0;
-// 	while (arg[i] >= '0' && arg[i] <= '9')
-// 	{
-// 		ans = (ans * 10) + (arg[i] - '0');
-// 		i++;
-// 	}
-// 	ans = ans * flag;
-// 	return (ans);
-// }
-
-int ft_atoi(const char *arg)
+int		ft_atoi(const char *arg)
 {
-	int i;
-	int ans;
-	int flag;
+	int		i;
+	int		flag;
+	int		ans;
 
 	flag = 1;
 	i = 0;
@@ -269,14 +242,41 @@ int ft_atoi(const char *arg)
 			flag = -1;
 		i++;
 	}
+	ans = 0;
 	while (arg[i] >= '0' && arg[i] <= '9')
 	{
-		ans = ans * 10 + (arg[i] - '0');
+		ans = (ans * 10) + (arg[i] - '0');
 		i++;
 	}
 	ans = ans * flag;
 	return (ans);
 }
+
+// int ft_atoi(const char *arg)
+// {
+// 	int i;
+// 	int ans;
+// 	int flag;
+
+// 	flag = 1;
+// 	i = 0;
+// 	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
+// 			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
+// 		i++;
+// 	if (arg[i] == '-' || arg[i] == '+')
+// 	{
+// 		if (arg[i] == '-')
+// 			flag = -1;
+// 		i++;
+// 	}
+// 	while (arg[i] >= '0' && arg[i] <= '9')
+// 	{
+// 		ans = ans * 10 + (arg[i] - '0');
+// 		i++;
+// 	}
+// 	ans = ans * flag;
+// 	return (ans);
+// }
 
 // int		ft_isdigit(int c)
 // {
@@ -347,11 +347,8 @@ int field_precision(t_plist flag_list, char *str_num, int num, int i)
 	{
 		if (!(tmp = (char *)malloc(sizeof(char) * (flag_list.precision + 1))))
 			return (-1);
-		while (flag_list.precision - (int)ft_strlen(str_num) - i > 0)
-		{
-			printf("ok\n");
+		while ((flag_list.precision - (int)ft_strlen(str_num) - i) > 0)
 			tmp[i++] = '0';
-		}
 		j = 0;
 	}
 	while (ft_strlen(str_num) - j > 0)
