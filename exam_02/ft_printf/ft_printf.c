7a226105b7dd366af784f01a34a5bd4af4e3a4fa
Hiroaki-K4 <49no19@gmail.com>
2021-07-09 23:01:34 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/09 23:01:05 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/09 23:01:33 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -225,11 +225,38 @@ char *ft_itoa(int num)
 	return (ans);
 }
 
-int		ft_atoi(const char *arg)
+// int		ft_atoi(const char *arg)
+// {
+// 	int		i;
+// 	int		flag;
+// 	int		ans;
+
+// 	flag = 1;
+// 	i = 0;
+// 	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
+// 			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
+// 		i++;
+// 	if (arg[i] == '-' || arg[i] == '+')
+// 	{
+// 		if (arg[i] == '-')
+// 			flag = -1;
+// 		i++;
+// 	}
+// 	ans = 0;
+// 	while (arg[i] >= '0' && arg[i] <= '9')
+// 	{
+// 		ans = (ans * 10) + (arg[i] - '0');
+// 		i++;
+// 	}
+// 	ans = ans * flag;
+// 	return (ans);
+// }
+
+int ft_atoi(const char *arg)
 {
-	int		i;
-	int		flag;
-	int		ans;
+	int i;
+	int ans;
+	int flag;
 
 	flag = 1;
 	i = 0;
@@ -245,38 +272,12 @@ int		ft_atoi(const char *arg)
 	ans = 0;
 	while (arg[i] >= '0' && arg[i] <= '9')
 	{
-		ans = (ans * 10) + (arg[i] - '0');
+		ans = ans * 10 + (arg[i] - '0');
 		i++;
 	}
 	ans = ans * flag;
 	return (ans);
 }
-
-// int ft_atoi(const char *arg)
-// {
-// 	int i;
-// 	int ans;
-// 	int flag;
-
-// 	flag = 1;
-// 	i = 0;
-// 	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
-// 			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
-// 		i++;
-// 	if (arg[i] == '-' || arg[i] == '+')
-// 	{
-// 		if (arg[i] == '-')
-// 			flag = -1;
-// 		i++;
-// 	}
-// 	while (arg[i] >= '0' && arg[i] <= '9')
-// 	{
-// 		ans = ans * 10 + (arg[i] - '0');
-// 		i++;
-// 	}
-// 	ans = ans * flag;
-// 	return (ans);
-// }
 
 // int		ft_isdigit(int c)
 // {
