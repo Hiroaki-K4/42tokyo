a31dcda1eeb69ddf85b3459cb766fdb10d030ace
Hiroaki-K4 <49no19@gmail.com>
2021-07-10 22:07:31 +0900

    make quick_sort function

--- push_swap/quick_sort_utils.c
+++ push_swap/quick_sort_utils.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 16:09:38 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/10 16:48:58 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/10 22:07:10 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,10 @@ int	find_pivot(t_bi_list *stack, int min, int max, int len)
 	{
 		if (stack->next->data != min && stack->next->data != max)
 			return (stack->next->data);
-		stack = stack->next;
+		if (stack->next->data == min ||stack->next->data == max)
+			stack = stack->prev;
+		else if (stack->prev->data == min ||stack->prev->data == max)
+			stack = stack->next;
 	}
 	if (stack->data != min && stack->data != max)
 		return (stack->data);
