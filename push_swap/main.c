beee4673e2d2c79eead4bf91bf35ee1ede379ba1
Hiroaki-K4 <49no19@gmail.com>
2021-07-10 21:07:39 +0900

    make quick_sort function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/10 21:06:45 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/10 21:07:37 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -159,6 +159,7 @@ void	quick_sort(t_bi_list **stack_a, t_bi_list **stack_b)
 			}
 		}
 	}
+	write(1, "end\n", 4);
 }
 
 void	branch_process(int argc, t_bi_list **stack_a, t_bi_list **stack_b)
