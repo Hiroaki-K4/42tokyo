4f95fd713788432a963ad6e6c8527818caf5605d
Hiroaki-K4 <49no19@gmail.com>
2021-07-10 15:39:12 +0900

    make quick_sort function

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 11:32:18 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/07 23:14:41 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/10 15:39:08 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ void		reverse_rotate_b(t_bi_list **stack_b);
 void		reverse_rotate_ab(t_bi_list **stack_a, t_bi_list **stack_b);
 void		under_six(t_bi_list **stack_a, t_bi_list **stack_b, int argc);
 void		sort_three(t_bi_list **stack_a);
+void	branch_process(int argc, t_bi_list **stack_a, t_bi_list **stack_b);
 int			digit_check(const char *arg);
 int			duplicate_check(int argc, char *argv[]);
 int			sorted_check(t_bi_list *stack_a);
