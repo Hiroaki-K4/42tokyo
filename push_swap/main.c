41b9c0b4aaab9f645e1d198a3f7a34aa2bdee225
Hiroaki-K4 <49no19@gmail.com>
2021-07-10 18:44:53 +0900

    make quick_sort function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/10 18:43:45 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/10 18:44:49 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -178,9 +178,6 @@ void	branch_process(int argc, t_bi_list **stack_a, t_bi_list **stack_b)
 		under_six(stack_a, stack_b, argc);
 	else
 		quick_sort(stack_a, stack_b);
-	show_list(*stack_a);
-	show_list(*stack_b);
-	printf("b_top: %d\n", (*stack_b)->data);
 	printf("branch\n");
 }
 
@@ -211,6 +208,7 @@ int	main(int argc, char *argv[])
 	if (duplicate_check(argc, argv) == 1)
 		error_process();
 	branch_process(argc, &stack_a, &stack_b);
+	show_list(stack_a);
 	// printf("b_top: %d\n", stack_b->data);
 	printf("final\n");
 	return (0);
