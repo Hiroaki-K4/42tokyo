7837fd0cd59a13b5fcf92cf81b4a4df82f9389b3
Hiroaki-K4 <49no19@gmail.com>
2021-07-11 22:37:13 +0900

    improve quick_sort function

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/11 22:36:57 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/11 22:37:11 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,6 +100,6 @@ int	main(int argc, char *argv[])
 	if (duplicate_check(argc, argv) == 1)
 		error_process();
 	switch_by_args_num(argc, &stack_a, &stack_b);
-	// show_list(stack_a);
+	show_list(stack_a);
 	return (0);
 }
