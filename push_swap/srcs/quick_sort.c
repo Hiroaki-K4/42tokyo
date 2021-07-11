dd5585ae118aed09bf1b2845217a7cee6b9e611f
Hiroaki-K4 <49no19@gmail.com>
2021-07-11 20:48:30 +0900

    improve quick_sort function

--- push_swap/srcs/quick_sort.c
+++ push_swap/srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 19:50:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/11 20:47:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/11 20:48:28 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int	stack_b_not_sorted(t_bi_list **stack_a, t_bi_list **stack_b)
 			rotate_a(stack_a);
 			len--;
 		}
+		show_list(*stack_a);
 		return (1);
 	}
 	else
