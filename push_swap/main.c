86fd31c64f8118b596e3ba5c2e6c54145b743951
Hiroaki-K4 <49no19@gmail.com>
2021-07-11 14:05:59 +0900

    make quick_sort function

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/11 14:01:36 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/11 14:05:52 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,35 @@ int	stack_b_not_sorted(t_bi_list **stack_a, t_bi_list **stack_b)
 	t_bi_list	*tmp;
 
 	len = stack_len(*stack_b);
-	if (len < 3)
+	if (len == 1)
+	{
+		push_a(stack_a, stack_b);
+		rotate_a(stack_a);
+	}
+	else if (len == 2)
 	{
 		write(1, "branch1\n", 8);
-		stack_init(&tmp);
-		show_list(*stack_a);
-		switch_by_args_num(len + 1, stack_b, &tmp);
-		i = 0;
-		// show_list(*stack_b);
-		write(1, "ok1\n", 4);
-		while (i < len)
-		{
-			write(1, "okk\n", 4);
-			push_a(stack_a, stack_b);
-			write(1, "ok2\n", 4);
-			rotate_a(stack_a);
-			write(1, "ok3\n", 4);
-			i++;
-		}
-		write(1, "ok4\n", 4);
+		// stack_init(&tmp);
+		// show_list(*stack_a);
+		// switch_by_args_num(len + 1, stack_b, &tmp);
+		// i = 0;
+		// write(1, "ok1\n", 4);
+		// while (i < len)
+		// {
+		// 	write(1, "okk\n", 4);
+		// 	push_a(stack_a, stack_b);
+		// 	write(1, "ok2\n", 4);
+		// 	rotate_a(stack_a);
+		// 	write(1, "ok3\n", 4);
+		// 	i++;
+		// }
+		// write(1, "ok4\n", 4);
+		if ((*stack_b)->next->data > (*stack_b)->next->next->data)
+			rotate_b(stack_b);
+		push_a(stack_a, stack_a);
+		push_a(stack_a, stack_a);
+		rotate_a(stack_a);
+		rotate_a(stack_a);
 		show_list(*stack_a);
 		write(1, "~~~~~\n", 6);
 		show_list(*stack_b);
