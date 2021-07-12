47f448f4cb43ef2bb96a344badbcf929bb62135f
Hiroaki-K4 <49no19@gmail.com>
2021-07-12 22:17:13 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/11 11:25:58 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/12 22:17:07 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -770,20 +770,20 @@ int ft_printf_str(const char *arg, int *i)
 
 int ft_printf(const char *arg, ...)
 {
-	va_list ap;
 	int i;
 	int j;
 	int print_len;
+	va_list ap;
 
 	va_start(ap, arg);
 	i = 0;
+	print_len = 0;
 	if (arg == NULL)
 		i = -1;
-	print_len = 0;
 	while (i >= 0 && arg[i])
 	{
 		if (arg[i] != '%')
-			print_len = ft_printf_str(arg, &i);
+			print_len += ft_printf_str(arg, &i);
 		else
 		{
 			j = ft_printf_per(arg, &i, &ap);
