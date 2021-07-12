a94906d1330091075bca4a0a28edc9ffd04adc67
Hiroaki-K4 <49no19@gmail.com>
2021-07-12 23:17:21 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/12 23:10:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/12 23:17:04 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -473,18 +473,21 @@ int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 	int len;
 
 	len = 0;
-	if (flag_list.precision == 0 && num == 0)
+	if (flag_list.precision != 0 && num == 0)
 	{
-		while (flag_list.field - (len++) > 0)
+		while (flag_list.field - len > 0)
+		{
 			write(1, " ", 1);
+			len++;
+		}
 	}
-	else if (flag_list.field  > (int)ft_strlen(str_num))
+	else if (flag_list.field > (int)ft_strlen(str_num))
 	{
 		if (flag_list.precision > keta)
 			len = field_precision(flag_list, str_num, num, 0);
 		else
 		{
-			while (flag_list.field - (int)ft_strlen(str_num) - (len++) > 0)
+			while ((flag_list.field - (int)ft_strlen(str_num) - (len++) > 0))
 				write(1, " ", 1);
 			write(1, str_num, ft_strlen(str_num));
 			len = flag_list.field;
