cbf6e357153b8438d48153fac4fc40079a515444
Hiroaki-K4 <49no19@gmail.com>
2021-07-17 22:20:47 +0900

    make pivot list

--- push_swap/srcs/operate_pivot.c
+++ push_swap/srcs/operate_pivot.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 22:02:40 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/17 22:20:32 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/17 22:20:45 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,7 @@ int	add_pivot(int **pivot_list, int pivot)
 		*pivot_list[0] = pivot;
 		write(1, "ok5\n", 4);
 		*pivot_list[1] = '\0';
+		write(1, "ok6\n", 4);
 	}
 	else
 	{
