2b850471938beb17b7b90b5ed90ff888ef867582
Hiroaki-K4 <49no19@gmail.com>
2021-07-17 22:19:52 +0900

    make pivot list

--- push_swap/srcs/operate_pivot.c
+++ push_swap/srcs/operate_pivot.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 22:02:40 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/17 22:15:57 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/17 22:19:50 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ int	add_pivot(int **pivot_list, int pivot)
 
 	if (*pivot_list == NULL)
 	{
+		write(1, "ok3\n", 4);
 		if (!(*pivot_list = (int *)malloc(sizeof(int) * 2)))
 			return (-1);
 		*pivot_list[0] = pivot;
