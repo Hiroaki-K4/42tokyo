2e597123ca76fc159fe90b2a2a641d1eb34f734b
Hiroaki-K4 <49no19@gmail.com>
2021-07-17 10:20:01 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/17 10:03:48 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/17 10:19:53 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -257,7 +257,7 @@ int ft_atoi(const char *arg)
 	int i;
 	int ans;
 	int flag;
-
+	
 	i = 0;
 	flag = 1;
 	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
@@ -272,7 +272,7 @@ int ft_atoi(const char *arg)
 	ans = 0;
 	while (arg[i] >= '0' && arg[i] <= '9')
 	{
-		ans = ans * 10 + (arg[i] - '0');
+		ans = (ans * 10) + (arg[i] - '0');
 		i++;
 	}
 	ans = ans * flag;
