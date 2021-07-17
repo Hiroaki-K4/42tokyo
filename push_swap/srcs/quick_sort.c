fb1da7a339b30a22387183128371965b2ff29251
Hiroaki-K4 <49no19@gmail.com>
2021-07-17 22:16:51 +0900

    make pivot list

--- push_swap/srcs/quick_sort.c
+++ push_swap/srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 19:50:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/17 22:09:42 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/17 22:16:48 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -172,9 +172,9 @@ void	quick_sort(t_bi_list **stack_a, t_bi_list **stack_b)
 {
 	int top_flag;
 	int all_min;
-	int	**pivot_list;
+	int	*pivot_list;
 
-	*pivot_list = NULL;
+	pivot_list = NULL;
 	top_flag = 0;
 	all_min = stack_min(*stack_a);
 	while (sorted_check(*stack_a) == 1 || (*stack_b)->next != NULL)
@@ -205,7 +205,7 @@ void	quick_sort(t_bi_list **stack_a, t_bi_list **stack_b)
 		else
 		{
 			if (top_flag == 0)
-				first_partition(stack_a, stack_b, pivot_list);
+				first_partition(stack_a, stack_b, &pivot_list);
 			else
 				partition(stack_a, stack_b, all_min);
 		}
