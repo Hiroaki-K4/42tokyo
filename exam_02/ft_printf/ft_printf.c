008b3dc223f872052fd4d72eed18308860e0c0d5
Hiroaki-K4 <49no19@gmail.com>
2021-07-17 10:03:48 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/17 09:51:27 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/17 10:03:48 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -339,6 +339,7 @@ int field_precision(t_plist flag_list, char *str_num, int num, int i)
 		if (!(tmp = (char *)malloc(sizeof(char) * (flag_list.precision + 2))))
 			return (-1);
 		tmp[0] = '-';
+		i = 1;
 		while (flag_list.precision + 2 - (int)ft_strlen(str_num) - i > 0)
 			tmp[i++] = '0';
 		j = 1;
@@ -354,6 +355,7 @@ int field_precision(t_plist flag_list, char *str_num, int num, int i)
 	while (ft_strlen(str_num) - j > 0)
 		tmp[i++] = str_num[j++];
 	tmp[i] = '\0';
+	i = 0;
 	while (flag_list.field - (int)ft_strlen(tmp) - (i++) > 0)
 		write(1, " ", 1);
 	write(1, tmp, ft_strlen(tmp));
