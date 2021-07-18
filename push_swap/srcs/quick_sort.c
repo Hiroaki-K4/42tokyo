2cad3836ba002b438ef1f3891c2280dc0c796df9
Hiroaki-K4 <49no19@gmail.com>
2021-07-18 17:53:23 +0900

    make pivot_list

--- push_swap/srcs/quick_sort.c
+++ push_swap/srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 19:50:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/18 17:52:45 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/18 17:53:21 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -210,7 +210,7 @@ void	quick_sort(t_bi_list **stack_a, t_bi_list **stack_b)
 		i = 0;
 		while (pivot_list->len - i > 0)
 		{
-			printf("pivot%d: %d\n", i+1, pivot_list->pivot[i]);
+			// printf("pivot%d: %d\n", i+1, pivot_list->pivot[i]);
 			i++;
 		}
 	}
