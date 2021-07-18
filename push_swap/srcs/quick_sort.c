97233f1e3fc5e6f81beeafdd461a986b6dde2647
Hiroaki-K4 <49no19@gmail.com>
2021-07-18 16:06:43 +0900

    make pivot_list

--- push_swap/srcs/quick_sort.c
+++ push_swap/srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 19:50:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/18 16:03:00 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/18 16:06:40 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ int	stack_b_not_sorted(t_bi_list **stack_a, t_bi_list **stack_b)
 		i = 0;
 		while (i < len)
 		{
-			if ((*stack_b)->next->data >= pivot)
+			if ((*stack_b)->next->data > pivot)
 				push_a(stack_a, stack_b);
 			else
 				rotate_b(stack_b);
@@ -87,7 +87,7 @@ void	first_partition(t_bi_list **stack_a, t_bi_list **stack_b, t_pivot_list **pi
 	i = 0;
 	while (i < len)
 	{
-		if ((*stack_a)->next->data <= pivot)
+		if ((*stack_a)->next->data < pivot)
 			push_b(stack_a, stack_b);
 		else
 			rotate_a(stack_a);
