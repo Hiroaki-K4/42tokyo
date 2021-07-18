9b5be429c3c1ba0f7a5343e10d9456e2314dbb91
Hiroaki-K4 <49no19@gmail.com>
2021-07-18 18:05:15 +0900

    make pivot_list

--- push_swap/srcs/stack_min_max.c
+++ push_swap/srcs/stack_min_max.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 23:12:15 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/18 18:03:54 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/18 18:05:11 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ int	stack_min_limit(t_bi_list *stack, int all_min, t_pivot_list **pivot_list)
 {
 	int	min;
 
+	printf("min_pivot: %d\n", (*pivot_list)->pivot[(*pivot_list)->len - 1]);
 	min = stack->next->data;
 	while (stack->next != NULL && stack->next->data != all_min)
 	{
