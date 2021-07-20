2792b0407471487250250e85db2e43d0a2e15ad7
Hiroaki-K4 <49no19@gmail.com>
2021-07-21 08:28:20 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/20 22:55:46 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/21 08:28:20 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -406,7 +406,7 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 {
 	int keta;
 	char *tmp;
-
+	
 	keta = ft_strlen(str_num);
 	if (num < 0)
 		keta--;
@@ -415,9 +415,9 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 		len = flag_list.precision;
 		if (num < 0)
 		{
-			write(1, "-", 1);
 			if (!(tmp = ft_itoa(num * (-1))))
 				return (-1);
+			write(1, "-", 1);
 			while (flag_list.precision - (keta++) > 0)
 				write(1, "0", 1);
 			write(1, tmp, ft_strlen(tmp));
@@ -427,7 +427,7 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 		{
 			while (flag_list.precision - (keta++) > 0)
 				write(1, "0", 1);
-			write(1, str_num, ft_strlen(str_num));
+			write(1, tmp, ft_strlen(tmp));
 		}
 	}
 	else
