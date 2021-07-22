ad291bd1d0a88f435719c8cc47fdeb1ea35377d0
Hiroaki-K4 <49no19@gmail.com>
2021-07-22 11:14:39 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/22 11:10:58 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/22 11:14:38 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -474,13 +474,11 @@ int print_digit(t_plist flag_list, char *str_num, int num, int keta)
 {
 	int len;
 	
+	len = 0;
 	if (flag_list.precision == 0 && num == 0)
 	{
 		while (flag_list.field - (len++) > 0)
-		{
 			write(1, " ", 1);
-			len++;
-		}
 	}
 	else if (flag_list.field > (int)ft_strlen(str_num))
 	{
