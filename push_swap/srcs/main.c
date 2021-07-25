53d0c2049e3ac80149a434e8957c566dbb47a668
Hiroaki-K4 <49no19@gmail.com>
2021-07-25 18:33:27 +0900

    make pre-process for quick_sort

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,28 +6,34 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/25 18:26:23 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/25 18:33:25 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	make_sorted_list(t_bi_list *stack, int **sorted_list)
+void	make_sorted_list(t_bi_list **stack, int **sorted_list)
 {
 	int	i;
+	int	len;
 
+	len = stack_len(*stack);
+	printf("len: %d\n", len);
+	if (!(*sorted_list = (int *)malloc(sizeof(int) * (len))))
+		exit(1);
 	i = 0;
-	while (stack->next != NULL)
+	while ((*stack)->next != NULL)
 	{
-		*sorted_list[i] = stack->next->data;
-		stack = stack->next;
+		printf("i: %d\n", i);
+		*sorted_list[i] = (*stack)->next->data;
+		(*stack) = (*stack)->next;
 		i++;
 	}
 }
 
 void	switch_by_args_num(int argc, t_bi_list **stack_a, t_bi_list **stack_b)
 {
-	int	sorted_list[argc - 1];
+	int	*sorted_list;
 
 	if (argc <= 2)
 		return ;
@@ -45,7 +51,7 @@ void	switch_by_args_num(int argc, t_bi_list **stack_a, t_bi_list **stack_b)
 	else
 	{
 		quick_sort(stack_a, stack_b, 0);
-		make_sorted_list(*stack_a, &sorted_list);
+		// make_sorted_list(stack_a, &sorted_list);
 		quick_sort(stack_a, stack_b, 1);
 	}
 }
