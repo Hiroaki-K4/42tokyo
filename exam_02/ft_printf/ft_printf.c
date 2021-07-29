a5cad5776cffc942683b156422bd5f05a99806b6
Hiroaki-K4 <49no19@gmail.com>
2021-07-29 09:03:38 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/29 08:58:30 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/29 09:03:15 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -622,6 +622,22 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 // 	return (print_len);
 // }
 
+int str_to_num(const char *arg, int *i)
+{
+	int num;
+	int j;
+
+	num = ft_atoi(arg);
+	if (num >= 0)
+	{
+		j = 0;
+		while (ft_isdigit(arg[j++]))
+			(*i)++;
+	}
+	else
+		num = -1;
+	return (num);
+}
 
 int ft_printf_per(const char *arg, int *i, va_list *ap)
 {
