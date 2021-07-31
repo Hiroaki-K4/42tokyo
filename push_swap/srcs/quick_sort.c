0f9df5e39724d061a8a176960dfd53f0596914cd
Hiroaki-K4 <49no19@gmail.com>
2021-07-31 14:10:56 +0900

    fix memory leak

--- push_swap/srcs/quick_sort.c
+++ push_swap/srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 19:50:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/31 14:10:28 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/31 14:10:52 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -214,4 +214,5 @@ void	quick_sort(t_bi_list **stack_a, t_bi_list **stack_b, int output_flag, int *
 		}
 	}
 	free(pivot_list->pivot);
+	free(pivot_list);
 }
