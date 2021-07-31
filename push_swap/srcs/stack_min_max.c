6e01e036f4642f324dc8b53ecf0a85202eed7bc4
Hiroaki-K4 <49no19@gmail.com>
2021-07-31 13:46:23 +0900

    fix memory leak

--- push_swap/srcs/stack_min_max.c
+++ push_swap/srcs/stack_min_max.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 23:12:15 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/30 23:08:18 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/31 13:46:18 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int	stack_min_limit(t_bi_list *stack, int all_min, t_pivot_list **pivot_list)
 	min = stack->next->data;
 	while (stack->next != NULL && stack->next->data != all_min)
 	{
-		if (stack->next->data == (*pivot_list)->pivot[(*pivot_list)->len - 1])
+		if ((*pivot_list)->len > 0 && stack->next->data == (*pivot_list)->pivot[(*pivot_list)->len - 1])
 		{
 			if (min > stack->next->data)
 			{
