e5580e649972c4ec70d0fa1754bfa77c30c69e39
Hiroaki-K4 <49no19@gmail.com>
2021-07-31 16:47:13 +0900

    fix for norminette

--- push_swap/srcs/quick_sort.c
+++ push_swap/srcs/quick_sort.c
@@ -6,11 +6,34 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 19:50:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/31 16:43:00 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/31 16:47:08 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	stack_b_len_three(t_bi_list **stack_a, t_bi_list **stack_b,
+	t_pivot_list **pivot_list, t_sort_tool sort_tool)
+{
+	int	len;
+	int	min;
+
+	len = stack_len(*stack_b);
+	while (len > 0)
+	{
+		min = stack_min(*stack_b);
+		if ((*stack_b)->next->data == min)
+		{
+			push_a(stack_a, stack_b, sort_tool.output_flag);
+			rotate_a(stack_a, sort_tool.output_flag);
+			len--;
+		}
+		else
+			rotate_b(stack_b, sort_tool.output_flag);
+	}
+	delete_min_pivot(pivot_list);
+	return (1);
+}
 
 int	stack_b_not_sorted(t_bi_list **stack_a, t_bi_list **stack_b,
 	t_pivot_list **pivot_list, t_sort_tool sort_tool)
@@ -26,20 +49,21 @@ int	stack_b_not_sorted(t_bi_list **stack_a, t_bi_list **stack_b,
 		return (stack_b_len_two(stack_a, stack_b, pivot_list, sort_tool));
 	else if (len < 4)
 	{
-		while (len > 0)
-		{
-			min = stack_min(*stack_b);
-			if ((*stack_b)->next->data == min)
-			{
-				push_a(stack_a, stack_b, sort_tool.output_flag);
-				rotate_a(stack_a, sort_tool.output_flag);
-				len--;
-			}
-			else
-				rotate_b(stack_b, sort_tool.output_flag);
-		}
-		delete_min_pivot(pivot_list);
-		return (1);
+		return (stack_b_len_three(stack_a, stack_b, pivot_list, sort_tool));
+		// while (len > 0)
+		// {
+		// 	min = stack_min(*stack_b);
+		// 	if ((*stack_b)->next->data == min)
+		// 	{
+		// 		push_a(stack_a, stack_b, sort_tool.output_flag);
+		// 		rotate_a(stack_a, sort_tool.output_flag);
+		// 		len--;
+		// 	}
+		// 	else
+		// 		rotate_b(stack_b, sort_tool.output_flag);
+		// }
+		// delete_min_pivot(pivot_list);
+		// return (1);
 	}
 	else
 	{
