5665b03efd1f596252b534a751ff7651c0861ee8
Hiroaki-K4 <49no19@gmail.com>
2021-07-31 14:24:12 +0900

    make error process

--- push_swap/srcs/operate_pivot.c
+++ push_swap/srcs/operate_pivot.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 22:02:40 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/31 14:11:20 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/31 14:24:08 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,9 @@ int	find_pivot(t_bi_list *stack, int min, int max, int len)
 int	init_pivot_list(t_pivot_list **pivot_list, int all_min)
 {
 	(*pivot_list)->len = 0;
-	if (!((*pivot_list)->pivot = (int *)malloc(sizeof(int) * 1)))
-		return (-1);
+	(*pivot_list)->pivot = (int *)malloc(sizeof(int) * 1);
+	if (!(*pivot_list)->pivot)
+		exit(1);
 	(*pivot_list)->pivot[0] = all_min;
 	return (0);
 }
