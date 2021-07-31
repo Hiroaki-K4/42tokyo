febd6b46adfacb9692e95ab998cee668ff61511d
Hiroaki-K4 <49no19@gmail.com>
2021-07-31 16:57:09 +0900

    fix for norminette

--- push_swap/srcs/quick_sort.c
+++ push_swap/srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 19:50:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/31 16:55:41 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/31 16:56:56 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,7 @@ int	stack_b_not_sorted(t_bi_list **stack_a, t_bi_list **stack_b,
 		add_pivot(pivot_list, pivot);
 		i = -1;
 		while (++i < len)
-		{
 			stack_b_over_three(stack_a, stack_b, pivot, sort_tool);
-			// if ((*stack_b)->next->data >= pivot)
-			// 	push_a(stack_a, stack_b, sort_tool.output_flag);
-			// else
-			// 	rotate_b(stack_b, sort_tool.output_flag);
-		}
 		return (0);
 	}
 }
