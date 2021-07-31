fd8aef5f4f7d74caffd541dc252444291a08be22
Hiroaki-K4 <49no19@gmail.com>
2021-07-31 17:22:34 +0900

    fix for norminette

--- push_swap/srcs/quick_sort.c
+++ push_swap/srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 19:50:54 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/31 17:22:14 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/31 17:22:29 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,7 +133,7 @@ void	quick_sort(t_bi_list **stack_a, t_bi_list **stack_b, int output_flag,
 	// pivot_list = (t_pivot_list *)malloc(sizeof(t_pivot_list));
 	// if (!pivot_list)
 	// 	exit(1);
-	pivot_list = init_pivot_list(&pivot_list, sort_tool.all_min);
+	pivot_list = init_pivot_list(sort_tool.all_min);
 	while (sorted_check(*stack_a) == 1 || (*stack_b)->next != NULL)
 	{
 		if (top_flag == 1 && (*stack_a)->next->data == sort_tool.all_min
