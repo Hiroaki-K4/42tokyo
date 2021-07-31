62b71276b48011f5a35990cef9e1c37257cf3c22
Hiroaki-K4 <49no19@gmail.com>
2021-07-31 14:07:22 +0900

    fix memory leak

--- push_swap/srcs/operate_pivot.c
+++ push_swap/srcs/operate_pivot.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 22:02:40 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/31 14:06:31 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/31 14:07:18 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,6 +111,7 @@ t_pivot_list	*delete_min_pivot(t_pivot_list *pivot_list)
 	if (!(new_pivot_list = (int *)malloc(sizeof(int) * (pivot_list->len - 1))))
 	{
 		free(pivot_list->pivot);
+		free(new_pivot_list);
 		exit (1);
 	}
 	i = 0;
