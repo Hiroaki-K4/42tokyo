d235442fa601badf0c92c2e98618b910ada652b1
Hiroaki-K4 <49no19@gmail.com>
2021-07-31 14:30:08 +0900

    make error process

--- push_swap/srcs/end_process.c
+++ push_swap/srcs/end_process.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 18:30:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/07/31 11:31:31 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/07/31 14:30:04 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	error_process()
 {
 	write(1, "Error\n", 6);
-	exit(0);
+	exit(1);
 }
 
 void	free_stack(t_bi_list **stack)
