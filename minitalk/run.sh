57ff99e091594bc6173efe3ea155d337bddc7693
Hiroaki-K4 <49no19@gmail.com>
2021-08-01 22:29:58 +0900

    make run.sh

--- minitalk/run.sh
+++ minitalk/run.sh
@@ -3,11 +3,11 @@
 make re
 # # gcc server.c -o server
 # # gcc client.c -o client
-# ./server &
-# ps --no-heading -C server -o pid
+./server &
+ps --no-heading -C server -o pid
 # ./client pid
-# # echo ${pid}
-# # kill ${pid}
+echo ${pid}
+kill ${pid}
 make fclean
 cd ..
 sh git.sh $*
