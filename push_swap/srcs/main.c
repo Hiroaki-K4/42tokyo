9b24760e8fcef27b3a8849903131133f4815bb12
Hiroaki-K4 <49no19@gmail.com>
2021-08-01 14:05:20 +0900

    fix the error about small number

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:51:51 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/01 13:54:15 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/01 14:05:17 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,18 +34,11 @@ void	make_sorted_list(t_bi_list *stack, int **sorted_list)
 void	few_args_process(int argc, t_bi_list **stack_a, t_bi_list **stack_b)
 {
 	if (argc <= 2)
-	{
-		printf("ok1\n");
 		return ;
-	}
 	else if (sorted_check(*stack_a) == 0)
-	{
-		printf("ok2\n");
 		return ;
-	}
 	else if (argc == 3)
 	{
-		// printf("ok\n");
 		if ((*stack_a)->next->data > (*stack_a)->next->next->data)
 			swap_a(*stack_a, 1);
 	}
