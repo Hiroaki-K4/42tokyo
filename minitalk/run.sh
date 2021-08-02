1775db1e8e28c2da5e75c2a2533ab4ff5137068f
Hiroaki-K4 <49no19@gmail.com>
2021-08-02 22:12:05 +0900

    test ft_printf

--- minitalk/run.sh
+++ minitalk/run.sh
@@ -5,10 +5,12 @@ make re
 # # gcc client.c -o client
 ./server &
 ps --no-heading -C server -o pid
-# ./client pid
-echo ${pid}
-kill ${pid}
+echo "aaa"
+./client pid
+# echo ${pid}
+# kill ${pid}
 make fclean
 cd ..
 sh git.sh $*
 cd minitalk
+echo "aaa"
