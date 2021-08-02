7db8b0f5a133d185d4921ca576afe75f875c8099
Hiroaki-K4 <49no19@gmail.com>
2021-08-02 22:44:38 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/02 22:43:27 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/02 22:44:36 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -246,6 +246,21 @@ char	*ft_strdup(const char *s)
 // 	}
 // 	return (len);
 // }
+
+int digit_size(int num)
+{
+	int len;
+
+	len = 0;
+	if (num <= 0)
+		len++;
+	while (num != 0)
+	{
+		num = num / 10;
+		len++;
+	}
+	return (len);
+}
 
 char *ft_itoa(int num)
 {
