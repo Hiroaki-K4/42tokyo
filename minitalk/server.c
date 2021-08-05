9e991a6d2b9f0293da16d9dc65527002f78724e9
Hiroaki-K4 <49no19@gmail.com>
2021-08-05 23:16:49 +0900

    make bit store process

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 18:27:23 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/05 23:13:56 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/05 23:16:35 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	sigusr_handler(int sig, siginfo_t *info, void *ucontext)
 		new_bit = 0;
 	else if (sig == 12)
 		new_bit = 1;
-	end_pos = 0;
+	end_pos = -1;
 	i = 0;
 	while (i < 7)
 	{
