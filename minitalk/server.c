995236d2319422475c407ad8ddd3d8c4145d8a43
Hiroaki-K4 <49no19@gmail.com>
2021-08-05 23:11:25 +0900

    make bit store process

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 18:27:23 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/05 23:10:52 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/05 23:11:24 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,9 +46,9 @@ void	sigusr_handler(int sig, siginfo_t *info, void *ucontext)
 		printf("SIGUSR1\n");
 	else if (sig == 12)
 		printf("SIGUSR2\n");
-	printf("ucontext: %p\n", ucontext);
-	printf("signo: %d si_code: %d\n", info->si_signo, info->si_code);
-	printf("si_pid: %d si_uid: %d\n", (int)info->si_pid, (int)info->si_uid);
+	// printf("ucontext: %p\n", ucontext);
+	// printf("signo: %d si_code: %d\n", info->si_signo, info->si_code);
+	// printf("si_pid: %d si_uid: %d\n", (int)info->si_pid, (int)info->si_uid);
 }
 
 int	main(int argc, char *argv[])
