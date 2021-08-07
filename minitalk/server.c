e51bee8bdddd98d9c0facc36937a20d369a02eca
Hiroaki-K4 <49no19@gmail.com>
2021-08-07 22:17:26 +0900

    make char output process

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 18:27:23 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/07 22:16:42 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/07 22:17:24 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,7 +112,7 @@ void	sigusr_handler(int sig, siginfo_t *info, void *ucontext)
 
 int	main(int argc, char *argv[])
 {
-	printf("argc: %d argv[0] %s\n", argc, argv[0]);
+	// printf("argc: %d argv[0] %s\n", argc, argv[0]);
 	struct sigaction sigusr;
 	ft_bzero(&sigusr, sizeof(struct sigaction));
 	sigusr.sa_sigaction = sigusr_handler;
