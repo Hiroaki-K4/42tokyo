5c73052410b7b5f10a07da6afcc5e43b23cd04e5
Hiroaki-K4 <49no19@gmail.com>
2021-08-07 20:57:42 +0900

    make bit store list

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 18:27:23 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/07 20:50:33 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/07 20:57:40 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,8 +103,8 @@ void	sigusr_handler(int sig, siginfo_t *info, void *ucontext)
 		printf("%i", bit_store[i]);
 		i++;
 	}
-	printf("ucontext: %p\n", ucontext);
-	printf("signo: %d si_code: %d\n", info->si_signo, info->si_code);
+	// printf("ucontext: %p\n", ucontext);
+	// printf("signo: %d si_code: %d\n", info->si_signo, info->si_code);
 }
 
 int	main(int argc, char *argv[])
