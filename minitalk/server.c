b30b18910bdb26748af705656fb7faee9b4399c2
Hiroaki-K4 <49no19@gmail.com>
2021-08-07 18:18:45 +0900

    make bit store list

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 18:27:23 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/07 18:17:39 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/07 18:18:36 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,15 +55,7 @@ void	sigusr_handler(int sig, siginfo_t *info, void *ucontext)
 	end_pos = get_end_pos();
 	printf("end_pos: %d\n", end_pos);
 	if (end_pos == -1)
-	{
-		// i = 0;
-		// while (i < 7)
-		// {
-		// 	printf("%i", bit_store[i]);
-		// 	i++;
-		// }
 		init_bit_store();
-	}
 	else
 		bit_store[end_pos] = new_bit;
 	i = 0;
