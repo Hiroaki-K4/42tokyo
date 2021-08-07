00b60d2effb20dea92b83cb8c52bf28cbd5e0155
Hiroaki-K4 <49no19@gmail.com>
2021-08-07 21:40:04 +0900

    make char output process

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 18:27:23 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/07 21:37:56 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/07 21:39:58 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,7 +111,7 @@ void	sigusr_handler(int sig, siginfo_t *info, void *ucontext)
 	// 	i++;
 	// }
 	// printf("ok\n");
-	info->si_signo = 1;
+	(void)info->si_signo;
 	// (void)ucontext;
 	// UNUSED(ucontext);
 	printf("\nucontext: %p\n", ucontext);
