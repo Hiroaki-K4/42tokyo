9c4c62da6ed87c4b96e88141ab006672db79fd5c
Hiroaki-K4 <49no19@gmail.com>
2021-08-08 16:20:24 +0900

    fix send_char function

--- minitalk/run.sh
+++ minitalk/run.sh
@@ -5,7 +5,8 @@ make re
 # # gcc client.c -o client
 ./server &
 pid=$(ps --no-heading -C server -o pid)
-./client $pid Hello
+# ./client $pid Hello
+./client $pid あああ
 # echo ${pid}
 kill $pid
 make fclean
