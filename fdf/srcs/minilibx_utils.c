954427df0844793a232c5fed5e4606242b5b9eff
Hiroaki-K4 <49no19@gmail.com>
2021-08-09 21:37:01 +0900

    make minilibx init process

--- fdf/srcs/minilibx_utils.c
+++ fdf/srcs/minilibx_utils.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 20:21:45 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/09 20:22:13 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/09 21:36:57 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void	draw(t_info *info)
 {
-	(void)info;
+	my_mlx_pixel_put(&info.img, 5, 5, 0x00FF0000);
+	my_mlx_pixel_put(&info.img, 10, 10, 0x00FF0000);
 }
 
 int	win_close(t_info *info)
