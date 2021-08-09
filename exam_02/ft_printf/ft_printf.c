26ae4cdfb01ac00013140b15f91e00b0efcd87a1
Hiroaki-K4 <49no19@gmail.com>
2021-08-09 11:12:53 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/09 11:10:01 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/09 11:12:48 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -624,6 +624,28 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 // 	va_end(ap);
 // 	return (print_len);
 // }
+
+int print_digit(t_plist flag_list, char *str_num, int num, int keta)
+{
+	int len;
+
+	len = 0;
+	if (flag_list.field > (int)ft_strlen(str_num))
+	{
+		if (flag_list.precision > keta)
+			len = field_precision(flag_list, str_num, num, 0);
+		else
+		{
+			while (flag_list.field - (int)ft_strlen(str_num) - (len++) > 0)
+				write(1, " ", 1);
+			write(1, str_num, ft_strlen(str_num));
+			len = flag_list.field;
+		}
+	}
+	else
+		len = no_field_int(flag_list, str_num, num, 0);
+	return (len);
+}
 
 int print_string(va_list *ap, t_plist flag_list)
 {
