b41fcdfdab5ba2d3fc0f41b2ff29fad8e7836d1f
Hiroaki-K4 <49no19@gmail.com>
2021-08-10 21:40:20 +0900

    make draw function

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/10 21:39:02 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/10 21:40:11 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	draw(t_info *info)
 			my_mlx_pixel_put(&info->img, info->x_start + j, info->y_start + i, 0x00FF0000);
 			j++;
 		}
-		i++;
+		i += info->y_step;
 	}
 	// my_mlx_pixel_put(&info->img, 5, 5, 0x00FF0000);
 	// my_mlx_pixel_put(&info->img, 10, 10, 0x00FF0000);
