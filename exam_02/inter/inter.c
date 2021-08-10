ba16f6a9c910f8489856f8a614fb22008353289a
Hiroaki-K4 <49no19@gmail.com>
2021-08-10 11:36:58 +0900

    test inter

--- exam_02/inter/inter.c
+++ exam_02/inter/inter.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 18:15:09 by hkubo             #+#    #+#             */
-/*   Updated: 2021/04/27 12:53:48 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/10 11:36:56 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,49 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
+// int main(int argc, char *argv[])
+// {
+// 	char	*arg1;
+// 	char	*arg2;
+// 	int		i;
+// 	int		j;
+// 	int		flag;
+
+// 	if (argc == 3)
+// 	{
+// 		arg1 = argv[1];
+// 		arg2 = argv[2];
+// 		i = 0;
+// 		while (arg1[i])
+// 		{
+// 			if (ft_strchr(arg2, arg1[i]))
+// 			{
+// 				j = i;
+// 				flag = 1;
+// 				while (j > 0)
+// 				{
+// 					if (arg1[j - 1] == arg1[i])
+// 						flag = 0;
+// 					j--;
+// 				}
+// 				if (flag == 1)
+// 					write(1, &arg1[i], 1);
+// 			}
+// 			i++;
+// 		}
+// 	}
+// 	write(1, "\n", 1);
+// 	return (0);
+// }
+
+
 int main(int argc, char *argv[])
 {
-	char	*arg1;
-	char	*arg2;
-	int		i;
-	int		j;
-	int		flag;
+	int i;
+	int j;
+	int flag;
+	char *arg1;
+	char *arg2;
 
 	if (argc == 3)
 	{
@@ -47,8 +83,8 @@ int main(int argc, char *argv[])
 		{
 			if (ft_strchr(arg2, arg1[i]))
 			{
-				j = i;
 				flag = 1;
+				j = i;
 				while (j > 0)
 				{
 					if (arg1[j - 1] == arg1[i])
