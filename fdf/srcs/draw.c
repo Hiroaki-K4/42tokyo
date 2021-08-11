34a5e6dc98f4ec7790c7a37b84d3dd3bea984ba1
Hiroaki-K4 <49no19@gmail.com>
2021-08-11 20:54:20 +0900

    make draw function

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/11 20:53:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/11 20:54:13 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,10 @@ void	draw(t_info *info)
 
 	// draw row line
 	i = 0;
-	while (info->y_start + i <= info->y_start + info->y_step * info->row_count)
+	while (i <= info->y_step * info->row_count)
 	{
 		j = 0;
-		while (info->x_start + j <= info->x_start + info->x_step * info->col_count[0])
+		while (j <= info->x_step * info->col_count[0])
 		{
 			my_mlx_pixel_put(&info->img, info->x_start + j, info->y_start + i, 0x00FF0000);
 			j++;
@@ -41,10 +41,10 @@ void	draw(t_info *info)
 	}
 	// draw col line
 	i = 0;
-	while (info->x_start + i <= info->x_start + info->x_step * info->col_count[0])
+	while (i <= info->x_step * info->col_count[0])
 	{
 		j = 0;
-		while (info->y_start + j <= info->y_start + info->y_step * info->row_count)
+		while (j <= info->y_step * info->row_count)
 		{
 			my_mlx_pixel_put(&info->img, info->x_start + i, info->y_start + j, 0x00FF0000);
 			j++;
