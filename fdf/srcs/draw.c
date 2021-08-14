243d91a68d2ac6049a8b4bd361efc5ef65dcb8e9
Hiroaki-K4 <49no19@gmail.com>
2021-08-14 17:42:25 +0900

    make draw algorithm

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/14 17:41:29 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/14 17:42:16 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ void	draw(t_info *info)
 	int	x_pos;
 	int	y_pos;
 	int	x_convert;
-	// int	y_convert;
+	int	y_convert;
 
 	printf("cos: %f sin: %f\n", cos(PI * 30.0 / 180.0), sin(PI * 30.0 / 180.0));
 	// draw row line
@@ -57,8 +57,8 @@ void	draw(t_info *info)
 		{
 			x_convert = convert_x(info->x_start + x_pos, info->y_start + y_pos, 30.0);
 			printf("z: %d\n", info->fdf_list[i][j]);
-			// y_convert = convert_y(info->x_start + j, info->y_start + i, info->fdf_list[i][j], 30.0);
-			// printf("x_convert: %d y_convert: %d\n", x_convert, y_convert);
+			y_convert = convert_y(info->x_start + x_pos, info->y_start + y_pos, info->fdf_list[i][j], 30.0);
+			printf("x_convert: %d y_convert: %d\n", x_convert, y_convert);
 			my_mlx_pixel_put(&info->img, info->x_start + x_pos, info->y_start + y_pos, 0x00FF0000);
 			x_pos++;
 			j++;
