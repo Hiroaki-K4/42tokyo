ecaa49a288453880533e14153997539c3b1169d1
Hiroaki-K4 <49no19@gmail.com>
2021-08-14 21:31:00 +0900

    make draw algorithm

--- fdf/run.sh
+++ fdf/run.sh
@@ -3,12 +3,12 @@
 make re
 ./fdf maps/test_maps/42.fdf
 ./fdf maps/test_maps/10-2.fdf
-./fdf maps/test_maps/42.fdf
-./fdf maps/test_maps/42.fdf
-./fdf maps/test_maps/42.fdf
-./fdf maps/test_maps/42.fdf
-./fdf maps/test_maps/42.fdf
-./fdf maps/test_maps/42.fdf
+./fdf maps/test_maps/10-70.fdf
+./fdf maps/test_maps/20-60.fdf
+./fdf maps/test_maps/50-4.fdf
+./fdf maps/test_maps/100-6.fdf
+./fdf maps/test_maps/basictest.fdf
+./fdf maps/test_maps/elem-col.fdf
 make fclean
 cd ..
 sh git.sh $*
