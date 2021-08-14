22f05f8d3859ecd75025cf186247241d80a52677
Hiroaki-K4 <49no19@gmail.com>
2021-08-14 17:26:51 +0900

    make draw algorithm

--- fdf/srcs/main.c
+++ fdf/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:21:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/14 17:25:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/14 17:26:36 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,9 @@ void	init_info(t_info *info)
 	int	size_y;
 
 	info->mlx = mlx_init();
-	mlx_get_screen_size(info->mlx, &size_x, &size_y);
+	// mlx_get_screen_size(info->mlx, &size_x, &size_y);
+	size_x = 1920;
+	size_y = 1280;
 	info->width = size_x;
 	info->height = size_y;
 	info->x_start = info->width * 0.4;
