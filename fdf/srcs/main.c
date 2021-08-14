0b5ffe6af43e799fcbf85c1c177bbf3b70526638
Hiroaki-K4 <49no19@gmail.com>
2021-08-14 21:28:26 +0900

    make draw algorithm

--- fdf/srcs/main.c
+++ fdf/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:21:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/14 21:27:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/14 21:27:58 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ void	init_info(t_info *info)
 	info->width = size_x;
 	info->height = size_y;
 	info->x_start = info->width * 0.5;
-	info->x_end = info->width * 0.7;
+	info->x_end = info->width * 0.75;
 	get_x_step(info);
 	info->y_start = info->height * 0.1;
 	info->y_end = info->height * 0.4;
