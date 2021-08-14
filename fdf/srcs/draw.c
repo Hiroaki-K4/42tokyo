440711929d5e0ece628f5aabb0d7848a9b6fda4a
Hiroaki-K4 <49no19@gmail.com>
2021-08-14 21:10:10 +0900

    make draw algorithm

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/14 21:09:07 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/14 21:09:46 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,8 @@ void	draw(t_info *info)
 			// printf("z: %d\n", info->fdf_list[i][j]);
 			y_convert = convert_y(info->x_start + x_pos, info->y_start + y_pos, 0, 30.0);
 			printf("x_convert: %d y_convert: %d\n", x_convert, y_convert);
-			my_mlx_pixel_put(&info->img, info->x_start + x_pos, info->y_start + y_pos, 0x00FF0000);
+			// my_mlx_pixel_put(&info->img, info->x_start + x_pos, info->y_start + y_pos, 0x00FF0000);
+			my_mlx_pixel_put(&info->img, x_convert, y_convert, 0x00FF0000);
 			x_pos++;
 			if (x_pos == info->x_step * (i + 1))
 				j++;
