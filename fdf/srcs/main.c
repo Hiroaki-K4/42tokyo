dfaa98c947354100d2ee31f64340d9d957f046eb
Hiroaki-K4 <49no19@gmail.com>
2021-08-14 21:41:00 +0900

    make draw algorithm

--- fdf/srcs/main.c
+++ fdf/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:21:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/14 21:40:31 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/14 21:40:40 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,8 @@ void	init_info(t_info *info)
 	info->x_start = info->width * 0.4;
 	info->x_end = info->width * 0.8;
 	get_x_step(info);
-	info->y_start = info->height * 0.3;
-	info->y_end = info->height * 0.7;
+	info->y_start = info->height * 0.2;
+	info->y_end = info->height * 0.6;
 	info->y_step = (info->y_end - info->y_start) / info->row_count;
 	printf("width: %d height: %d\n", info->width, info->height);
 	printf("x_start: %d x_end: %d y_start: %d y_end: %d x_step: %d y_step: %d\n", info->x_start, info->x_end, info->y_start, info->y_end, info->x_step, info->y_step);
