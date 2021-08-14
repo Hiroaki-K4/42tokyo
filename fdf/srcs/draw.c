5af945bbb024d67cb79779cc01f1d3be3828a124
Hiroaki-K4 <49no19@gmail.com>
2021-08-14 17:09:11 +0900

    make draw algorithm

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/14 17:08:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/14 17:08:59 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ void	draw(t_info *info)
 {
 	int	i;
 	int	j;
-	int	convert_x;
+	int	x_convert;
 	// int	convert_y;
 
 	printf("cos: %f sin: %f\n", cos(PI * 30.0 / 180.0), sin(PI * 30.0 / 180.0));
@@ -51,8 +51,8 @@ void	draw(t_info *info)
 		j = 0;
 		while (j <= info->x_step * info->col_count[0])
 		{
-			convert_x = convert_x(info->x_start + j, info->y_start + i, 30.0);
-			printf("convert_x: %d\n", convert_x);
+			x_convert = convert_x(info->x_start + j, info->y_start + i, 30.0);
+			printf("x_convert: %d\n", x_convert);
 			my_mlx_pixel_put(&info->img, info->x_start + j, info->y_start + i, 0x00FF0000);
 			j++;
 		}
