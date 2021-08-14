1f4a5213ecc067be0cf7328db8426dcefd9b36b0
Hiroaki-K4 <49no19@gmail.com>
2021-08-14 17:41:09 +0900

    make draw algorithm

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/14 17:39:50 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/14 17:41:04 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,8 @@ int	convert_y(int x, int y, int z, double angle)
 
 void	draw(t_info *info)
 {
-	// int	i;
-	// int	j;
+	int	i;
+	int	j;
 	int	x_pos;
 	int	y_pos;
 	// int	x_convert;
@@ -47,21 +47,25 @@ void	draw(t_info *info)
 
 	printf("cos: %f sin: %f\n", cos(PI * 30.0 / 180.0), sin(PI * 30.0 / 180.0));
 	// draw row line
+	i = 0;
 	y_pos = 0;
 	while (y_pos <= info->y_step * info->row_count)
 	{
+		y = 0;
 		x_pos = 0;
 		while (x_pos <= info->x_step * info->col_count[0])
 		{
-			// x_convert = convert_x(info->x_start + x_pos, info->y_start + y_pos, 30.0);
-			// printf("z: %d\n", info->fdf_list[i][j]);
+			x_convert = convert_x(info->x_start + x_pos, info->y_start + y_pos, 30.0);
+			printf("z: %d\n", info->fdf_list[i][j]);
 			// y_convert = convert_y(info->x_start + j, info->y_start + i, info->fdf_list[i][j], 30.0);
 			// printf("x_convert: %d y_convert: %d\n", x_convert, y_convert);
 			my_mlx_pixel_put(&info->img, info->x_start + x_pos, info->y_start + y_pos, 0x00FF0000);
 			x_pos++;
+			j++;
 		}
 		printf("x_end: %d\n", info->x_start + x_pos);
 		y_pos += info->y_step;
+		i++;
 	}
 	// draw col line
 	x_pos = 0;
