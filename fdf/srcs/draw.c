8a4b086376e43fca996e662b9e1f8294016a44b6
Hiroaki-K4 <49no19@gmail.com>
2021-08-15 21:27:17 +0900

    make coordinate operation

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/15 21:26:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/15 21:27:14 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,6 +160,7 @@ void	zoom_map(t_info *info)
 
 	x_ratio = info->width / (info->coords.xmax - info->coords.xmin);
 	y_ratio = info->height / (info->coords.ymax - info->coords.ymin);
+	printf("x_ratio: %d y_ratio: %d\n", x_ratio, y_ratio);
 	if (x_ratio <= y_ratio)
 		ratio = x_ratio;
 	else
