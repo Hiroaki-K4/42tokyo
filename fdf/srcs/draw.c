c8368b3191ef45367bd13c41adb02b4ee0c08d0d
Hiroaki-K4 <49no19@gmail.com>
2021-08-15 17:12:33 +0900

    make rotate function

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/15 17:12:10 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/15 17:12:28 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,9 +106,9 @@ void	rotate(t_info *info)
 		j = 0;
 		while (j < info->col_count[0])
 		{
-			x_convert = convert_x(info->map[i][j].x * 50, info->map[i][j].y * 50, 30.0);
+			x_convert = convert_x(info->map[i][j].x * 20, info->map[i][j].y * 20, 30.0);
 			// y_convert = convert_y(info->map[i][j].x * 10, info->map[i][j].y * 10, info->map[i][j].z / 10, 30.0);
-			y_convert = convert_y(info->map[i][j].x * 50, info->map[i][j].y * 50, 0, 30.0);
+			y_convert = convert_y(info->map[i][j].x * 20, info->map[i][j].y * 20, 0, 30.0);
 			printf("i: %d j: %d x: %d y: %d\n", i, j, x_convert, y_convert);
 			if (x_convert >= 0 && y_convert >= 0 && x_convert <= 640 && y_convert <= 480)
 				my_mlx_pixel_put(&info->img, x_convert, y_convert, 0x00FF0000);
