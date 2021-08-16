637e4adcc564339acbbf575776b2092e23e907f9
Hiroaki-K4 <49no19@gmail.com>
2021-08-16 21:58:26 +0900

    make draw algorithm

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/16 21:57:17 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/16 21:58:21 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -241,7 +241,7 @@ void	draw_map(t_info *info)
 		while (j < info->col_count[i])
 		{
 			x_convert = convert_x(info->map[i][j].x * info->ratio * 0.7, info->map[i][j].y * info->ratio * 0.7, 30.0);
-			y_convert = convert_y(info->map[i][j].x * info->ratio * 0.7, info->map[i][j].y * info->ratio * 0.7, info->map[i][j].z * 5, 30.0);
+			y_convert = convert_y(info->map[i][j].x * info->ratio * 0.7, info->map[i][j].y * info->ratio * 0.7, info->map[i][j].z * info->ratio * 0.1, 30.0);
 			// y_convert = convert_y(info->map[i][j].x * info->ratio * 0.7, info->map[i][j].y * info->ratio * 0.7, 0, 30.0);
 			printf("i: %d j: %d x: %d y: %d ratio: %f\n", i, j, x_convert, y_convert, info->ratio);
 			info->map[i][j].x = x_convert;
