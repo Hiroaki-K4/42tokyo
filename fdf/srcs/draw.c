2119bb95040e7a3391fa4b077bc8aec72654ec50
Hiroaki-K4 <49no19@gmail.com>
2021-08-17 23:01:09 +0900

    make color algorithm

--- fdf/srcs/draw.c
+++ fdf/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 21:38:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/17 22:55:30 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/17 23:00:52 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -164,6 +164,7 @@ void	draw_col_line(t_info *info)
 				// printf("i: %d j: %d x: %d y_f: %f y_d: %f slope %f\n", i, j, x, y, round(y), slope);
 				// my_mlx_pixel_put(&info->img, x, (int)round(y), 0x00FF0000);
 				my_mlx_pixel_put(&info->img, x, (int)round(y), 0x00FFFFFF);
+				my_mlx_pixel_put(&info->img, x, (int)round(y), 0x00ffffff);
 			}
 			// printf("x: %f y: %f slope: %f\n", (double)(info->map[i][j + 1].x - info->map[i][j].x), (double)(info->map[i][j + 1].y - info->map[i][j].y), slope);
 			i++;
