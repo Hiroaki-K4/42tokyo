7ec0cdcac0a6dd7f94a533e617fac24ef934811b
Hiroaki-K4 <49no19@gmail.com>
2021-08-17 22:04:12 +0900

    make color algorithm

--- fdf/run.sh
+++ fdf/run.sh
@@ -3,8 +3,8 @@
 make re
 ./fdf maps/test_maps/42.fdf
 ./fdf maps/test_maps/10-2.fdf
-# ./fdf maps/test_maps/10-70.fdf
-# ./fdf maps/test_maps/20-60.fdf
+./fdf maps/test_maps/10-70.fdf
+./fdf maps/test_maps/20-60.fdf
 # ./fdf maps/test_maps/50-4.fdf
 # ./fdf maps/test_maps/100-6.fdf
 # ./fdf maps/test_maps/basictest.fdf
