21e42314188d7f6442e35d202d8b3cad811df0b7
Hiroaki-K4 <49no19@gmail.com>
2021-08-18 22:22:50 +0900

    make color algorithm

--- fdf/run.sh
+++ fdf/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 make re
+
 ./fdf maps/test_maps/42.fdf
-# ./fdf maps/test_maps/10-2.fdf
+./fdf maps/test_maps/10-2.fdf
 # ./fdf maps/test_maps/10-70.fdf
 # ./fdf maps/test_maps/20-60.fdf
 # ./fdf maps/test_maps/50-4.fdf
