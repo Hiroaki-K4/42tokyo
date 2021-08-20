d8d7d41546c16efc41a31a4ff908372ed65c97e9
Hiroaki-K4 <49no19@gmail.com>
2021-08-20 18:47:29 +0900

    make draw algorithm

--- fdf/srcs/get_color.c
+++ fdf/srcs/get_color.c
@@ -6,20 +6,20 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 22:42:08 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/20 18:32:09 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/20 18:47:04 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	check_color_hex(char *color)
+int	check_color_hex(char *color, int len)
 {
 	int	i;
 
 	if (color[0] != '0' || color[1] != 'x')
 		return (-1);
 	i = 2;
-	while (i < 8)
+	while (i < len)
 	{
 		if (ft_isdigit(color[i]) != 1 && (color[i] < 'A' || color[i] > 'F') && (color[i] < 'a' || color[i] > 'f'))
 			return (-1);
@@ -38,59 +38,6 @@ int		ft_strcmp(const char *s1, const char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-// int ft_strchr_place(const char *arg, int c)
-// {
-// 	int j;
-
-// 	j = 0;
-// 	while (arg[j])
-// 	{
-// 		if (arg[j] == c)
-// 			return (j);
-// 		j++;
-// 	}
-// 	return (-1);
-// }
-
-// int	convert_calc(int a, int b, char word)
-// {
-// 	int	ans;
-// 	int	pos;
-
-// 	pos = ft_strchr_place("0123456789ABCDEF", word);
-// 	if (pos == -1)
-// 		pos = ft_strchr_place("0123456789abcdef", word);
-// 	if (pos == -1)
-// 		exit(1);
-// 	ans = 1;
-// 	// if (b == 0)
-// 		// return (1);
-// 	while (b > 0)
-// 	{
-// 		ans = ans * a;
-// 		b--;
-// 	}
-// 	ans = ans * pos;
-// 	return (ans);
-// }
-
-// int	convert_hex_to_dec(char *color)
-// {
-// 	int	i;
-// 	int	dec;
-
-// 	printf("color: %s\n", color);
-// 	dec = 0;
-// 	i = 2;
-// 	while (i < 8 && color[i])
-// 	{
-// 		dec += convert_calc(16, 7 - i, color[i]);
-// 		i++;
-// 	}
-// 	printf("color: %s dec: %d\n", color, dec);
-// 	return (dec);
-// }
-
 char	*get_color(char *line_split)
 {
 	char	*color;
@@ -102,15 +49,23 @@ char	*get_color(char *line_split)
 		if (ft_strlen(color) == 9)
 		{
 			color++;
-			if (check_color_hex(color) == -1)
+			if (check_color_hex(color, 8) == -1)
+				exit(1);
+		}
+		else if (ft_strlen(color) == 5)
+		{
+			color++;
+			if (check_color_hex(color, 4) == -1)
+				exit(1);
+		}
+		else if (ft_strlen(color) == 7)
+		{
+			color++;
+			if (check_color_hex(color, 6) == -1)
 				exit(1);
 		}
 		else
-		{
-			if (ft_strcmp(color, ",0xff") == 0 || ft_strcmp(color, ",0xFF") == 0)
-				return ("0xFFFFFF");
 			exit(1);
-		}
 	}
 	else
 		color = "0xFFFFFF";
