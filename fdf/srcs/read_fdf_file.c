82a96f5165da2f6c9990e86dbdff0cc8e133f995
Hiroaki-K4 <49no19@gmail.com>
2021-08-21 17:06:57 +0900

    make end process

--- fdf/srcs/read_fdf_file.c
+++ fdf/srcs/read_fdf_file.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 20:24:50 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/21 17:03:19 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/21 17:06:38 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,13 +100,13 @@ void	read_fdf_file(t_info *info, char *path)
 	info->row_count = 0;
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
-		exit(1);
+		all_free(info, "[End process] read_fdf_file");
 	i = 1;
 	while (i > 0)
 	{
 		i = get_next_line(fd, &line, 0);
 		if (i == -1)
-			exit(1);
+			all_free(info, "[End process] read_fdf_file");
 		store_fdf_value(info, line);
 		info->row_count++;
 		free(line);
