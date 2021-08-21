7a663e6c2128d0544fd159db13216237ab9196f2
Hiroaki-K4 <49no19@gmail.com>
2021-08-21 21:20:55 +0900

    fix for norminette

--- fdf/srcs/draw_line.c
+++ fdf/srcs/draw_line.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 21:11:12 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/21 21:18:01 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/21 21:20:30 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,16 @@ void	draw_row_line_core(t_info *info, int i, int j)
 	double	y;
 	double	slope;
 
-	slope = (double)(info->map[i][j + 1].y - info->map[i][j].y) / (double)(info->map[i][j + 1].x - info->map[i][j].x);
+	slope = (double)(info->map[i][j + 1].y - info->map[i][j].y)
+		/ (double)(info->map[i][j + 1].x - info->map[i][j].x);
 	x = info->map[i][j].x;
 	y = (double)info->map[i][j].y;
 	while (x < info->map[i][j + 1].x)
 	{
 		y += slope;
 		x++;
-		if (x < 0 || x > info->width || (int)round(y) < 0 || (int)round(y) > info->height)
+		if (x < 0 || x > info->width || (int)round(y) < 0
+			|| (int)round(y) > info->height)
 			all_free(info, "[End process] draw_row_line\n");
 		my_mlx_pixel_put(&info->img, x, (int)round(y), info->map[i][j].color);
 	}
@@ -43,9 +45,6 @@ void	draw_row_line(t_info *info)
 {
 	int		i;
 	int		j;
-	// int		x;
-	// double	y;
-	// double	slope;
 
 	i = 0;
 	while (i < info->row_count - 1)
@@ -53,17 +52,6 @@ void	draw_row_line(t_info *info)
 		j = 0;
 		while (j < info->col_count[i] - 1)
 		{
-			// slope = (double)(info->map[i][j + 1].y - info->map[i][j].y) / (double)(info->map[i][j + 1].x - info->map[i][j].x);
-			// x = info->map[i][j].x;
-			// y = (double)info->map[i][j].y;
-			// while (x < info->map[i][j + 1].x)
-			// {
-			// 	y += slope;
-			// 	x++;
-			// 	if (x < 0 || x > info->width || (int)round(y) < 0 || (int)round(y) > info->height)
-			// 		all_free(info, "[End process] draw_row_line\n");
-			// 	my_mlx_pixel_put(&info->img, x, (int)round(y), info->map[i][j].color);
-			// }
 			draw_row_line_core(info, i, j);
 			j++;
 		}
@@ -71,13 +59,32 @@ void	draw_row_line(t_info *info)
 	}
 }
 
+void	draw_col_line_core(t_info *info, int i, int j)
+{
+	int		x;
+	double	y;
+	double	slope;
+
+	slope = (double)(info->map[i + 1][j].y - info->map[i][j].y) / (double)(info->map[i][j].x - info->map[i + 1][j].x);
+	x = info->map[i][j].x;
+	y = (double)info->map[i][j].y;
+	while (x > info->map[i + 1][j].x)
+	{
+		y += slope;
+		x--;
+		if (x < 0 || x > info->width || (int)round(y) < 0 || (int)round(y) > info->height)
+			all_free(info, "[End process] draw_col_line\n");
+		my_mlx_pixel_put(&info->img, x, (int)round(y), info->map[i][j].color);
+	}
+}
+
 void	draw_col_line(t_info *info)
 {
 	int		i;
 	int		j;
-	int		x;
-	double	y;
-	double	slope;
+	// int		x;
+	// double	y;
+	// double	slope;
 
 	j = 0;
 	while (j < info->col_count[0])
@@ -85,17 +92,18 @@ void	draw_col_line(t_info *info)
 		i = 0;
 		while (i < info->row_count - 2)
 		{
-			slope = (double)(info->map[i + 1][j].y - info->map[i][j].y) / (double)(info->map[i][j].x - info->map[i + 1][j].x);
-			x = info->map[i][j].x;
-			y = (double)info->map[i][j].y;
-			while (x > info->map[i + 1][j].x)
-			{
-				y += slope;
-				x--;
-				if (x < 0 || x > info->width || (int)round(y) < 0 || (int)round(y) > info->height)
-					all_free(info, "[End process] draw_col_line\n");
-				my_mlx_pixel_put(&info->img, x, (int)round(y), info->map[i][j].color);
-			}
+			// slope = (double)(info->map[i + 1][j].y - info->map[i][j].y) / (double)(info->map[i][j].x - info->map[i + 1][j].x);
+			// x = info->map[i][j].x;
+			// y = (double)info->map[i][j].y;
+			// while (x > info->map[i + 1][j].x)
+			// {
+			// 	y += slope;
+			// 	x--;
+			// 	if (x < 0 || x > info->width || (int)round(y) < 0 || (int)round(y) > info->height)
+			// 		all_free(info, "[End process] draw_col_line\n");
+			// 	my_mlx_pixel_put(&info->img, x, (int)round(y), info->map[i][j].color);
+			// }
+			draw_col_line_core(info, i, j);
 			i++;
 		}
 		j++;
