747ad7dbcffb61d4d14a2d22f8e9a17f6fcc473e
Hiroaki-K4 <49no19@gmail.com>
2021-08-21 18:08:37 +0900

    make end process

--- fdf/run.sh
+++ fdf/run.sh
@@ -41,7 +41,7 @@ echo "42_7"
 ./fdf maps/test_maps/42_7.fdf
 echo "42_8"
 ./fdf maps/test_maps/42_8.fdf
-valgrind -s --log-file="log1.txt" --leak-check=full --show-leak-kinds=all --track-origins=yes ./fdf maps/test_maps/42.fdf | grep "ERROR SUMMARY"
+valgrind --log-file="log1.txt" --leak-check=full --show-leak-kinds=all --track-origins=yes ./fdf maps/test_maps/42.fdf | grep "ERROR SUMMARY"
 make fclean
 cd ..
 sh git.sh $*
