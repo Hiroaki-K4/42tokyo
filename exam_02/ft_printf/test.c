5563266bf7873814753221b754d0325d61ad798d
Hiroaki-K4 <49no19@gmail.com>
2021-08-21 12:22:28 +0900

    test ft_printf

--- exam_02/ft_printf/test.c
+++ exam_02/ft_printf/test.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("");
 	ft_printf("--Format---");
 	printf("--Format---");
-	// ft_printf("\n");
+	ft_printf("\n");
 	printf("\n");
     // ft_printf("%d\n", 0);
 	ft_printf("%d\n", 0);
