de124c0868a463f3b3eb258862a13d7e219caa58
Hiroaki-K4 <49no19@gmail.com>
2021-08-21 16:15:56 +0900

    make end process

--- fdf/run.sh
+++ fdf/run.sh
@@ -2,42 +2,43 @@
 
 make re
 
-# ./fdf maps/test_maps/42.fdf
-# ./fdf maps/test_maps/10-2.fdf
-# ./fdf maps/test_maps/10-70.fdf
-# ./fdf maps/test_maps/20-60.fdf
-# ./fdf maps/test_maps/50-4.fdf
-# ./fdf maps/test_maps/100-6.fdf
-# ./fdf maps/test_maps/basictest.fdf
-# ./fdf maps/test_maps/elem-col.fdf
-# ./fdf maps/test_maps/elem-fract.fdf
-# ./fdf maps/test_maps/elem.fdf
-# ./fdf maps/test_maps/elem2.fdf
-# ./fdf maps/test_maps/julia.fdf
-# ./fdf maps/test_maps/mars.fdf
-# ./fdf maps/test_maps/pentenegpos.fdf
-# ./fdf maps/test_maps/plat.fdf
-# ./fdf maps/test_maps/pnp_flat.fdf
-# ./fdf maps/test_maps/pylone.fdf
-# ./fdf maps/test_maps/pyra.fdf
-# ./fdf maps/test_maps/pyramide.fdf
-# ./fdf maps/test_maps/t1.fdf
-# ./fdf maps/test_maps/t2.fdf
-echo "Error case"
-echo "42_1"
-./fdf maps/test_maps/42_1.fdf
-echo "42_2"
-./fdf maps/test_maps/42_2.fdf
-echo "42_3"
-./fdf maps/test_maps/42_3.fdf
-echo "42_4"
-./fdf maps/test_maps/42_4.fdf
-echo "42_5"
-./fdf maps/test_maps/42_5.fdf
-echo "42_6"
-./fdf maps/test_maps/42_6.fdf
-echo "42_7"
-./fdf maps/test_maps/42_7.fdf
+./fdf maps/test_maps/42.fdf
+./fdf maps/test_maps/10-2.fdf
+./fdf maps/test_maps/10-70.fdf
+./fdf maps/test_maps/20-60.fdf
+./fdf maps/test_maps/50-4.fdf
+./fdf maps/test_maps/100-6.fdf
+./fdf maps/test_maps/basictest.fdf
+./fdf maps/test_maps/elem-col.fdf
+./fdf maps/test_maps/elem-fract.fdf
+./fdf maps/test_maps/elem.fdf
+./fdf maps/test_maps/elem2.fdf
+./fdf maps/test_maps/julia.fdf
+./fdf maps/test_maps/mars.fdf
+./fdf maps/test_maps/pentenegpos.fdf
+./fdf maps/test_maps/plat.fdf
+./fdf maps/test_maps/pnp_flat.fdf
+./fdf maps/test_maps/pylone.fdf
+./fdf maps/test_maps/pyra.fdf
+./fdf maps/test_maps/pyramide.fdf
+./fdf maps/test_maps/t1.fdf
+./fdf maps/test_maps/t2.fdf
+
+# echo "Error case"
+# echo "42_1"
+# ./fdf maps/test_maps/42_1.fdf
+# echo "42_2"
+# ./fdf maps/test_maps/42_2.fdf
+# echo "42_3"
+# ./fdf maps/test_maps/42_3.fdf
+# echo "42_4"
+# ./fdf maps/test_maps/42_4.fdf
+# echo "42_5"
+# ./fdf maps/test_maps/42_5.fdf
+# echo "42_6"
+# ./fdf maps/test_maps/42_6.fdf
+# echo "42_7"
+# ./fdf maps/test_maps/42_7.fdf
 valgrind -s --log-file="log1.txt" --leak-check=full --show-leak-kinds=all --track-origins=yes ./fdf maps/test_maps/42.fdf | grep "ERROR SUMMARY"
 make fclean
 cd ..
