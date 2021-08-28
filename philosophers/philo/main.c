32c915070f0c84d7e81addbe4475f76b70454e33
Hiroaki-K4 <49no19@gmail.com>
2021-08-28 21:28:17 +0900

    make main function

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,11 +6,38 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:27:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/28 21:19:35 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/28 21:27:21 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int		i;
+	int		flag;
+	int		ans;
+
+	flag = 1;
+	i = 0;
+	while (nptr[i] == ' ' || nptr[i] == '\f' || nptr[i] == '\n'
+		|| nptr[i] == '\r' || nptr[i] == '\t' || nptr[i] == '\v')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			flag = -1;
+		i++;
+	}
+	ans = 0;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		ans = (ans * 10) + (nptr[i] - '0');
+		i++;
+	}
+	ans = ans * flag;
+	return (ans);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -18,12 +45,18 @@ int	main(int argc, char *argv[])
 	int	t_die;
 	int	t_eat;
 	int	t_sleep;
+	int	must_eat_num;
 
-	if (argc == 6 || argc == 7)
+	must_eat_num = -1;
+	if (argc == 5 || argc == 6)
 	{
 		philo_num = ft_atoi(argv[1]);
 		t_die = ft_atoi(argv[2]);
-		t
+		t_eat = ft_atoi(argv[3]);
+		t_sleep = ft_atoi(argv[4]);
+		if (argc == 7)
+			must_eat_num = ft_atoi(argv[5]);
+		printf("philo_num: %d t_die: %d t_eat: %d t_sleep: %d must_eat: %d", philo_num, t_die, t_eat, t_sleep, must_eat_num);
 	}
 	return (0);
 }
--- philosophers/philo/philosophers.h
+++ philosophers/philo/philosophers.h
@@ -6,11 +6,13 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:29:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/28 17:30:15 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/28 21:25:38 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
+
+#include <stdio.h>
 
 #endif
