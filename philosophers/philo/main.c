358a56a1ab0f0832e2489143fca6032d8af8499f
Hiroaki-K4 <49no19@gmail.com>
2021-08-29 21:16:50 +0900

    add thread

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:27:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/29 20:59:47 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/29 21:16:34 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ int	main(int argc, char *argv[])
 	int	t_eat;
 	int	t_sleep;
 	int	must_eat_num;
-	pthread_t	thread;
+	int	i;
+	pthread_t	*thread;
 
 	must_eat_num = -1;
 	if (argc == 5 || argc == 6)
@@ -39,8 +40,16 @@ int	main(int argc, char *argv[])
 		if (argc == 7)
 			must_eat_num = ft_atoi(argv[5]);
 		printf("philo_num: %d t_die: %d t_eat: %d t_sleep: %d must_eat: %d\n", philo_num, t_die, t_eat, t_sleep, must_eat_num);
-		if (pthread_create(&thread, NULL, thread1, NULL) != 0)
+		thread = (pthread_t *)malloc(sizeof(pthread_t) * philo_num);
+		if (!thread)
 			return (1);
+		i = 0;
+		while (i < philo_num)
+		{
+			if (pthread_create(&thread[i], NULL, thread1, NULL) != 0)
+				return (1);
+			i++;
+		}
 		usleep(100);
 		printf("thread: %ld\n", thread);
 	}
