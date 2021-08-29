d6a9a7b2147c82a99d9fe2eeacd3f7ea6f79e424
Hiroaki-K4 <49no19@gmail.com>
2021-08-29 10:37:47 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/29 10:37:22 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/29 10:37:46 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,32 +140,32 @@ char			*ft_itoa(int n)
 	return (ans);
 }
 
-int		ft_atoi(const char *arg)
-{
-	int		i;
-	int		flag;
-	int		ans;
+// int		ft_atoi(const char *arg)
+// {
+// 	int		i;
+// 	int		flag;
+// 	int		ans;
 
-	flag = 1;
-	i = 0;
-	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
-			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
-		i++;
-	if (arg[i] == '-' || arg[i] == '+')
-	{
-		if (arg[i] == '-')
-			flag = -1;
-		i++;
-	}
-	ans = 0;
-	while (arg[i] >= '0' && arg[i] <= '9')
-	{
-		ans = (ans * 10) + (arg[i] - '0');
-		i++;
-	}
-	ans = ans * flag;
-	return (ans);
-}
+// 	flag = 1;
+// 	i = 0;
+// 	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
+// 			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
+// 		i++;
+// 	if (arg[i] == '-' || arg[i] == '+')
+// 	{
+// 		if (arg[i] == '-')
+// 			flag = -1;
+// 		i++;
+// 	}
+// 	ans = 0;
+// 	while (arg[i] >= '0' && arg[i] <= '9')
+// 	{
+// 		ans = (ans * 10) + (arg[i] - '0');
+// 		i++;
+// 	}
+// 	ans = ans * flag;
+// 	return (ans);
+// }
 
 int		ft_isdigit(int c)
 {
@@ -340,32 +340,32 @@ int		ft_isdigit(int c)
 // 	return (0);
 // }
 
-// int ft_atoi(const char *arg)
-// {
-// 	int i;
-// 	int flag;
-// 	int ans;
+int ft_atoi(const char *arg)
+{
+	int i;
+	int flag;
+	int ans;
 
-// 	i = 0;
-// 	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
-// 			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
-// 		i++;
-// 	flag = 1;
-// 	if (arg[i] == '-' || arg[i] == '+')
-// 	{
-// 		if (arg[i] == '-')
-// 			flag = -1;
-// 		i++;
-// 	}
-// 	ans = 0;
-// 	while (arg[i] >= '0' && arg[i] <= '9')
-// 	{
-// 		ans = ans * 10 + (arg[i] - '0');
-// 		i++;
-// 	}
-// 	ans = ans * flag;
-// 	return (ans);
-// }
+	i = 0;
+	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
+			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
+		i++;
+	flag = 1;
+	if (arg[i] == '-' || arg[i] == '+')
+	{
+		if (arg[i] == '-')
+			flag = -1;
+		i++;
+	}
+	ans = 0;
+	while (arg[i] >= '0' && arg[i] <= '9')
+	{
+		ans = ans * 10 + (arg[i] - '0');
+		i++;
+	}
+	ans = ans * flag;
+	return (ans);
+}
 
 int field_precision(t_plist flag_list, char *str_num, int num, int i)
 {
