44a831e76a493970d27bdcebe0837aa89d54864d
Hiroaki-K4 <49no19@gmail.com>
2021-08-30 22:57:52 +0900

    make thread

--- philosophers/philo/init.c
+++ philosophers/philo/init.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 22:44:49 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/30 22:45:35 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/30 22:57:49 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	init_philo(t_philo *philo_info)
 
 void	init_info(t_info *info)
 {
-	info->philo_num = -1;
+	info->philo_total = -1;
 	info->t_die = -1;
 	info->t_eat = -1;
 	info->t_sleep = -1;
