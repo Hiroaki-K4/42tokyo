b17242da14f3b479201de1e245a65f5bbe438276
Hiroaki-K4 <49no19@gmail.com>
2021-08-30 22:54:30 +0900

    make thread

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:27:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/30 22:54:14 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/30 22:54:28 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ void	*thread1(void *arg)
 	t_info *info;
 	
 	info = (t_info *)arg;
+	info->philo_num++;
 	printf("philo: %d\n", info->philo_num);
 	init_philo(&philo_info);
 	i = 0;
