605a43ece1ff91977033642c1bd562c8830ca3fa
Hiroaki-K4 <49no19@gmail.com>
2021-08-31 22:53:23 +0900

    make eating process

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,46 +6,13 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:27:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/31 22:51:18 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/31 22:52:34 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
 int	store;
-
-void	*eating(t_info *info, t_philo *philo_info)
-{
-	struct timeval tv;
-
-	if (gettimeofday(&tv, NULL) == -1)
-		return (NULL);
-	printf("%ld%ld %d is eating\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
-	usleep(info->t_eat * 1000);
-	return (NULL);
-}
-
-void	*sleeping(t_info *info, t_philo *philo_info)
-{
-	struct timeval tv;
-
-	if (gettimeofday(&tv, NULL) == -1)
-		return (NULL);
-	printf("%ld%ld %d is sleeping\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
-	usleep(info->t_sleep * 1000);
-	return (NULL);
-}
-
-void	*thinking(t_info *info, t_philo *philo_info)
-{
-	struct timeval tv;
-
-	(void)info;
-	if (gettimeofday(&tv, NULL) == -1)
-		return (NULL);
-	printf("%ld%ld %d is thinking\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
-	return (NULL);
-}
 
 void	*philo_thread(void *arg)
 {
---
+++ philosophers/philo/philo_status.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_status.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/31 22:52:16 by hkubo             #+#    #+#             */
+/*   Updated: 2021/08/31 22:52:38 by hkubo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	*eating(t_info *info, t_philo *philo_info)
+{
+	struct timeval tv;
+
+	if (gettimeofday(&tv, NULL) == -1)
+		return (NULL);
+	printf("%ld%ld %d is eating\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
+	usleep(info->t_eat * 1000);
+	return (NULL);
+}
+
+void	*sleeping(t_info *info, t_philo *philo_info)
+{
+	struct timeval tv;
+
+	if (gettimeofday(&tv, NULL) == -1)
+		return (NULL);
+	printf("%ld%ld %d is sleeping\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
+	usleep(info->t_sleep * 1000);
+	return (NULL);
+}
+
+void	*thinking(t_info *info, t_philo *philo_info)
+{
+	struct timeval tv;
+
+	(void)info;
+	if (gettimeofday(&tv, NULL) == -1)
+		return (NULL);
+	printf("%ld%ld %d is thinking\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
+	return (NULL);
+}
