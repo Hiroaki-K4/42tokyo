8cd05cc51df0b52f6aaa64ccdb440c26095bd7ff
Hiroaki-K4 <49no19@gmail.com>
2021-08-31 22:30:56 +0900

    make eating process

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:27:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/31 22:26:49 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/31 22:30:53 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	*sleeping(t_info *info, t_philo *philo_info)
 
 	if (gettimeofday(&tv, NULL) == -1)
 		return (NULL);
-	printf("%ld%ld %d is sleeping\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
+	printf("%ld%ld %d is sleeping\n", tv.tv_sec, tv.tv_usec, philo_info->philo_num);
 	usleep(info->t_sleep);
 	return (NULL);
 }
@@ -44,7 +44,6 @@ void	*thinking(t_info *info, t_philo *philo_info)
 	if (gettimeofday(&tv, NULL) == -1)
 		return (NULL);
 	printf("%ld%ld %d is thinking\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
-	// usleep(info->t_sleep);
 	return (NULL);
 }
 
