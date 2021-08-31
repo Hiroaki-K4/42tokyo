01dbe0c0832d862eda2e6a6deb5f3da1b6bd3480
Hiroaki-K4 <49no19@gmail.com>
2021-08-31 22:26:51 +0900

    make eating process

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:27:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/31 22:26:36 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/08/31 22:26:49 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ void	*thinking(t_info *info, t_philo *philo_info)
 {
 	struct timeval tv;
 
+	(void)info;
 	if (gettimeofday(&tv, NULL) == -1)
 		return (NULL);
 	printf("%ld%ld %d is thinking\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
