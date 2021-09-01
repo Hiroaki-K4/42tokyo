4290e2562aa224d7830f7d891f93aca4ed93fb4c
Hiroaki-K4 <49no19@gmail.com>
2021-09-01 22:34:23 +0900

    add eating time

--- philosophers/philo/philosophers.h
+++ philosophers/philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:29:35 by hkubo             #+#    #+#             */
-/*   Updated: 2021/08/31 23:02:38 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/01 22:34:22 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ extern "C"{
 
 	int increment(int i);
 	int	ft_atoi(const char *nptr);
-	void	init_philo(t_philo *philo_info, t_info *info);
+	void	*init_philo(t_philo *philo_info, t_info *info);
 	void	init_info(t_info *info);
 	void	*eating(t_info *info, t_philo *philo_info);
 	void	*sleeping(t_info *info, t_philo *philo_info);
