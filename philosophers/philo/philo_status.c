4f4c5a4ba4b583a98b26da3295f232862825fe2a
Hiroaki-K4 <49no19@gmail.com>
2021-09-01 22:49:29 +0900

    add eating time

--- philosophers/philo/philo_status.c
+++ philosophers/philo/philo_status.c
@@ -6,11 +6,23 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 22:52:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/01 22:36:10 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/01 22:49:26 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+long	diff_check(struct timeval tv, t_philo *philo_info)
+{
+	long	before_t;
+	long	now_t;
+	long	diff;
+	
+	now_t = (long)(tv.tv_sec * 1000) + (long)(tv.tv_usec / 1000);
+	before_t = (long)(philo_info->eat_date.tv_sec * 1000) + (long)(philo_info->eat_date.tv_usec / 1000);
+	diff = now_t - before_t;
+	return (diff);
+}
 
 void	*eating(t_info *info, t_philo *philo_info)
 {
@@ -18,6 +30,7 @@ void	*eating(t_info *info, t_philo *philo_info)
 
 	if (gettimeofday(&tv, NULL) == -1)
 		return (NULL);
+	printf("diff: %d\n", diff_check(tv, philo_info));
 	if (philo_info->first_eat == 0)
 	{
 		printf("now_sec: %ld before_sec: %ld\n", tv.tv_sec, philo_info->eat_date.tv_sec);
