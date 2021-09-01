678e2c3ec0aaffc114c0d4d8418c2977e25773c9
Hiroaki-K4 <49no19@gmail.com>
2021-09-01 22:35:19 +0900

    add eating time

--- philosophers/philo/init.c
+++ philosophers/philo/init.c
@@ -6,24 +6,24 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 22:44:49 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/01 22:34:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/01 22:35:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	*init_philo(t_philo *philo_info, t_info *info)
+int	init_philo(t_philo *philo_info, t_info *info)
 {
 	struct timeval tv;
 
 	if (gettimeofday(&tv, NULL) == -1)
-		return (NULL);
+		return (-1);
 	philo_info->die_flag = 0;
 	info->philo_count++;
 	philo_info->philo_num = info->philo_count;
 	philo_info->first_eat = 1;
 	philo_info->eat_date = tv;
-	return (NULL);
+	return (0);
 }
 
 void	init_info(t_info *info)
