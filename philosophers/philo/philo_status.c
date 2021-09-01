9eabb6ca18ee22d12866eda7e8215dfacaa436ed
Hiroaki-K4 <49no19@gmail.com>
2021-09-01 22:51:20 +0900

    add check process of eat date

--- philosophers/philo/philo_status.c
+++ philosophers/philo/philo_status.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 22:52:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/01 22:49:32 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/01 22:50:54 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,8 @@ void	*eating(t_info *info, t_philo *philo_info)
 	if (gettimeofday(&tv, NULL) == -1)
 		return (NULL);
 	printf("diff: %ld\n", diff_check(tv, philo_info));
-	if (philo_info->first_eat == 0)
-	{
-		printf("now_sec: %ld before_sec: %ld\n", tv.tv_sec, philo_info->eat_date.tv_sec);
-		printf("now_usec: %ld before_usec: %ld\n", tv.tv_usec, philo_info->eat_date.tv_usec);
-	}
-	else
-	{
-		printf("now_sec: %ld before_sec: %ld\n", tv.tv_sec, philo_info->eat_date.tv_sec);
-		printf("now_usec: %ld before_usec: %ld\n", tv.tv_usec, philo_info->eat_date.tv_usec);
-	}
+	printf("now_sec: %ld before_sec: %ld\n", tv.tv_sec, philo_info->eat_date.tv_sec);
+	printf("now_usec: %ld before_usec: %ld\n", tv.tv_usec, philo_info->eat_date.tv_usec);
 	printf("%ld%ld %d is eating\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
 	philo_info->eat_date = tv;
 	if (philo_info->first_eat == 1)
