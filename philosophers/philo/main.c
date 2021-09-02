f9932d4303ef0bc8401fe673fb0e366285378aab
Hiroaki-K4 <49no19@gmail.com>
2021-09-02 21:49:36 +0900

    add mutex

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,11 +6,13 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:27:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/02 21:44:26 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/02 21:49:15 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+pthread_mutex_t	*fork_mutex;
 
 void	*philo_thread(void *arg)
 {
@@ -47,7 +49,12 @@ int	main(int argc, char *argv[])
 		thread = (pthread_t *)malloc(sizeof(pthread_t) * info.philo_total);
 		if (!thread)
 			return (1);
-		
+		if (info.philo_total == 1)
+			fork_mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * 2);
+		else
+			fork_mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * info.philo_total);
+		if (!fork_mutex)
+			return (-1);
 		i = 0;
 		while (i < info.philo_total)
 		{
