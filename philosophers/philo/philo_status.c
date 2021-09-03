78dbbfdd93fc333f4b03ccd3bd9327e3c7c15964
Hiroaki-K4 <49no19@gmail.com>
2021-09-03 22:07:12 +0900

    add mutex

--- philosophers/philo/philo_status.c
+++ philosophers/philo/philo_status.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 22:52:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/03 22:05:28 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/03 22:07:09 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ void	*eating(t_info *info, t_philo *philo_info)
 		// printf("die: %d diff: %ld\n", info->t_die, diff_check(tv, philo_info));
 		// exit(1);
 	// }
-	printf("%ld%lu %d is eating\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
+	printf("%lu%lu %d is eating\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
 	philo_info->eat_date = tv;
 	if (philo_info->first_eat == 1)
 		philo_info->first_eat = 0;
