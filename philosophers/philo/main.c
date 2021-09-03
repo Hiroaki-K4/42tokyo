102a02cf8f941f0a50e4a478913fcafc850878c6
Hiroaki-K4 <49no19@gmail.com>
2021-09-03 22:06:10 +0900

    add mutex

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:27:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/03 22:05:57 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/03 22:06:08 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ void	*philo_thread(void *arg)
 		eating(info, &philo_info);
 		sleeping(info, &philo_info);
 		thinking(info, &philo_info);
-		printf("die_flag: %d\n", philo_info.die_flag);
+		// printf("die_flag: %d\n", philo_info.die_flag);
 	}
-	printf("die2_flag: %d\n", philo_info.die_flag);
+	// printf("die2_flag: %d\n", philo_info.die_flag);
 	return (NULL);
 }
 
