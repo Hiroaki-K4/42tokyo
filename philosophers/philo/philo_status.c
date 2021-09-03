bbf929183cfc46e25bca67ecb0a5d9d6e4e1fe62
Hiroaki-K4 <49no19@gmail.com>
2021-09-03 22:50:20 +0900

    add mutex

--- philosophers/philo/philo_status.c
+++ philosophers/philo/philo_status.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 22:52:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/03 22:49:57 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/03 22:50:19 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ void	*eating(t_info *info, t_philo *philo_info)
 	printf("%ld%03ld %d has taken a fork\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
 	if (check_time_diff(tv, philo_info) > info->t_die)
 	{
-		printf("die: %d diff: %ld\n", info->t_die, diff_check(tv, philo_info));
+		printf("die: %d diff: %ld\n", info->t_die, check_time_diff(tv, philo_info));
 		info->die_flag = 1;
 	}
 	if (gettimeofday(&tv, NULL) == -1)
