fb3d99a46cb5622f4f47a1035f05543f0019d359
Hiroaki-K4 <49no19@gmail.com>
2021-09-04 16:48:01 +0900

    add death monitor

--- philosophers/philo/philo_status.c
+++ philosophers/philo/philo_status.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 22:52:16 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/04 16:44:56 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/04 16:47:44 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,12 @@ void	*eating(t_info *info, t_philo *philo_info)
 {
 	struct timeval tv;
 
-	// printf("start_eat: %d\n", philo_info->philo_num);
 	if (philo_info->philo_num == info->philo_total)
 	{
 		if (pthread_mutex_lock(&fork_mutex[philo_info->philo_num - 1]) != 0)
 			printf("error\n");
 		if (pthread_mutex_lock(&fork_mutex[0]) != 0)
 			printf("error\n");
-		// printf("lock %d %d\n", 0, philo_info->philo_num - 1);
 	}
 	else
 	{
@@ -43,17 +41,10 @@ void	*eating(t_info *info, t_philo *philo_info)
 			printf("error\n");
 		if (pthread_mutex_lock(&fork_mutex[philo_info->philo_num]) != 0)
 			printf("error\n");
-		// printf("lock %d %d\n", philo_info->philo_num - 1, philo_info->philo_num);
 	}
 	if (gettimeofday(&tv, NULL) == -1)
 		return (NULL);
 	printf("%ld%03ld %d has taken a fork\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
-	// if (check_time_diff(tv, philo_info) > info->t_die)
-	// {
-	// 	printf("die: %d diff: %ld\n", info->t_die, check_time_diff(tv, philo_info));
-	// 	printf("%ld%03ld %d died\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
-	// 	info->die_flag = 1;
-	// }
 	if (gettimeofday(&tv, NULL) == -1)
 		return (NULL);
 	printf("%ld%03ld %d is eating\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
@@ -67,7 +58,6 @@ void	*eating(t_info *info, t_philo *philo_info)
 			printf("error\n");
 		if (pthread_mutex_unlock(&fork_mutex[0]) != 0)
 			printf("error\n");
-		// printf("unlock %d %d\n", 0, philo_info->philo_num - 1);
 	}
 	else
 	{
@@ -75,9 +65,7 @@ void	*eating(t_info *info, t_philo *philo_info)
 			printf("error\n");
 		if (pthread_mutex_unlock(&fork_mutex[philo_info->philo_num]) != 0)
 			printf("error\n");
-		// printf("unlock %d %d\n", philo_info->philo_num - 1, philo_info->philo_num);
 	}
-	// printf("end_eat: %d\n", philo_info->philo_num);
 	return (NULL);
 }
 
@@ -85,12 +73,10 @@ void	*sleeping(t_info *info, t_philo *philo_info)
 {
 	struct timeval tv;
 
-	// printf("start_sleep: %d\n", philo_info->philo_num);
 	if (gettimeofday(&tv, NULL) == -1)
 		return (NULL);
 	printf("%ld%03ld %d is sleeping\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
 	usleep(info->t_sleep * 1000);
-	// printf("end_sleep: %d\n", philo_info->philo_num);
 	return (NULL);
 }
 
@@ -98,12 +84,9 @@ void	*thinking(t_info *info, t_philo *philo_info)
 {
 	struct timeval tv;
 
-	// printf("start_think: %d\n", philo_info->philo_num);
 	(void)info;
 	if (gettimeofday(&tv, NULL) == -1)
 		return (NULL);
 	printf("%ld%03ld %d is thinking\n", tv.tv_sec, tv.tv_usec / 1000, philo_info->philo_num);
-	// usleep(200 * 1000);
-	// printf("end_think: %d\n", philo_info->philo_num);
 	return (NULL);
 }
