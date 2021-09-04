7eac37fcc9f6dd51bd2dab96247f12c888af177b
Hiroaki-K4 <49no19@gmail.com>
2021-09-04 22:49:27 +0900

    fix for norminette

--- philosophers/philo/run.sh
+++ philosophers/philo/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 make re
-./philo 3 901 300 300
+./philo 3 899 300 300
 valgrind --log-file="log_leak.txt" --leak-check=full --show-leak-kinds=all --track-origins=yes ./philo 3 899 300 300 | grep "ERROR SUMMARY"
 # valgrind --tool=helgrind --log-file="log_thread.txt" ./philo 3 899 300 300 | grep "ERROR SUMMARY"
 make fclean
