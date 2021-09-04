9492c437e7477ac1c864ec33bbd82a398a8bcc45
Hiroaki-K4 <49no19@gmail.com>
2021-09-04 20:06:32 +0900

    fix memory leak

--- philosophers/philo/run.sh
+++ philosophers/philo/run.sh
@@ -2,7 +2,7 @@
 
 make re
 ./philo 3 899 300 300
-valgrind --log-file="log1.txt" --leak-check=full --show-leak-kinds=all --track-origins=yes ./philo 3 899 300 300 | grep "ERROR SUMMARY"
+valgrind --tool=helgrind --log-file="log1.txt" --leak-check=full --show-leak-kinds=all --track-origins=yes ./philo 3 899 300 300 | grep "ERROR SUMMARY"
 make fclean
 # cd test
 # make re
