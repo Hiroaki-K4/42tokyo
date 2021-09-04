e2b8825fc31add69d7dbad35a67d2db8eaf87696
Hiroaki-K4 <49no19@gmail.com>
2021-09-04 18:25:31 +0900

    add death monitor

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 17:27:34 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/04 18:24:57 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/04 18:25:26 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void	exit_thread(pthread_t *thread, t_info *info)
 		if (pthread_join(thread[i], NULL) != 0)
 			printf("error\n");
 		if (pthread_mutex_destroy(&fork_mutex[i]) != 0)
-			printf("error\n");
+			printf("error2\n");
 		i++;
 	}
 	i = 0;
