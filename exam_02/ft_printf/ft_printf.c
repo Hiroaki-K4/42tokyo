fbca221dd9865ec0cfe27c578d5e883c39efd859
Hiroaki-K4 <49no19@gmail.com>
2021-09-05 10:51:33 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/05 10:50:42 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/05 10:51:24 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -247,6 +247,12 @@ char	*ft_strdup(const char *s)
 // 	return (len);
 // }
 
+int ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
 
 int field_precision(t_plist flag_list, char *str_num, int num, int i)
 {
