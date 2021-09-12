7c8b2dd5407359cec8d6660fe8d9b9d69aac9e74
Hiroaki-K4 <49no19@gmail.com>
2021-09-12 22:18:37 +0900

    add eat mutex

--- philosophers/philo/run.sh
+++ philosophers/philo/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 make re
-./philo 3 920 300 300 3
+./philo 3820 300 300 3
 valgrind --log-file="log_leak.txt" --leak-check=full --show-leak-kinds=all --track-origins=yes ./philo 3 899 300 300 3 | grep "ERROR SUMMARY"
-valgrind --tool=helgrind --log-file="log_thread.txt" ./philo 3 899 300 300 3 | grep "ERROR SUMMARY"
+valgrind --tool=helgrind --log-file="log_thread.txt" ./philo 3 899 400 300 3 | grep "ERROR SUMMARY"
 make fclean
 # cd test
 # make re
