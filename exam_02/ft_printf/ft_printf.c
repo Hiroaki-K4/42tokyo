1895268cf734e676f9bd9caa4606910ae2073e02
Hiroaki-K4 <49no19@gmail.com>
2021-09-21 15:55:18 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/21 15:51:36 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/21 15:55:16 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -246,6 +246,33 @@ char	*ft_strdup(const char *s)
 // 	}
 // 	return (len);
 // }
+
+int ft_atoi(const char *arg)
+{
+	int ans;
+	int flag;
+	int i;
+
+	i = 0;
+	while (arg[i] == ' ' || arg[i] == '\f' || arg[i] == '\n' ||
+			arg[i] == '\r' || arg[i] == '\t' || arg[i] == '\v')
+		i++;
+	flag = 1;
+	if (arg[i] == '-' || arg[i] == '+')
+	{
+		if (arg[i] == '-')
+			flag = -1
+		i++;
+	}
+	ans = 0;
+	while (arg[i] >= '0' && arg[i] <= '9')
+	{
+		ans = ans * 10 + (arg[i] - '0');
+		i++;
+	}
+	ans = ans * flag;
+	return (ans);
+}
 
 int ft_isdigit(int c)
 {
