e3fa3a95d64de677597fedb401a9361445941b9e
Hiroaki-K4 <49no19@gmail.com>
2021-09-21 23:09:19 +0900

    fix ft_atoi

--- philosophers/philo/utils.c
+++ philosophers/philo/utils.c
@@ -6,38 +6,11 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/29 20:43:24 by hkubo             #+#    #+#             */
-/*   Updated: 2021/09/21 23:07:16 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/09/21 23:09:06 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-
-// int	ft_atoi(const char *nptr)
-// {
-// 	int		i;
-// 	int		flag;
-// 	int		ans;
-
-// 	flag = 1;
-// 	i = 0;
-// 	while (nptr[i] == ' ' || nptr[i] == '\f' || nptr[i] == '\n'
-// 		|| nptr[i] == '\r' || nptr[i] == '\t' || nptr[i] == '\v')
-// 		i++;
-// 	if (nptr[i] == '-' || nptr[i] == '+')
-// 	{
-// 		if (nptr[i] == '-')
-// 			flag = -1;
-// 		i++;
-// 	}
-// 	ans = 0;
-// 	while (nptr[i] >= '0' && nptr[i] <= '9')
-// 	{
-// 		ans = (ans * 10) + (nptr[i] - '0');
-// 		i++;
-// 	}
-// 	ans = ans * flag;
-// 	return (ans);
-// }
 
 int	ft_atoi(const char *nptr)
 {
