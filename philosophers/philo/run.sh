2a2705f6756761a513eb3f082d9b48c2bb2d9c45
Hiroaki-K4 <49no19@gmail.com>
2021-09-22 21:19:46 +0900

    fix ft_atoi

--- philosophers/philo/run.sh
+++ philosophers/philo/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 make re
-# ./philo 3 899 300 300 3
-./philo 3 4294967296 300 300 3
+./philo 3 899 300 300 3
+# ./philo 3 4294967296 300 300 3
 valgrind --log-file="log_leak.txt" --leak-check=full --show-leak-kinds=all --track-origins=yes ./philo 3 899 300 300 3 | grep "ERROR SUMMARY"
 valgrind --tool=helgrind --log-file="log_thread.txt" ./philo 3 899 400 300 3 | grep "ERROR SUMMARY"
 make fclean
