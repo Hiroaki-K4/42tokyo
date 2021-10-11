37969ec9556d305b7603dff987f1dd30c73f3763
Hiroaki-K4 <49no19@gmail.com>
2021-10-11 22:13:39 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/10/11 22:11:49 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/10/11 22:13:38 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -626,7 +626,19 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 // }
 
 
+int ft_printf_str(const char *arg, int *i)
+{
+	int len;
 
+	len = 0;
+	while (arg[*i] && arg[*i] != '%')
+	{
+		write(1, &arg[*i], 1);
+		len++;
+		(*i)++;
+	}
+	return (len);
+}
 
 int ft_printf(const char *arg, ...)
 {
