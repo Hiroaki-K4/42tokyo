9c8c1215aa9212d3042755fdd7a67a958d1f9c42
Hiroaki-K4 <49no19@gmail.com>
2021-11-02 10:33:30 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/11/02 10:32:27 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/11/02 10:33:30 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -246,6 +246,13 @@ char	*ft_strdup(const char *s)
 // 	}
 // 	return (len);
 // }
+
+int ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
 
 int field_precision(t_plist flag_list, char *str_num, int num, int i)
 {
