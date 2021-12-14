6d8648c9a332927a0a75c9b40d3597a84b728d05
Hiroaki Kubo <kubo@globalwalkers.co.jp>
2021-12-14 22:46:05 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/12/14 22:41:23 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/12/14 22:46:03 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -411,7 +411,7 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 
 	keta = ft_strlen(str_num);
 	if (num < 0)
-		keta--;
+		keta++;
 	if (flag_list.precision > keta)
 	{
 		len = flag_list.precision;
