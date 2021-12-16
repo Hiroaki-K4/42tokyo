0d676129d97aec27b5865ed85fa2ba398481e665
Hiroaki Kubo <kubo@globalwalkers.co.jp>
2021-12-16 21:14:02 +0900

    test ft_printf

--- exam_02/ft_printf/ft_printf.c
+++ exam_02/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:39:32 by hkubo             #+#    #+#             */
-/*   Updated: 2021/12/16 21:03:46 by hkubo            ###   ########.fr       */
+/*   Updated: 2021/12/16 21:14:02 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -624,6 +624,33 @@ int no_field_int(t_plist flag_list, char *str_num, int num, int len)
 // 	return (print_len);
 // }
 
+int print_digit(t_plist flag_list, char *str_num, int num, int keta)
+{
+	int len;
+
+	len = 0;
+	if (flag_list.precision == 0 && num == 0)
+	{
+		while (flag_list.field - (len++) > 0)
+			write(1, " ", 1);
+	}
+	else if (flag_list.field > (int)ft_strlen(str_num))
+	{
+		if (flag_list.precision > keta)
+			len = field_precision(flag_list, str_num, num, 0);
+		else
+		{
+			while (flag_list.field - (int)ft_strlen(str_num) - (len++) > 0)
+				write(1, " ", 1);
+			write(1, str_num, ft_strlen(str_num));
+			len = flag_list.field;
+		}
+	}
+	else
+		len = no_field_int(flag_list, str_num, num, 0);
+	return (len);
+}
+
 
 int print_string(va_list *ap, t_plist flag_list)
 {
@@ -700,6 +727,7 @@ int ft_printf_per(const char *arg, int *i, va_list *ap)
 	t_plist flag_list;
 
 	len = 0;
+	(*i)++;
 	flag_list.field = str_to_num(&arg[*i], i);
 	flag_list.precision = -1;
 	if (arg[*i] == '.')
